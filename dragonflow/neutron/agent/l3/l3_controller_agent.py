875bf40064bfb9e38b60a6bd6d06829b21eafa76
Saggi Mizrahi <saggi.mizrahi@huawei.com>
2015-03-11 15:15:21 +0200

    l3 controller agent: Simplify interface iteration
    This removes a condition from the flow making it simpler to understand.
    
    Change-Id: I444a7f51e232161689bc6b4239db351e2b9fec42
    Related-Bug: #1429590
    Signed-off-by: Saggi Mizrahi <saggi.mizrahi@huawei.com>

--- dragonflow/neutron/agent/l3/l3_controller_agent.py
+++ dragonflow/neutron/agent/l3/l3_controller_agent.py
@@ -217,10 +217,8 @@ class L3ControllerAgent(manager.Manager):
         #self._process_router_if_compatible(router)
         self.controller.sync_router(router)
 
-        # Handle case of router with no interfaces
-        if '_interfaces' in router:
-            for interface in router['_interfaces']:
-                self.sync_subnet_port_data(interface['subnet']['id'])
+        for interface in router.get('_interfaces', ()):
+            self.sync_subnet_port_data(interface['subnet']['id'])
 
         LOG.debug("Finished a router update for %s", update.id)
         router_processor.fetched_and_processed(update.timestamp)
