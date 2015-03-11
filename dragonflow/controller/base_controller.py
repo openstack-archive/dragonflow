9fcdbf6a86c6607bc33d08503cfc2343d1471e43
Eran Gampel <Eran.Gampel@Huawei.com>
2015-03-11 09:25:21 +0200

    Copyright message Fix
    Change-Id: I91266f6d604dda2e57acc170aaf061e1acd1720b

--- dragonflow/controller/base_controller.py
+++ dragonflow/controller/base_controller.py
@@ -1,5 +1,4 @@
-
-# Copyright (c) 2014 OpenStack Foundation.
+# Copyright (c) 2015 OpenStack Foundation.
 # All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
