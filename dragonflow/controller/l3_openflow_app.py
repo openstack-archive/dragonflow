e6cfcc23cc27a286e1c1bb6337d5e3933632f5f0
eran gampel <Eran.Gampel@Huawei.com>
2015-04-15 17:46:39 +0300

    Fix the way we check is_router to include the DVR type
    Change-Id: I641e4f0ca1aa6267838564ca3e48732960fbf040

--- dragonflow/controller/l3_openflow_app.py
+++ dragonflow/controller/l3_openflow_app.py
@@ -242,7 +242,7 @@ class PortData(object):
 
     @property
     def is_router_interface(self):
-        if self._port_data['device_owner'] == const.DEVICE_OWNER_ROUTER_INTF:
+        if self._port_data['device_owner'] in const.DEVICE_OWNER_ROUTER_INTF:
             return True
         else:
             return False
