1090e6bd7f37a49a812374eb2c3fbfca7c2be2cf
eran gampel <Eran.Gampel@Huawei.com>
2015-06-21 18:54:16 +0300

    Added get_subnets to the Router PortData Class This will enable to get a list of subnets configured for this port
    Change-Id: I0d2a08f0636c70a15d4106c03ce9d61a982c8f75

--- dragonflow/controller/l3_openflow_app.py
+++ dragonflow/controller/l3_openflow_app.py
@@ -398,6 +398,17 @@ class PortData(object):
             return tuple()
 
     @property
+    def subnets(self):
+        subnets = []
+        for fixed_ip in self.fixed_ips:
+            try:
+                subnets.append(fixed_ip['subnet_id'])
+            except KeyError:
+                LOG.error(_LE("No subnet info for %s"), fixed_ip)
+                continue
+        return subnets
+
+    @property
     def segmentation_id(self):
         return self._port_data['segmentation_id']
 
