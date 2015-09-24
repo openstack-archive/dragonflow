c665d83eaa27d80fdcea013d9fbdb7cf36681bd3
itamaro <itamar.ofek@huawei.com>
2015-09-24 10:28:04 +0300

    add virtual function stubs for DFlowApp class
    Change-Id: I809bc8b69c91540191ef7bf2226dae2fe156978c

---
+++ dragonflow/controller/df_db_notifier.py
@@ -0,0 +1,83 @@
+# Copyright (c) 2015 OpenStack Foundation.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+class DBNotifyInterface(object):
+    """An interface class which provide virtual hook callback functions stubs
+    for an application wishing to be notified on db updates
+    """
+
+    def add_local_port(self, lport):
+        """add local logical port hook callback
+
+
+        :param lport:    local logical port which is added to db
+        """
+
+    def add_remote_port(self, lport):
+        """add remote logical port hook callback
+
+
+        :param lport:   logical port which resides on other compute node, and
+        is added to db
+        """
+
+    def remove_local_port(self, lport_id):
+        """remove local logical port hook callback
+
+
+        :param lport_id:     id of local logical port that is removed from db
+        """
+
+    def remove_remote_port(self, lport_id):
+        """remove remote logical port hook callback
+
+
+        :param lport_id:      id of logical port which resides on other
+        compute node, and is removed from db
+        """
+
+    def add_new_router_port(self, router, router_port, local_network_id):
+        """add  new port to router hook callback
+
+
+        param router:      the router that owns the new port
+        param router_port: the new port
+        param local_network_id: the id of the local network that the added port
+        belongs to
+        """
+
+    def delete_router_port(self, router_port, local_network_id):
+        """delata a  router port hook callback
+
+
+        param router_port: the deleted port
+        param local_network_id: the id of the local network that the deleted
+        port belongs to
+        """
+
+    def logical_switch_deleted(self, lswitch_id):
+        """logical switch deleted hook callback
+
+
+        :param lswitch_id: logical switch id of the deleted switch
+        """
+
+    def logical_switch_updated(self, lswitch):
+        """logical switch updated hook callback
+
+
+        :param lswitch: logical switch that is updated
+        """
