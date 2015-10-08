ac0c56d344d646278d5c2919463178298d7627cf
aharonav <aharon.avitzur@huawei.com>
2015-10-08 14:28:51 +0300

    Adding RAMCloud driver
    Adding RAMCloud driver implementation and python bindings
    
    Fix RAMCloud driver and setup
    
    Change-Id: I205769eaab22fa6e6abad2958a8a8dc2f6857fa8

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -33,6 +33,10 @@ if is_service_enabled df-etcd ; then
 fi
 if is_service_enabled df-ramcloud ; then
    source $DEST/dragonflow/devstack/ramcloud_driver
+   NB_DRIVER_CLASS="dragonflow.db.drivers.ramcloud_db_driver.RamCloudDbDriver"
+fi
+if is_service_enabled df-rethinkdb ; then
+   source $DEST/dragonflow/devstack/rethinkdb_driver
 fi
 
 # Dragonflow installation uses functions from these files
