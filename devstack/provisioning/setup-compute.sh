85cfbd319e2a86df020a4d8b62c70bda6f025307
Gal Sagie <gal.sagie@huawei.com>
2015-10-14 08:44:10 +0300

    Add Vagrant support for Dragonflow deployment
    This Vagrant installation configure and deploy two nodes,
    one a controller and one is compute node and deploy them
    with Dragonflow as their Neutron plugin.
    
    Change-Id: I0ba777b7f58ac23d32e1c174a2a7bf4f70d5106b

---
+++ devstack/provisioning/setup-compute.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cp dragonflow/doc/source/multi-node-conf/compute_node_local_controller.conf devstack/local.conf
+if [ "$1" != "" ]; then
+    sed -i -e 's/<IP address of host running everything else>/'$1'/g' devstack/local.conf
+fi
+devstack/stack.sh
