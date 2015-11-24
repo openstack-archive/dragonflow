a80f7c831cbc7924bcfca7d37949c632a97ce4e2
Eran Gampel <eran@gampel.net>
2015-11-24 18:51:49 +0200

    Ipv4 DHCP support without running the DHCP agent
    We fully support distributed Ipv4 DHCP, if the deployment
    is IPv4 only no need for the dhcp agent it could be removed
    from the installation. the plugin will take care of the
    DHCP server port creation.
    
    In case the deployment use IPv6 the dhcp agent need to be
    installed and the neutron server need to be configured
    with additional variable
    
    use_centralized_ipv6_DHCP=True
    
    This extra setting could be removed once we implement
    the Ipv6 distributed DHCP SDN application
    Change-Id: I48f38f80886dc564f8da788959acc70df3cf2180

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -83,6 +83,10 @@ function configure_df_plugin {
         iniset $NEUTRON_CONF DEFAULT service_plugins ""
     fi
 
+    if is_service_enabled q-dhcp ; then
+        iniset $NEUTRON_CONF df use_centralized_ipv6_DHCP "True"
+    fi
+
     if ! is_service_enabled q-svc; then
         _create_neutron_conf_dir
         NEUTRON_CONF=/etc/neutron/neutron.conf
