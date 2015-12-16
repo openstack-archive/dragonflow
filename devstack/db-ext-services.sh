dcd1c02a72b4cf63bb7da125f818ab86a7eeeebe
shlominar <shlominar@gmail.com>
2015-12-16 13:10:31 +0200

    Adding RethinkDB driver
    Closes-Bug:#1527970
    
    Change-Id: I2e592b752a51cd3651be6c020ca052f57fa9efef

---
+++ devstack/db-ext-services.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Locate devstack directory
+TOP_DIR=/home/stack/devstack
+
+# Detect path for this script file
+DF_DIR=$(cd $(dirname "$0") && pwd)
+
+# Main include for all openstack functions
+source openrc
+
+# Include other dragonflow scripts
+source $DF_DIR/settings
+source $DF_DIR/plugin.sh
+
+if ! nb_db_driver_status_server; then
+    # make sure the db is stopped
+    nb_db_driver_stop_server
+    echo "Going to start db"
+    nb_db_driver_start_server
+fi
+
+# Start ovs db and etcd/ramcloud
+start_ovs
