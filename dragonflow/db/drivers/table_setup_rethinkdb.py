bffc8305a998b5a335479f23712c5d4f0da63bdf
shlominar <shlominar@gmail.com>
2015-12-16 13:10:31 +0200

    Adding RethinkDB driver
    Closes-Bug: #1527970
    
    Change-Id: I2e592b752a51cd3651be6c020ca052f57fa9efef

---
+++ dragonflow/db/drivers/table_setup_rethinkdb.py
@@ -0,0 +1,77 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# !/usr/bin/python
+
+import getopt
+import rethinkdb as R
+import sys
+
+
+def main(argv):
+    db_ip = ''
+    db_port = ''
+    try:
+        opts, args = getopt. \
+            getopt(sys.argv[1:],
+                   'd:t:i:p:', ['-t val', '-i val', '-p val'])
+    except getopt.GetoptError:
+        print ('table_setup_rethinkdb.py '
+               ' -t <table_name>,<table_name>..<table_name>'
+               ' -i <db_ip> -p <db_port>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('table_setup_rethinkdb.py '
+                   '-t <table_name> -i <db_ip> -p <db_port>')
+            sys.exit()
+        elif opt in "-t":
+            db_table = arg.split(',')
+        elif opt in "-i":
+            db_ip = arg
+        elif opt in "-p":
+            db_port = arg
+    print ('table names are: ', db_table)
+    print ('db_ip  is: ', db_ip)
+    print ('db_port is: ', db_port)
+    print ('driver for rethinkdb')
+    print ('DB Host ' + db_ip + ':' + db_port)
+    db_name = 'dragonflow'
+    R.connect(host=db_ip, port=int(db_port)).repl()
+    db_exist = 0
+    all_databases = R.db_list().run()
+    for database in all_databases:
+        if database == db_name:
+            print ('Database ' + db_name +
+                   ' already exist, do not create')
+            # R.db_drop(db_name).run()
+            db_exist = 1
+    if db_exist == 0:
+        print ('Creating Database: ' + db_name)
+        R.db_create(db_name).run()
+
+    all_tables = R.db(db_name).table_list().run()
+    for table in db_table:
+        for _table in all_tables:
+            if _table == table:
+                print ('Table ' + table +
+                       ' already exist in database '
+                       + db_name + '...cleaning up...')
+                R.db(db_name).table_drop(table).run()
+        print ('Creating Table: ' + table +
+               ' in database: ' + db_name)
+        R.db(db_name). \
+            table_create(table, primary_key='name').run()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
