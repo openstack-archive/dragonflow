e77e011431325bd2234f91e08971ec8c49687952
Yuli <yuli.stremovsky@huawei.com>
2015-12-23 16:18:05 +0200

    Add new test and code refactoring
    Change-Id: I9cfdec5049a9c71ebddbad7d069f9ec271443a10

---
+++ dragonflow/tests/fullstack/test_objects.py
@@ -0,0 +1,81 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+class RouterTestWrapper(object):
+
+    def __init__(self, neutron, nb_api):
+        self.router_id = None
+        self.neutron = neutron
+        self.nb_api = nb_api
+        self.deleted = False
+
+    def create(self):
+        router = {'name': 'myrouter1', 'admin_state_up': True}
+        new_router = self.neutron.create_router({'router': router})
+        self.router_id = new_router['router']['id']
+        return self.router_id
+
+    def __del__(self):
+        if self.deleted or self.router_id is None:
+            return
+        self.delete()
+
+    def delete(self):
+        ports = self.neutron.list_ports(device_id=self.router_id)
+        ports = ports['ports']
+        for port in ports:
+            self.neutron.delete_port(port['id'])
+        self.neutron.delete_router(self.router_id)
+        self.deleted = True
+
+    def exists(self):
+        routers = self.nb_api.get_routers()
+        for router in routers:
+            if router.get_name() == self.router_id:
+                return True
+        return False
+
+
+class NetworkTestWrapper(object):
+
+    def __init__(self, neutron, nb_api):
+        self.network_id = None
+        self.neutron = neutron
+        self.nb_api = nb_api
+        self.deleted = False
+
+    def create(self):
+        network = {'name': 'mynetwork1', 'admin_state_up': True}
+        network = self.neutron.create_network({'network': network})
+        self.network_id = network['network']['id']
+        return self.network_id
+
+    def __del__(self):
+        if self.deleted or self.network_id is None:
+            return
+        self.delete()
+
+    def delete(self):
+        subnets = self.neutron.list_subnets(device_id=self.network_id)
+        subnets = subnets['subnets']
+        #for subnet in subnets:
+        #print("delete sub-network", subnet['id'], subnet)
+        #self.neutron.delete_subnet( subnet['id'] )
+        self.neutron.delete_network(self.network_id)
+        self.deleted = True
+
+    def exists(self):
+        network = self.nb_api.get_lswitch(self.network_id)
+        if network:
+            return True
+        return False
