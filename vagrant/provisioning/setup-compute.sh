ffd28c7a766d35d80c15b8c8044f388ddb877e30
Kyle Mestery <mestery@mestery.com>
2016-01-03 07:01:13 -0600

    Vagrant: Use the IP address from eth1
    Make sure to use the IP address from eth1 for all OpenStack services.
    This is the private IP address which is accessible from the host running
    Vagrant.
    
    Fixup the names of the two compute nodes to be independent.
    
    Change-Id: I8a62e8ba09bfd4321d36ea1f7013ea2188e11d19
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- vagrant/provisioning/setup-compute.sh
+++ vagrant/provisioning/setup-compute.sh
@@ -3,4 +3,18 @@ cp /dragonflow/doc/source/multi-node-conf/compute_node_local_controller.conf ~/d
 if [ "$1" != "" ]; then
     sed -i -e 's/<IP address of host running everything else>/'$1'/g' ~/devstack/local.conf
 fi
+
+# Get the IP address
+ipaddress=$(/sbin/ifconfig eth1 | grep 'inet addr' | awk -F' ' '{print $2}' | awk -F':' '{print $2}')
+
+# Adjust some things in local.conf
+cat << DEVSTACKEOF >> devstack/local.conf
+
+# Set this to the address of the main DevStack host running the rest of the
+# OpenStack services.
+Q_HOST=$1
+HOST_IP=$ipaddress
+HOSTNAME=$(hostname)
+DEVSTACKEOF
+
 ~/devstack/stack.sh
