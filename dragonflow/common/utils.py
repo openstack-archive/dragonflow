88f9d5cfbfbb8fefda825f103f9848c21a9109f3
Eran Gampel <eran@gampel.net>
2016-01-04 16:04:40 +0200

    Add support for DF built-in pub/sub
    Add zmq as the built in pub/sub mechanism.
    Added configuration use_df_pub_sub default to True
    If set to false will use the DB naitive Pub/Sub mechanisem
    if supported.
    This is the first patch covering Pluggability the Zmq driver
    next patch will cover the reliability issues
    
    partially implements blueprint: pubsub-module
    
    Co-Authored-By:Gal Sagie <gal.sagie@huawei.com>
    
    Change-Id: I6102f630a8766880d8e8b101f9c26b88e5c77a91

---
+++ dragonflow/common/utils.py
@@ -0,0 +1,67 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from oslo_log import log as logging
+from oslo_utils import importutils
+
+import eventlet
+from stevedore import driver
+import sys
+
+from neutron._i18n import _, _LE
+
+DF_PUBSUB_DRIVER_NAMESPACE = 'dragonflow.pubsub_driver'
+LOG = logging.getLogger(__name__)
+
+eventlet.monkey_patch()
+
+
+def load_driver(driver_cfg, namespace):
+    try:
+        # Try to resolve by alias
+        mgr = driver.DriverManager(namespace, driver_cfg)
+        class_to_load = mgr.driver
+    except RuntimeError:
+        e1_info = sys.exc_info()
+        # try with name
+        try:
+            class_to_load = importutils.import_class(driver_cfg)
+        except (ImportError, ValueError):
+            LOG.error(_LE("Error loading class by alias"),
+                      exc_info=e1_info)
+            LOG.error(_LE("Error loading class by class name"),
+                      exc_info=True)
+            raise ImportError(_("Class not found."))
+    return class_to_load()
+
+
+class DFDaemon(object):
+
+    def __init__(self):
+        super(DFDaemon, self).__init__()
+        self.pool = eventlet.GreenPool()
+        self.is_daemonize = False
+        self.thread = None
+
+    def daemonize(self, run):
+        if self.is_daemonize:
+            LOG.error(_LE("all ready daemonized"))
+            return
+        self.thread = self.pool.spawn_n(run)
+        eventlet.sleep(0)
+        self.is_daemonize = True
+        return self.thread
+
+    def stop(self):
+        if self.thread:
+            eventlet.greenthread.kill(self.thread)
+            eventlet.sleep(0)
