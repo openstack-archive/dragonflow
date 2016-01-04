de57172add51a576bd5b321323407d54118f2c7e
Eran Gampel <eran@gampel.net>
2016-01-04 16:04:40 +0200

    Add support for DF built-in pub/sub
    Add zmq as the built in pub/sub mechanism.
    Added configuration use_df_pub_sub default to True
    If set to false will use the DB naitive Pub/Sub mechanisem
    if supported.
    This is the first patch covering Pluggability the Zmq driver
    next patch will cover the reliability issues
    
    partially implements blueprint: pubsub-module
    
    Co-Authored-By:Gal Sagie <gal.sagie@huawei.com>
    
    Change-Id: I6102f630a8766880d8e8b101f9c26b88e5c77a91

---
+++ dragonflow/db/pub_sub_api.py
@@ -0,0 +1,233 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import abc
+import eventlet
+import msgpack
+import multiprocessing
+import six
+
+from oslo_log import log as logging
+
+from dargonflow._i18n import _LE
+
+LOG = logging.getLogger(__name__)
+
+eventlet.monkey_patch()
+
+
+@six.add_metaclass(abc.ABCMeta)
+class PubSubApi(object):
+
+    @abc.abstractmethod
+    def get_publisher(self):
+        """Return a Publisher Driver Object
+
+        :returns: an PublisherApi Object
+        """
+
+    @abc.abstractmethod
+    def get_subscriber(self):
+        """Return a Subscriber Driver Object
+
+        :returns: an PublisherApi Object
+        """
+
+
+@six.add_metaclass(abc.ABCMeta)
+class PublisherApi(object):
+
+    @abc.abstractmethod
+    def initialize(self, ip, is_neutron_server, publish_port, **args):
+        """Initialize the DB client
+
+        :param ip:      Publiser IP address
+        :type ip:       string
+        :param is_neutron_server: Publisher is part of the neutron server
+        :type is_neutron_server: boolean (True or False)
+        :param publish_port:    Publisher server port number
+        :type publisher_port:     int
+        :param args:       Additional args that were read from configuration
+                           file
+        :type args:        dictionary of <string, object>
+        :returns:          None
+        """
+
+    @abc.abstractmethod
+    def daemonize(self):
+        """Start the Publisher thread
+        """
+
+    @abc.abstractmethod
+    def run(self):
+        """Method that will run in the Publisher thread
+        """
+
+    @abc.abstractmethod
+    def stop(self):
+        """Stop the Publisher thread
+        """
+
+    @abc.abstractmethod
+    def send_event(self, update, topic):
+        """Publish the update
+
+        :param update:  Encapsulates a Publisher update
+        :type update:   DbUpdate object
+        :param topic:   topic to send event to
+        :type topic:    string
+        :returns:       None
+        """
+
+
+@six.add_metaclass(abc.ABCMeta)
+class SubscriberApi(object):
+
+    @abc.abstractmethod
+    def initialize(self, ip, callback, plugin_port, cont_port, **args):
+        """Initialize the DB client
+
+        :param ip:      Subscriber IP address
+        :type ip:       string
+        :param callback:  callback method to call for every db change
+        :type callback :  callback method of type:
+                          callback(table, key, action, value)
+                          table - table name
+                          key - object key
+                          action = 'create' / 'set' / 'delete' / 'sync'
+                          value = new object value
+        :param plugin_port: Subscription port for plugin Events
+        :type plugin_port:     int
+        :param controller_port: Subscription port for controllers Events
+        :type controller_port:     int
+        :param args:       Additional args that were read from configuration
+                           file
+        :type args:        dictionary of <string, object>
+        :returns:          None
+        """
+
+    @abc.abstractmethod
+    def run(self):
+        """Method that will run in the Subscriber thread
+        """
+
+    @abc.abstractmethod
+    def daemonize(self):
+        """Start the Subscriber thread
+        """
+
+    @abc.abstractmethod
+    def stop(self):
+        """Stop the Subscriber thread
+        """
+    @abc.abstractmethod
+    def add_topic(self, topic):
+        """Add a topic to the subsciber listening list
+
+        :param topic:  topic to listen to
+        :type topic:   string
+        """
+
+
+class PublisherAgentBase(PublisherApi):
+
+    def initialize(self, ip, is_neutron_server, publish_port=8866, **args):
+        self.ip = ip
+        self.port = publish_port
+        self.pub_socket = None
+        if is_neutron_server:
+            self._queue = multiprocessing.Queue()
+        else:
+            self._queue = eventlet.queue.PriorityQueue()
+
+        self.is_neutron_server = is_neutron_server
+        self.deamon = DFDeamon()
+
+    def pack_message(self, message):
+        data = None
+        try:
+            data = msgpack.packb(message, encoding='utf-8')
+        except Exception as e:
+            LOG.warning(e)
+        return data
+
+    def send_event(self, update, topic=b'D'):
+        if self.is_daemonize:
+            #NOTE(gampel)In this reference implementation we develop a trigger
+            #based pub sub without sending the value mainly in order to avoid
+            #consistency issues in th cost of extra latency i.e get
+            update.value = None
+            if not update.topic:
+                update.topic = topic
+            self._queue.put(update)
+            eventlet.sleep(0)
+
+    def daemonize(self):
+        self.deamon.daemonize(self.run)
+
+    def stop(self):
+        self.deamon.daemonize()
+
+
+class SubscriberAgentBase(SubscriberApi):
+
+    def __init__(self):
+        super(SubscriberAgentBase, self).__init__()
+        self.topic_list = []
+        self.topic_list.append(b'D')
+
+    def initialize(self, ip, callback, plugin_port=8866, cont_port=8867):
+        self.db_changes_callback = callback
+        self.ip = ip
+        self.plugin_updates_port = plugin_port
+        self.controllers_updates_port = cont_port
+        self.deamon = DFDeamon()
+
+    def unpack_message(self, message):
+        entry = None
+        try:
+            entry = msgpack.unpackb(message, encoding='utf-8')
+        except Exception as e:
+            LOG.warning(e)
+        return entry
+
+    def daemonize(self):
+        self.deamon.daemonize(self.run)
+
+    def stop(self):
+        self.deamon.daemonize()
+
+    def add_topic(self, topic):
+        self.topic_list.append(topic)
+
+
+class DFDeamon(object):
+
+    def __init__(self):
+        super(DFDeamon, self).__init__()
+        self.pool = eventlet.GreenPool()
+        self.is_daemonize = False
+        self.therad = None
+
+    def daemonize(self, run):
+        if self.is_daemonize:
+            LOG.error(_LE("all ready aemonize"))
+            return
+        self.thread = self.pool.spawn_n(run)
+        eventlet.sleep(0)
+        self.is_daemonize = True
+        return self.thread
+
+    def stop(self, thread):
+        if self.thread:
+            eventlet.greenthread.kill(self.thread)
+            eventlet.sleep(0)
