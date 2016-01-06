07ad22222578ce7ca2c55652b9a4692ec859673f
Yuli <yuli.stremovsky@huawei.com>
2016-01-06 17:20:20 +0200

    Add ovs flows beautifier script.
    Change-Id: I3d9cb7d72c02a81765c25ec369ee8731b22fda47

---
+++ tools/df-ovs-flow-dumper.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import re
+import sys
+import os_client_config
+from oslo_config import cfg
+from neutron.agent.common import utils
+from neutron.common import config as common_config
+from neutronclient.neutron import client
+from dragonflow.common import common_params
+
+cfg.CONF.register_opts(common_params.df_opts, 'df')
+
+EXPECTED_NUMBER_OF_FLOWS_AFTER_GATE_DEVSTACK = 26
+
+
+def get_cloud_config(cloud='devstack-admin'):
+    return os_client_config.OpenStackConfig().get_one_cloud(cloud=cloud)
+
+def credentials(cloud='devstack-admin'):
+    """Retrieves credentials to run functional tests"""
+    return get_cloud_config(cloud=cloud).get_auth_args()
+
+class OvsInfo():
+    def __init__(self):
+        creds = credentials()
+        tenant_name = creds['project_name']
+        auth_url = creds['auth_url'] + "/v2.0"
+        self.neutron = client.Client('2.0', username=creds['username'],
+             password=creds['password'], auth_url=auth_url,
+             tenant_name=tenant_name)
+
+    def _get_ovs_flows(self):
+        full_args = ["ovs-ofctl", "dump-flows", 'br-int', '-O Openflow13']
+        flows = utils.execute(full_args, run_as_root=True,
+                              process_input=None)
+        return flows
+
+    def _parse_ovs_flows(self, flows):
+        flow_list = flows.split("\n")[1:]
+        flows_as_dicts = []
+        for flow in flow_list:
+            if len(flow) == 0:
+                continue
+            fs = flow.split(' ')
+            res = {}
+            res['table'] = fs[3].split('=')[1]
+            res['match'] = fs[-2]
+            res['packets'] = fs[4].split('=')[1]
+            res['actions'] = fs[-1].split('=')[1]
+            res['cookie'] = fs[1].split('=')[1]
+            m = re.search('priority=(\d+)', res['match'])
+            if m:
+                res['priority'] = m.group(1)
+                res['match'] = re.sub(r'priority=(\d+),?', '', res['match'])
+            flows_as_dicts.append(res)
+        return flows_as_dicts
+
+    def list_flows(self):
+        flows = self._get_ovs_flows()
+        flows_list = self._parse_ovs_flows(flows)
+        flows_list2 = self._ovs_bitify_tables(flows_list)
+        return flows_list2
+
+    def _get_ovs_local_ports(self):
+        #full_args = ["ovs-dpctl", "show"]
+        full_args = ["ovs-ofctl", "show", 'br-int', '-O Openflow13']
+        ports = utils.execute(full_args, run_as_root=True,
+                              process_input=None)
+        return ports
+
+    def _parse_ovs_local_ports(self, ports):
+        port_list = ports.split("\n")[1:]
+        result = list()
+        for line in port_list:
+            if "addr:" in line:
+                line = line.strip()
+                m = re.search('\A(\w+)\(([-\w]+)\): addr:(.*?)\Z', line)
+                if m:
+                    result.append({'id': m.group(1), 'name': m.group(2), 'addr': m.group(3)})
+        return result
+
+    def _ovs_bitify_tables(self, flows):
+        tables = {'0': 'START', '9': 'APPCHECK', '10': 'ARPINJECT', '11': 'DHCPINJECT', '17': 'L2LOOKUP', '20': 'L3ROUTE', '64': 'OUTPUT' }
+        result = list()
+        for flow in flows:
+            if flow['table'].strip(',') in tables:
+                flow['table'] = tables[flow['table'].strip(',')]
+            m = re.search('goto_table:(\d+)', flow['actions'])
+            if m and m.group(1) in tables:
+                flow['actions'] = re.sub('goto_table:(\d+)','goto:'+tables[m.group(1)], flow['actions'])
+            m = re.search(r'resubmit\(,(\d+)\)', flow['actions'])
+            if m and m.group(1) in tables:
+                flow['actions'] = re.sub(r'resubmit\(,(\d+)\)', r'resubmit(,'+tables[m.group(1)]+')', flow['actions'])
+            flow['actions'] = re.sub(r'set_field:0x([0-9a-fA-F]+)[-]>metadata', r'NETWORK=0x\1', flow['actions'])
+            flow['match'] = re.sub(r'metadata=0x([0-9a-fA-F]+)', r'NETWORK=0x\1', flow['match'])
+            flow['actions'] = re.sub(r'set_field:0x([0-9a-fA-F]+)->reg6', r'STAG=0x\1', flow['actions'])
+            flow['match'] = re.sub(r'reg6=0x([0-9a-fA-F]+)', r'STAG=0x\1', flow['match'])
+            flow['actions'] = re.sub(r'set_field:0x([0-9a-fA-F]+)->reg7', r'DTAG=0x\1', flow['actions'])
+            flow['match'] = re.sub(r'reg7=0x([0-9a-fA-F]+)', r'DTAG=0x\1', flow['match'])
+            flow['match'] = re.sub('tun_id=', 'VTAG=', flow['match'])
+            result.append(flow)
+        return result
+
+
+    def ovs_list_ports(self):
+        ports = self._get_ovs_local_ports()
+        ports = self._parse_ovs_local_ports(ports)
+        return ports
+
+    def print_ovs_flow_row(self, row):
+        if 'cookie' in row and row['cookie'] == '0x0,':
+            row.pop('cookie')
+        if row['packets'] == '0,':
+            row.pop('packets')
+        priority = row['priority']
+        actions = row['actions']
+        table = row['table']
+        match = row['match']
+        row.pop('priority')
+        row.pop('actions')
+        row.pop('table')
+        row.pop('match')
+        if len(row) == 0:
+            row = ""
+        print("%-10s | %-3s | %-50s | %-40s | %s" % (table, priority, match, actions, row))
+
+    def print_data(self):
+        ports = self.neutron.list_ports(retrieve_all=True)
+        ports = ports['ports']
+        neutron_ports = dict()
+        for port in ports:
+            id = port['id'][0:11]
+            neutron_ports[id] = port
+        networks = self.neutron.list_networks()
+        subnets = dict()
+        networks = networks['networks']
+        for network in networks:
+            subnets2 = network['subnets']
+            for subnet in subnets2:
+                subnets[subnet] = {'name': network['name'], 'router:external': network['router:external']}
+        ovs_ports = self.ovs_list_ports()
+        for ovs_port in ovs_ports:
+            id = ovs_port['name'][3:]
+            if id in neutron_ports:
+                info = neutron_ports[id]
+                netinfo = list()
+                for subnet in info['fixed_ips']:
+                    if 'subnet_id' in subnet and subnet['subnet_id'] in subnets:
+                            z = subnet.copy()
+                            z.update(subnets[subnet['subnet_id']])
+                            netinfo.append(z)
+                    else:
+                        netinfo.append(subnet) # just copy fixed_ip array
+                print("in_port_"+ovs_port['id'], ovs_port['name'], ovs_port['addr'], info['name'], info['device_owner'], info['mac_address'], netinfo)
+                print(info)
+            else:
+                print("in_port_"+ovs_port['id'], ovs_port['name'], ovs_port['addr'], '-----------------')
+        flows = self.list_flows()
+        print("=" * 130)
+        print("TABLE:     | PRI | PATTERN                                            | ACTION                                   | More")
+        print("=" * 130)
+        for flow in flows:
+            self.print_ovs_flow_row(flow)
+
+def main():
+    c = OvsInfo()
+    c.print_data()
+
+if __name__ == '__main__':
+    sys.exit(main())
