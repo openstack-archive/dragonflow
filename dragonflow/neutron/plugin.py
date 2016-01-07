c7b50108755cf71206fbefacbb5a981f0cd8fc44
ZhaoBo <zhaobo6@huawei.com>
2016-01-07 09:10:00 +0800

    Not allow delete subnet gateway port
    There is no check about delete gateway port. User can delete it through
    API directly.
    
    So this patch add the check like ml2.
    
    Change-Id: Ibe4223e7801c44d632d3cc4adfc5953dfdd5f364
    Closes-Bug: #1531681

--- dragonflow/neutron/plugin.py
+++ dragonflow/neutron/plugin.py
@@ -27,6 +27,7 @@ from neutron.api.rpc.handlers import l3_rpc
 from neutron.api.rpc.handlers import metadata_rpc
 from neutron.api.v2 import attributes as attr
 from neutron.callbacks import events
+from neutron.callbacks import exceptions
 from neutron.callbacks import registry
 from neutron.callbacks import resources
 from neutron.common import exceptions as n_exc
@@ -456,7 +457,31 @@ class DFPlugin(db_base_plugin_v2.NeutronDbPluginV2,
 
         return port
 
+    def _pre_delete_port(self, context, port_id, port_check):
+        """Do some preliminary operations before deleting the port."""
+        LOG.debug("Deleting port %s", port_id)
+        if not port_check:
+            return
+        try:
+            port = self.get_port(context, port_id)
+        except n_exc.PortNotFound:
+            return
+        if port['device_owner'] in ['network:router_interface',
+                                    'network:router_gateway',
+                                    'network:floatingip']:
+            fixed_ips = port['fixed_ips']
+            if fixed_ips:
+                reason = _('has device owner %s') % port['device_owner']
+                raise n_exc.ServicePortInUse(port_id=port['id'],
+                                             reason=reason)
+            else:
+                LOG.debug("Port %(port_id)s has owner %(port_owner)s, but "
+                          "no IP address, so it can be deleted",
+                          {'port_id': port['id'],
+                           'port_owner': port['device_owner']})
+
     def delete_port(self, context, port_id, l3_port_check=True):
+        self._pre_delete_port(context, port_id, l3_port_check)
         try:
             self.nb_api.delete_lport(port_id)
         except df_exceptions.DBKeyNotFound:
