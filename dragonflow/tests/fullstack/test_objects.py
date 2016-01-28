b29d0787f9cfb2d9a9d96836dfbfdf43cc18c410
Yuli <yuli.stremovsky@huawei.com>
2016-01-28 17:00:31 +0200

    Add new class to handle VM creation/destruction during tests
    Change-Id: Id6bd459b46e60531197aed6a3276f73e7a64ea19

--- dragonflow/tests/fullstack/test_objects.py
+++ dragonflow/tests/fullstack/test_objects.py
@@ -78,3 +78,62 @@ class NetworkTestWrapper(object):
         if network:
             return True
         return False
+
+
+class VMTestWrapper(object):
+
+    def __init__(self, parent):
+        self.server = None
+        self.deleted = False
+        self.parent = parent
+        creds = test_base.credentials()
+        auth_url = creds['auth_url'] + "/v2.0"
+        self.nova = novaclient.Client('2', creds['username'],
+                        creds['password'], 'demo', auth_url)
+
+    def create(self, script=None):
+        image = self.nova.images.find(name="cirros-0.3.4-x86_64-uec")
+        self.parent.assertIsNotNone(image)
+        flavor = self.nova.flavors.find(name="m1.tiny")
+        self.parent.assertIsNotNone(flavor)
+        network = self.nova.networks.find(label='private')
+        self.parent.assertIsNotNone(network)
+        nics = [{'net-id': network.id}]
+        self.server = self.nova.servers.create(name='test', image=image.id,
+                           flavor=flavor.id, nics=nics, user_data=script)
+        self.parent.assertIsNotNone(self.server)
+        self.server_id = self.server.id
+        server_is_ready = self._waite_for_server_ready(30)
+        self.parent.assertTrue(server_is_ready)
+        return self.server.id
+
+    def _waite_for_server_ready(self, timeout):
+        if self.server is None:
+            return False
+        while timeout > 0:
+            server = self.nova.servers.find(id=self.server.id)
+            if server is not None and server.status == 'ACTIVE':
+                return True
+            time.sleep(1)
+            timeout = timeout - 1
+        return False
+
+    def __del__(self):
+        if self.deleted or self.server is None:
+            return
+        self.delete()
+
+    def delete(self):
+        self.nova.servers.delete(self.server)
+        self.deleted = True
+
+    def exists(self):
+        if self.server is None:
+            return False
+        server = self.nova.servers.find(id=self.server.id)
+        if server is None:
+            return False
+        return True
+
+    def dump(self):
+        return self.nova.servers.get_console_output(self.server)
