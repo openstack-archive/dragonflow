631f15dcc759ca8dc19b045e0b22c14e673dfb2a
Yuli <yuli.stremovsky@huawei.com>
2016-01-28 15:48:25 +0200

    Add new OVS test for DHCP application pipeline
    Change-Id: If23519f39a8ebd308c968847355c4aaa73a23e45

--- dragonflow/tests/fullstack/test_objects.py
+++ dragonflow/tests/fullstack/test_objects.py
@@ -10,6 +10,9 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from neutron.agent.common import utils
+import re
+
 
 class RouterTestWrapper(object):
 
@@ -78,3 +81,39 @@ class NetworkTestWrapper(object):
         if network:
             return True
         return False
+
+
+class OvsTestWarapper(object):
+
+    def _get_ovs_flows(self):
+        full_args = ["ovs-ofctl", "dump-flows", 'br-int', '-O Openflow13']
+        flows = utils.execute(full_args, run_as_root=True,
+                              process_input=None)
+        return flows
+
+    def _parse_ovs_flows(self, flows):
+        flow_list = flows.split("\n")[1:]
+        flows_as_dicts = []
+        for flow in flow_list:
+            if len(flow) == 0:
+                continue
+            fs = flow.split(' ')
+            res = {}
+            res['table'] = fs[3].split('=')[1]
+            res['match'] = fs[-2]
+            res['actions'] = fs[-1].split('=')[1]
+            res['cookie'] = fs[1].split('=')[1]
+            m = re.search('priority=(\d+)', res['match'])
+            if m:
+                res['priority'] = m.group(1)
+                res['match'] = re.sub(r'priority=(\d+),?', '', res['match'])
+            flows_as_dicts.append(res)
+        return flows_as_dicts
+
+    def diff_flows(self, list1, list2):
+        result = [v for v in list2 if v not in list1]
+        return result
+
+    def dump(self):
+        flows = self._get_ovs_flows()
+        return self._parse_ovs_flows(flows)
