5c381d13d6c45c36a60dc071a58f04c7b4d7a786
Omer Anson <omer.anson@toganetworks.com>
2016-02-04 10:33:51 +0200

    Add wait to DFDaemon class.
    Add the method wait to the DFDaemon class. This will allow threads to
    wait for one another to finish.
    
    This is useful in case one threads needs to wait for another thread to
    finish a task before continuing.
    
    Change-Id: I6972477f38e1980d0a8bfea6072d2d5410872fef

--- dragonflow/common/utils.py
+++ dragonflow/common/utils.py
@@ -18,6 +18,7 @@ from stevedore import driver
 import sys
 
 from dragonflow._i18n import _, _LE
+import greenlet
 
 DF_PUBSUB_DRIVER_NAMESPACE = 'dragonflow.pubsub_driver'
 LOG = logging.getLogger(__name__)
@@ -68,3 +69,14 @@ class DFDaemon(object):
             eventlet.sleep(0)
             self.thread = None
             self.is_daemonize = False
+
+    def wait(self, timeout=None):
+        if timeout and timeout > 0:
+            timeout_obj = eventlet.Timeout(timeout)
+        try:
+            self.thread.wait()
+        except greenlet.GreenletExit:
+            return True  # Good news
+        finally:
+            if timeout_obj:
+                timeout_obj.cancel()
