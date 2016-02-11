1f5afbe53c01ba4bb6546bd6b1fdec222ef43952
Eran Gampel <eran@gampel.net>
2016-02-11 09:36:13 +0200

    Add polling for the chassis table on the neutron server
    In the first phase we will not add publishing on the ADF controller
    The new chassis detection and publishing will be done on the neutron
    server side and published using the neutron publisher
    
    Change-Id: I58eb08e186fbd00a1d0433cd9b15e8bff01b900c

--- dragonflow/db/pub_sub_api.py
+++ dragonflow/db/pub_sub_api.py
@@ -18,6 +18,8 @@ import six
 from oslo_log import log as logging
 
 from dragonflow.common import utils as df_utils
+from oslo_serialization import jsonutils
+
 from dragonflow.db import db_common
 
 LOG = logging.getLogger(__name__)
@@ -176,7 +178,7 @@ class PublisherAgentBase(PublisherApi):
 
     @property
     def is_daemonize(self):
-        return self.daemon.is_daemonize
+        self.daemon.is_daemonize
 
     def stop(self):
         self.daemon.stop()
@@ -220,3 +222,39 @@ class SubscriberAgentBase(SubscriberApi):
 
     def unregister_topic(self, topic):
         self.topic_list.remove(topic)
+
+
+class ChassisPublisher(object):
+
+    def __init__(self, driver, publisher, polling_time=10):
+        super(ChassisPublisher, self).__init__()
+        self.driver = driver
+        self.daemon = df_utils.DFDaemon()
+        self.chassis = {}
+        self.polling_time = polling_time
+        self.publisher = publisher
+
+    def daemonize(self):
+        self.daemon.daemonize(self.run)
+
+    def stop(self):
+        self.daemon.stop()
+
+    def run(self):
+        eventlet.sleep(0)
+        while True:
+            eventlet.sleep(self.polling_time)
+            all_chassis = self.driver.get_all_entries('chassis')
+            for chassis_unicode in all_chassis:
+                chassis = jsonutils.loads(chassis_unicode)
+                chassis_name = chassis['name']
+                if chassis_name not in self.chassis:
+                    self.chassis[chassis_name] = chassis
+                    update = db_common.DbUpdate(
+                                        'chassis',
+                                        chassis_name,
+                                        'create',
+                                        chassis)
+                    self.publisher.send_event(update)
+                    #TODO(gampel)make the sleep time random
+                    eventlet.sleep(0.3)
