bb4d670188b6a039576481b04e9a6297ec966bbb
Omer Anson <omer.anson@toganetworks.com>
2016-02-16 15:26:16 +0200

    Make pack and unpack message utility methods
    Change-Id: Id941aff85c2379680ae03a30cb064788ad27898f

--- dragonflow/db/pub_sub_api.py
+++ dragonflow/db/pub_sub_api.py
@@ -26,6 +26,24 @@ LOG = logging.getLogger(__name__)
 eventlet.monkey_patch(socket=False)
 
 
+def pack_message(self, message):
+    data = None
+    try:
+        data = msgpack.packb(message, encoding='utf-8')
+    except Exception as e:
+        LOG.warning(e)
+    return data
+
+
+def unpack_message(self, message):
+    entry = None
+    try:
+        entry = msgpack.unpackb(message, encoding='utf-8')
+    except Exception as e:
+        LOG.warning(e)
+    return entry
+
+
 @six.add_metaclass(abc.ABCMeta)
 class PubSubApi(object):
 
@@ -171,14 +189,6 @@ class PublisherAgentBase(PublisherApi):
         self.daemon = df_utils.DFDaemon()
         self.config = config
 
-    def pack_message(self, message):
-        data = None
-        try:
-            data = msgpack.packb(message, encoding='utf-8')
-        except Exception as e:
-            LOG.warning(e)
-        return data
-
     def daemonize(self):
         self.daemon.daemonize(self.run)
 
@@ -205,14 +215,6 @@ class SubscriberAgentBase(SubscriberApi):
 
     def register_listen_address(self, uri):
         self.uri_list.append(uri)
-
-    def unpack_message(self, message):
-        entry = None
-        try:
-            entry = msgpack.unpackb(message, encoding='utf-8')
-        except Exception as e:
-            LOG.warning(e)
-        return entry
 
     def daemonize(self):
         self.daemon.daemonize(self.run)
