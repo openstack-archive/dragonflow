06485d9fd4d2d2412187de85ec678054a07baa2d
Li Ma <skywalker.nick@gmail.com>
2016-02-18 10:30:42 +0800

    [WIP] Implement DB consistency
    NOTE: under development.
    
    1. Introduce a lock primitives
    It is a distributed lock based on SQL. Each lock is
    associated with a Neutron object and a given API session.
    It ensures that a lock is acquired and released in the
    same API context.
    
    2. Introduce a versioned objects method:
    (1) Each object has a version parameter.
    (2) Each update increments the version.
    (3) The version is stored in Neutron DB and
    also the DF DB.
    
    In the API context, when Neutron updates an
    object, the corresponding version is incremented.
    
    It acquires the lock, calls NB-API operations
    and also increments the version.
    Finally, it releases the lock after everything is done.
    
    If the Neutron DB operation is failed, it will re-try.
    And moreover, we can sync the object from Neutron DB.
    
    Related-Bug: #1529812
    Related-Bug: #1529326
    Related-Bug: #1527234
    Related-Bug: #1497676
    Partially-Implements: blueprint bp/keep-db-consistency
    Change-Id: Iff916481282f2d60df66c0e916f3045f9944531e

---
+++ dragonflow/db/neutron/lockedobjects_db.py
@@ -0,0 +1,148 @@
+# Copyright (c) 2015 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import random
+
+from sqlalchemy import func
+from sqlalchemy.orm import exc as orm_exc
+
+from dragonflow._i18n import _LI
+from dragonflow.db.neutron import models
+
+from neutron.db import api as db_api
+
+from oslo_db import api as oslo_db_api
+from oslo_log import log
+
+
+# Used to identify each API session
+LOCK_SEED = 9876543210
+
+
+LOG = log.getLogger(__name__)
+
+
+class DFDBLock(object):
+    def __init__(self, context, id):
+        self.context = context
+        self.id = id
+
+    def __enter__(self):
+        self.session_id = _acquire_lock(self.context, self.id)
+
+    def __exit__(self, type, value, traceback):
+        _release_lock(self.context, self.id, self.session_id)
+
+
+def create_lock(context, oid):
+    try:
+        session = db_api.get_session()
+        with session.begin():
+            _create_db_row(session, oid=oid)
+    except orm_exc.MultipleResultsFound as e:
+        LOG.warning(e)
+
+
+def delete_lock(context, oid):
+    try:
+        session = db_api.get_session()
+        with session.begin():
+            _delete_db_row(session, oid=oid)
+    except orm_exc.NoResultFound as e:
+        LOG.warning(e)
+
+
+@oslo_db_api.wrap_db_retry(max_retries=db_api.MAX_RETRIES,
+                           retry_interval=1,
+                           inc_retry_interval=True,
+                           max_retry_interval=10,
+                           retry_on_deadlock=True,
+                           retry_on_request=True)
+def _acquire_lock(context, oid):
+    sid = _generate_session_id()
+    # NOTE(nick-ma-z): we disallow subtransactions because the
+    # retry logic will bust any parent transactions
+    session = db_api.get_session()
+    with session.begin():
+        LOG.info(_LI("Try to get lock for object %(oid)s in "
+                     "session %(sid)s."), {'oid': oid, 'sid': sid})
+        row = _get_object_with_lock(session, oid, False)
+        _update_lock(session, row, True, oid, session_id=sid)
+        LOG.info(_LI("Lock is acquired for object %(oid)s in "
+                     "session %(sid)s."), {'oid': oid, 'sid': sid})
+        return sid
+
+
+@oslo_db_api.wrap_db_retry(max_retries=db_api.MAX_RETRIES,
+                           retry_interval=1,
+                           inc_retry_interval=True,
+                           max_retry_interval=10,
+                           retry_on_deadlock=True,
+                           retry_on_request=True)
+def _release_lock(context, oid, sid):
+    # NOTE(nick-ma-z): we disallow subtransactions because the
+    # retry logic will bust any parent transactions
+    session = db_api.get_session()
+    with session.begin():
+        LOG.info(_LI("Try to get lock for object %(oid)s in "
+                     "session %(sid)s."), {'oid': oid, 'sid': sid})
+        row = _get_object_with_lock(session, oid, True,
+                                    session_id=sid)
+        _update_lock(session, row, False, oid, session_id=0)
+        LOG.info(_LI("Lock is released for object %(oid)s in "
+                     "session %(sid)s."), {'oid': oid, 'sid': sid})
+
+
+def _generate_session_id():
+    return random.randint(0, LOCK_SEED)
+
+
+def _get_all_db_rows(session):
+    return session.query(models.DFLockedObjects).all()
+
+
+def _get_object_with_lock(session, id, state, session_id=None):
+    if session_id:
+        return session.query(models.DFLockedObjects).filter_by(
+            object_uuid=id,
+            lock=state,
+            session_id=session_id).with_for_update().first()
+    else:
+        return session.query(models.DFLockedObjects).filter_by(
+            object_uuid=id, lock=state).with_for_update().first()
+
+
+def _update_lock(session, row, lock, oid, session_id):
+    row.lock = lock
+    row.session_id = session_id
+    session.merge(row)
+    session.flush()
+
+
+def _delete_db_row(session, row=None, oid=None):
+    if oid:
+        row = session.query(models.DFLockedObjects).filter_by(
+            object_uuid=oid).one()
+    if row:
+        session.delete(row)
+        session.flush()
+
+
+def _create_db_row(session, oid):
+    row = models.DFLockedObjects(object_uuid=oid,
+                                 lock=False, session_id=0,
+                                 created_at=func.now())
+    session.add(row)
+    session.flush()
---
+++ dragonflow/db/versioned_obj.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2015 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from dragonflow.common import exceptions as df_exceptions
+
+from oslo_log import log
+
+
+LOG = log.getLogger(__name__)
+
+
+def compare_and_increment_version(oid, json_obj, version):
+    if oid and json_obj and 'version' in json_obj:
+        LOG.debug("Neutron Object to be compared: %s, "
+                  "version is %s" % (json_obj, str(version)))
+        result = _compare_version(json_obj, version)
+        if result == 0:
+            return _increment_version(json_obj)
+        elif result < 0:
+            return _restore_version(json_obj, version)
+
+    # The only reason that throws this exception is that
+    # the object version stored in DF DB is higher than
+    # it in Neutron DB, which means that the object in
+    # DF DB is out-of-sync.
+    raise df_exceptions.DBKeyBadVersionException(id=oid)
+
+
+def create_version(json_obj):
+    if json_obj:
+        json_obj['version'] = 0
+    return json_obj
+
+
+def _increment_version(json_obj):
+    json_obj['version'] = int(json_obj['version']) + 1
+    return json_obj
+
+
+def _restore_version(json_obj, version):
+    json_obj['version'] = int(version) + 1
+    return json_obj
+
+
+def _compare_version(json_obj, version):
+    return cmp(int(json_obj['version']), int(version))
