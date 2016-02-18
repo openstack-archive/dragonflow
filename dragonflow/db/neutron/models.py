ccc86e0b72c82034e9ca50d5b5ecaf799047b90d
Li Ma <skywalker.nick@gmail.com>
2016-02-18 10:30:42 +0800

    Implement DB consistency
    It is a distributed lock based on SQL. Each lock is
    associated with a Neutron project(tenant) and a given
    API session. It ensures that a lock is acquired and
    released in the same API context.
    
    The detailed description is in the spec review.
    https://review.openstack.org/268005
    
    Closes-Bug: #1529812
    Closes-Bug: #1529326
    Closes-Bug: #1497676
    Related-Bug: #1527234
    Implements: blueprint bp/keep-db-consistency
    
    Change-Id: Iff916481282f2d60df66c0e916f3045f9944531e

---
+++ dragonflow/db/neutron/models.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2015 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import sqlalchemy as sa
+
+from neutron.db import model_base
+
+
+class DFLockedObjects(model_base.BASEV2):
+    __tablename__ = 'dflockedobjects'
+
+    object_uuid = sa.Column(sa.String(36), primary_key=True)
+    lock = sa.Column(sa.Boolean, default=False)
+    session_id = sa.Column(sa.BigInteger, default=0)
+    created_at = sa.Column(sa.DateTime)
