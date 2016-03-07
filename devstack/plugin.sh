2737b907e56ce45cd0a83103c0f1ab30962b1699
Li Ma <skywalker.nick@gmail.com>
2016-03-07 13:45:49 +0800

    Support db clean-up in df-db script and unstack
    The problem for unstack is that it doesn't clean
    all the data in df db. It makes debugging more difficult
    because the useless data are always increased and fill up
    the df db for each stack.sh deployment.
    
    This commit adds a cleanup function in df-db script and
    calls it in unstack.
    
    Change-Id: Ic706c1c71af86355dbc24395b974f3ff1efc35e8
    Closes-Bug: #1548437

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -381,6 +381,10 @@ function stop_pubsub_service {
     stop_process df-publisher-service
 }
 
+function cleanup_database {
+    df-db clean
+}
+
 # main loop
 if [[ "$Q_ENABLE_DRAGONFLOW_LOCAL_CONTROLLER" == "True" ]]; then
     if [[ "$1" == "stack" && "$2" == "install" ]]; then
@@ -424,5 +428,6 @@ if [[ "$Q_ENABLE_DRAGONFLOW_LOCAL_CONTROLLER" == "True" ]]; then
         if [[ "$DF_PUB_SUB" == "True" ]]; then
             stop_pubsub_service
         fi
+        cleanup_database
     fi
 fi
