4a2b93fd9edb4f513225f57e5429baa81a22b7d5
Fei Rao <milo.frao@gmail.com>
2016-03-06 22:21:44 -0500

    The implementation of distribute-DNAT
    This patch implement the distributed DNAT of blueprint distributed fip. The
    first stage only implements vxlan network. The change as below
    1. A df-l3-agent will be used to replace neutron-l3-agent.
    2. A new dnat app will be loaded to dragonflow controller.
    
    The next stage will support
    1. integrate with security group
    Current implement will not jump to SG processing due to SG is not ready.
    
    2. vlan network
    
    Change-Id: I57accfe5932b017de8e8384b4c302439ffd088e1

---
+++ dragonflow/cmd/eventlet/df_l3_agent.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# PBR Generated from u'console_scripts'
+
+from dragonflow.neutron.agent import df_l3_agent
+
+
+def main():
+    df_l3_agent.main()
--- dragonflow/controller/ryu_base_app.py
+++ dragonflow/controller/ryu_base_app.py
@@ -36,12 +36,13 @@ class RyuDFAdapter(OFPHandler):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
     OF_AUTO_PORT_DESC_STATS_REQ_VER = 0x04
 
-    def __init__(self, db_store=None):
-        super(RyuDFAdapter, self).__init__(db_store=db_store)
+    def __init__(self, **kwargs):
+        super(RyuDFAdapter, self).__init__(**kwargs)
         self.dispatcher = AppDispatcher('dragonflow.controller',
                 cfg.CONF.df.apps_list)
-        self.db_store = db_store
+        self.db_store = kwargs['db_store']
         self._datapath = None
+        self.kwargs = kwargs
         self.table_handlers = {}
 
     @property
@@ -50,7 +51,7 @@ class RyuDFAdapter(OFPHandler):
 
     def start(self):
         super(RyuDFAdapter, self).start()
-        self.load(self, db_store=self.db_store)
+        self.load(self, **self.kwargs)
         self.wait_until_ready()
 
     def load(self, *args, **kwargs):
@@ -109,6 +110,12 @@ class RyuDFAdapter(OFPHandler):
         self.dispatcher.dispatch('remove_security_group_rule',
                 secgroup=secgroup,
                 secgroup_rule=secgroup_rule)
+
+    def notify_associate_floatingip(self, floatingip):
+        self.dispatcher.dispatch('associate_floatingip', floatingip)
+
+    def notify_disassociate_floatingip(self, floatingip):
+        self.dispatcher.dispatch('disassociate_floatingip', floatingip)
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
