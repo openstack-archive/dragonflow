fb5bd081abcacdc875ba87e879b8ee6c78d9609d
Omer Anson <omer.anson@toganetworks.com>
2016-03-07 15:08:40 +0200

    On error when handling DB event do a full sync
    Previous implementation caused infinite loop. This fixes that issue, and
    the related bug.
    
    Change-Id: I51e61c9af4bf60137fb1d2107e8d3b79125cb351
    Closes-Bug: 1537759

--- dragonflow/db/api_nb.py
+++ dragonflow/db/api_nb.py
@@ -16,7 +16,6 @@
 
 import eventlet
 import netaddr
-import time
 
 from oslo_config import cfg
 from oslo_log import log
@@ -38,7 +37,6 @@ class NbApi(object):
         self.driver = db_driver
         self.controller = None
         self._queue = eventlet.queue.PriorityQueue()
-        self.db_apply_failed = False
         self.use_pubsub = use_pubsub
         self.publisher = None
         self.is_neutron_server = is_neutron_server
@@ -140,9 +138,8 @@ class NbApi(object):
 
     def _read_db_changes_from_queue(self):
         while True:
-            if not self.db_apply_failed:
-                self.next_update = self._queue.get(block=True)
-                LOG.debug("Event update: %s", self.next_update)
+            self.next_update = self._queue.get(block=True)
+            LOG.debug("Event update: %s", self.next_update)
             try:
                 value = self.next_update.value
                 if not value and self.next_update.action != 'delete':
@@ -154,13 +151,11 @@ class NbApi(object):
                                      self.next_update.key,
                                      self.next_update.action,
                                      value)
-                self.db_apply_failed = False
                 self._queue.task_done()
             except Exception as e:
                 if "ofport is 0" not in e.message:
-                    LOG.warning(e)
-                self.db_apply_failed = True
-                time.sleep(1)
+                    LOG.exception(e)
+                self.apply_db_change(None, None, 'sync', None)
 
     def apply_db_change(self, table, key, action, value):
         if action == 'sync':
