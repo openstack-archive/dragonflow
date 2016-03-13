b20bb62e18bbd4173c4ffbe67b67e6b384d49fbe
Omer Anson <omer.anson@toganetworks.com>
2016-03-13 12:58:33 +0200

    Add DHCP applicative testing
    Add an applicative testing where a DHCP client gets an IP automatically.
    
    Change-Id: I8360350c31dd1af6587ada5bdb5ab67d1d88b12b

--- dragonflow/tests/fullstack/test_apps.py
+++ dragonflow/tests/fullstack/test_apps.py
@@ -159,3 +159,217 @@ class TestArpResponder(test_base.DFTestBase):
         self.policy.wait(30)
         if len(self.policy.exceptions) > 0:
             raise self.policy.exceptions[0]
+
+
+class TestDHCPApp(test_base.DFTestBase):
+
+    def setUp(self):
+        super(TestDHCPApp, self).setUp()
+        self.topology = None
+        self.policy = None
+        try:
+            self.topology = self.store(
+                app_testing_objects.Topology(
+                    self.neutron,
+                    self.nb_api
+                )
+            )
+            self.subnet1 = self.topology.create_subnet(cidr='192.168.11.0/24')
+            self.port1 = self.subnet1.create_port()
+            self.port2 = self.subnet1.create_port()
+            time.sleep(test_utils.DEFAULT_CMD_TIMEOUT)
+            # Create policy
+            dhcp_packet = self._create_dhcp_discover()
+            send_dhcp_offer = app_testing_objects.SendAction(
+                self.subnet1.subnet_id,
+                self.port1.port_id,
+                str(dhcp_packet)
+            )
+            port_policies = self._create_port_policies()
+            self.policy = self.store(
+                app_testing_objects.Policy(
+                    initial_actions=[send_dhcp_offer],
+                    port_policies=port_policies,
+                    unknown_port_action=app_testing_objects.IgnoreAction()
+                )
+            )
+        except Exception:
+            if self.topology:
+                self.topology.close()
+            raise
+
+    def _create_udp_packet_for_dhcp(self,
+            dst_mac="ff:ff:ff:ff:ff:ff",
+            src_ip='0.0.0.0',
+            dst_ip='255.255.255.255'):
+        ethernet = ryu.lib.packet.ethernet.ethernet(
+            src=self.port1.port.get_logical_port().get_mac(),
+            dst=dst_mac,
+            ethertype=ryu.lib.packet.ethernet.ether.ETH_TYPE_IP,
+        )
+        ip = ryu.lib.packet.ipv4.ipv4(
+            src=src_ip,
+            dst=dst_ip,
+            proto=ryu.lib.packet.ipv4.inet.IPPROTO_UDP,
+        )
+        udp = ryu.lib.packet.udp.udp(
+            src_port=68,
+            dst_port=67,
+        )
+        result = ryu.lib.packet.packet.Packet()
+        result.add_protocol(ethernet)
+        result.add_protocol(ip)
+        result.add_protocol(udp)
+        return result
+
+    def _create_dhcp_discover(self):
+        result = self._create_udp_packet_for_dhcp()
+        options = [
+            ryu.lib.packet.dhcp.option(
+                ryu.lib.packet.dhcp.DHCP_MESSAGE_TYPE_OPT,
+                chr(ryu.lib.packet.dhcp.DHCP_DISCOVER),
+            ),
+            ryu.lib.packet.dhcp.option(
+                ryu.lib.packet.dhcp.DHCP_PARAMETER_REQUEST_LIST_OPT,
+                chr(ryu.lib.packet.dhcp.DHCP_GATEWAY_ADDR_OPT),
+            ),
+        ]
+        dhcp = ryu.lib.packet.dhcp.dhcp(
+            op=1,
+            chaddr=self.port1.port.get_logical_port().get_mac(),
+            options=ryu.lib.packet.dhcp.options(option_list=options),
+        )
+        result.add_protocol(dhcp)
+        result.serialize()
+        return result.data
+
+    def _create_dhcp_request(self, offer_buf, is_renewal=False):
+        pkt = ryu.lib.packet.packet.Packet(offer_buf)
+        offer = pkt.get_protocol(ryu.lib.packet.dhcp.dhcp)
+        self.assertEqual(
+            self.port1.port.get_logical_port().get_ip(),
+            offer.yiaddr
+        )
+        if is_renewal:
+            ether = pkt.get_protocol(ryu.lib.packet.ethernet.ethernet)
+            ip = pkt.get_protocol(ryu.lib.packet.ipv4.ipv4)
+            dst_mac = ether.src
+            dst_ip = ip.src
+            src_ip = self.port1.port.get_logical_port().get_ip()
+            result = self._create_udp_packet_for_dhcp(
+                dst_mac=dst_mac,
+                src_ip=src_ip,
+                dst_ip=dst_ip
+            )
+        else:
+            result = self._create_udp_packet_for_dhcp()
+        options = [
+            ryu.lib.packet.dhcp.option(
+                ryu.lib.packet.dhcp.DHCP_MESSAGE_TYPE_OPT,
+                chr(ryu.lib.packet.dhcp.DHCP_REQUEST),
+            ),
+            ryu.lib.packet.dhcp.option(
+                ryu.lib.packet.dhcp.DHCP_REQUESTED_IP_ADDR_OPT,
+                offer.yiaddr,
+            ),
+            ryu.lib.packet.dhcp.option(
+                ryu.lib.packet.dhcp.DHCP_PARAMETER_REQUEST_LIST_OPT,
+                chr(ryu.lib.packet.dhcp.DHCP_GATEWAY_ADDR_OPT),
+            ),
+        ]
+        dhcp = ryu.lib.packet.dhcp.dhcp(
+            op=1,
+            chaddr=self.port1.port.get_logical_port().get_mac(),
+            xid=offer.xid,
+            options=ryu.lib.packet.dhcp.options(option_list=options),
+        )
+        result.add_protocol(dhcp)
+        result.serialize()
+        return result.data
+
+    def _create_dhcp_renewal_request(self, offer_buf):
+        return self._create_dhcp_request(offer_buf, is_renewal=True)
+
+    def _create_port_policies(self):
+        ignore_action = app_testing_objects.IgnoreAction()
+        key1 = (self.subnet1.subnet_id, self.port1.port_id)
+        rules1 = [
+            app_testing_objects.PortPolicyRule(
+                # Detect dhcp offer
+                app_testing_objects.RyuDHCPOfferFilter(),
+                actions=[
+                    app_testing_objects.SendAction(
+                        self.subnet1.subnet_id,
+                        self.port1.port_id,
+                        self._create_dhcp_request
+                    ),
+                    app_testing_objects.DisableRuleAction(),
+                ]
+            ),
+            app_testing_objects.PortPolicyRule(
+                # Detect dhcp acknowledge
+                app_testing_objects.RyuDHCPAckFilter(),
+                actions=[
+                    app_testing_objects.DisableRuleAction(),
+                    app_testing_objects.WaitAction(5),
+                    app_testing_objects.SendAction(
+                        self.subnet1.subnet_id,
+                        self.port1.port_id,
+                        self._create_dhcp_renewal_request
+                    ),
+                ]
+            ),
+            app_testing_objects.PortPolicyRule(
+                # Detect dhcp acknowledge
+                app_testing_objects.RyuDHCPAckFilter(),
+                actions=[
+                    app_testing_objects.StopSimulationAction(),
+                    app_testing_objects.DisableRuleAction()
+                ]
+            ),
+            app_testing_objects.PortPolicyRule(
+                # Ignore IPv6 packets
+                app_testing_objects.RyuIPv6Filter(),
+                actions=[
+                    ignore_action
+                ]
+            ),
+        ]
+        key2 = (self.subnet1.subnet_id, self.port2.port_id)
+        rules2 = [
+            app_testing_objects.PortPolicyRule(
+                # Detect arp replies
+                app_testing_objects.RyuDHCPFilter(),
+                actions=[
+                    app_testing_objects.RaiseAction(
+                        "Received DHCP packet"
+                    )
+                ]
+            ),
+            app_testing_objects.PortPolicyRule(
+                # Ignore IPv6 packets
+                app_testing_objects.RyuIPv6Filter(),
+                actions=[
+                    ignore_action
+                ]
+            ),
+        ]
+        raise_action = app_testing_objects.RaiseAction("Unexpected packet")
+        policy1 = app_testing_objects.PortPolicy(
+            rules=rules1,
+            default_action=raise_action
+        )
+        policy2 = app_testing_objects.PortPolicy(
+            rules=rules2,
+            default_action=raise_action
+        )
+        return {
+            key1: policy1,
+            key2: policy2,
+        }
+
+    def test_dhcp_app(self):
+        self.policy.start(self.topology)
+        self.policy.wait(30)
+        if len(self.policy.exceptions) > 0:
+            raise self.policy.exceptions[0]
