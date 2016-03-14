be543813b335d7e53aab6c1922403ac29b75c968
Yuli <yuli.stremovsky@huawei.com>
2016-03-14 15:42:07 +0200

    Fix file limit bug
    Change-Id: Ib828f756bd2a0b3fd49769086edf5205ef6306ce
    Closes-bug: #1556598

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -198,6 +198,11 @@ function install_df {
 function install_ovs {
     echo "Installing OVS and dependent packages"
 
+    grep -E "^\*\s+soft\s+nofile" /etc/security/limits.conf > /dev/null || \
+      sudo sh -c 'echo "* soft nofile 50000" >> /etc/security/limits.conf'
+    grep -E "^\*\s+hard\s+nofile" /etc/security/limits.conf > /dev/null || \
+      sudo sh -c 'echo "* hard nofile 50000" >> /etc/security/limits.conf'
+
     # If OVS is already installed, remove it, because we're about to re-install
     # it from source.
     for package in openvswitch openvswitch-switch openvswitch-common; do
