e0ab03ee149da95c354c88dfe7d4dad88eb8f82f
feipeng <feipeng1@huawei.com>
2016-03-26 12:58:36 +0800

    Add Redis Devstack
    Change-Id: Ie100d3ae236a510dbc05fc7f12f224362a05ad29

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -12,6 +12,7 @@ DEFAULT_NB_DRIVER_CLASS="dragonflow.db.drivers.etcd_db_driver.EtcdDbDriver"
 DEFAULT_TUNNEL_TYPE="geneve"
 DEFAULT_APPS_LIST="l2_app.L2App,l3_proactive_app.L3ProactiveApp,dhcp_app.DHCPApp"
 DEFAULT_SELECTIVE_TOPO_DIST="False"
+DEFAULT_DF_REDIS_PUBSUB="False"
 
 # How to connect to the database storing the virtual topology.
 REMOTE_DB_IP=${REMOTE_DB_IP:-$HOST_IP}
@@ -21,6 +22,7 @@ NB_DRIVER_CLASS=${NB_DRIVER_CLASS:-$DEFAULT_NB_DRIVER_CLASS}
 TUNNEL_TYPE=${TUNNEL_TYPE:-$DEFAULT_TUNNEL_TYPE}
 DF_APPS_LIST=${DF_APPS_LIST:-$DEFAULT_APPS_LIST}
 DF_SELECTIVE_TOPO_DIST=${DF_SELECTIVE_TOPO_DIST:-$DEFAULT_SELECTIVE_TOPO_DIST}
+DF_REDIS_PUBSUB=${DF_REDIS_PUBSUB:-$DEFAULT_DF_REDIS_PUBSUB}
 
 #pubsub
 PUBLISHERS_HOSTS=${PUBLISHERS_HOSTS:-"$SERVICE_HOST"}
@@ -46,6 +48,10 @@ if is_service_enabled df-zookeeper ; then
     source $DEST/dragonflow/devstack/zookeeper_driver
     NB_DRIVER_CLASS="dragonflow.db.drivers.zookeeper_db_driver.ZookeeperDbDriver"
 fi
+if is_service_enabled df-redis ; then
+    source $DEST/dragonflow/devstack/redis_driver
+    NB_DRIVER_CLASS="dragonflow.db.drivers.redis_db_driver.RedisDbDriver"
+fi
 
 # Pub/Sub Service
 #----------------
@@ -60,6 +66,10 @@ if is_service_enabled df-zmq-publisher-service ; then
     source $DEST/dragonflow/devstack/zmq_pubsub_driver
 fi
 
+if [[ "$DF_REDIS_PUBSUB" == "True" ]]; then
+    init_pubsub
+    source $DEST/dragonflow/devstack/redis_pubsub_driver
+fi
 # Dragonflow installation uses functions from these files
 source $TOP_DIR/lib/neutron_plugins/ovs_base
 source $TOP_DIR/lib/neutron_plugins/openvswitch_agent
