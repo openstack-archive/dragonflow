5bcbd69a145c2a5fdec0eb57ff4022268ba7064f
Li Ma <skywalker.nick@gmail.com>
2016-03-29 16:25:24 +0800

    Fix py3 mock cannot be compared to int
    Change-Id: I14a3280cb1e7c963be99e4ea5ec05c9cec00a289
    Closes-Bug: #1563222

--- dragonflow/tests/unit/test_ryu_base_app.py
+++ dragonflow/tests/unit/test_ryu_base_app.py
@@ -89,6 +89,10 @@ class TestRyuDFAdapter(tests_base.BaseTestCase):
     def test_switch_features_handler(self):
         self.mock_app.reset_mock()
         ev = mock.Mock()
+        ev.msg = mock.Mock()
+        ev.msg.datapath = mock.Mock()
+        ev.msg.datapath.ofproto = mock.Mock()
+        ev.msg.datapath.ofproto.OFP_VERSION = 0x04
         self.ryu_df_adapter.switch_features_handler(ev)
         self.mock_app.assert_has_calls([mock.call.switch_features_handler(ev)])
 
