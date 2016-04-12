650ff3eac7421fd10ded849161dccb07480f9b2e
Shlomo <shlominar@gmail.com>
2016-04-12 07:17:15 -0400

    [WIP] Performance testing framework. Environment setup and data-plane performance testing.
    Change-Id: I6a4a585fbc2f0d5151ec62f4d2fc7e585fc2c86d

---
+++ dragonflow/tests/performance/env_setup/create_VMs_on_CN.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+computeName=""
+
+if [ $# -eq 0 ]
+	then
+		echo "No arguments supplied"
+		echo "usage:"
+		echo "./create-VMsOn2Nets_byCN.sh number_of_VMs Compute_Name (Compute_Name is optional)"
+		exit 1
+fi
+if [ $# -eq 2 ]
+	then
+		computeName=$2
+fi
+
+numOfVms=$1
+cnt=0
+sleepTime=1
+
+echo "===================================="
+echo "Creating $numOfVms on two networks"
+echo "===================================="
+for ((i=1; i<=numOfVms; i++)); do
+        echo "Creating VM #$i on Private Network 1 on $2"
+	if [ ! -z "$computeName" -a "$computeName" != " " ]; then
+	   python create-VM.py test private1 $2
+	else
+	   python create-VM.py test private1
+	fi
+
+        echo "sleeping $sleepTime secs"
+        sleep $sleepTime
+
+	i=$((i + 1))
+	echo "Creating VM #$i On Private Network 2 $2"
+	if [ ! -z "$computeName" -a "$computeName" != " " ]; then
+	   python create-VM.py test private2 $2
+	else
+	   python create-VM.py test private2
+	fi
+
+        echo "sleeping $sleepTime secs"
+        sleep $sleepTime
+
+	cnt=$((cnt + 1))
+	if [ $cnt == 60 ]; then
+		cnt=0
+		echo "========================================"
+		echo "sleeping 5 secs, after 60 VMs creation."
+		echo "========================================"
+		sleep 5
+	fi
+done
