ff8608035f41a49d4dc56ffdbd920b559045b90a
zhuweiqiong <zhuweiqiong@huawei.com>
2016-04-23 11:10:19 +0800

    Add redis HA implement
    Change-Id: I3711b2a18d3c384a392a26b509e2db3e7df1f356

--- dragonflow/db/pubsub_drivers/redis_db_pubsub_driver.py
+++ dragonflow/db/pubsub_drivers/redis_db_pubsub_driver.py
@@ -12,7 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from dragonflow._i18n import _LE
+from dragonflow._i18n import _LE, _LW
 from dragonflow.common import common_params
 from dragonflow.db.drivers.redis_mgt import RedisMgt
 from dragonflow.db import pub_sub_api
@@ -55,11 +55,23 @@ class RedisPublisherAgent(pub_sub_api.PublisherApi):
         # find a publisher server node
         super(RedisPublisherAgent, self).initialize()
         self.redis_mgt = RedisMgt.get_instance(cfg.CONF.df.remote_db_ip,
-                                              cfg.CONF.df.remote_db_port)
-        self.remote = self.redis_mgt.pubsub_select_node_idx()
-        ip_port = self.remote.split(':')
-        self.client = redis.client.StrictRedis(host=ip_port[0],
-                                               port=ip_port[1])
+                                               cfg.CONF.df.remote_db_port)
+        self._update_client()
+
+    def _update_client(self):
+        if self.redis_mgt is not None:
+            self.remote = self.redis_mgt.pubsub_select_node_idx()
+            if self.remote is not None:
+                ip_port = self.remote.split(':')
+                self.client = redis.client.StrictRedis(host=ip_port[0],
+                                                       port=ip_port[1])
+
+    def process_ha(self):
+        # None means that publisher connection should be updated.
+        # If not None, the publisher connection is still working and is not
+        # broken by DB single point failure.
+        if self.remote is None:
+            self._update_client()
 
     def send_event(self, update, topic=None):
         if topic:
@@ -68,14 +80,39 @@ class RedisPublisherAgent(pub_sub_api.PublisherApi):
         event_json = jsonutils.dumps(update.to_dict())
         local_topic = local_topic.encode('utf8')
         data = pub_sub_api.pack_message(event_json)
-        self.client.publish(local_topic, data)
+        try:
+            if self.client is not None:
+                self.client.publish(local_topic, data)
+        except Exception as e:
+            LOG.warning(e)
+            self.redis_mgt.remove_node_from_master_list(self.remote)
+            self._update_client()
+            try:
+                if self.client is not None:
+                    self.client.publish(local_topic, data)
+            except Exception as e:
+                self.redis_mgt.remove_node_from_master_list(self.remote)
+                self._update_client()
+                LOG.exception(_LE("publish error remote:%(remote)s")
+                              % {'remote': self.remote})
+
+    def _set_publisher_to_mgt(self, pub, callback):
+        self.redis_mgt.set_publisher(pub, callback)
+
+    def _start_detect_task(self):
+        # only start in NB plugin
+        if self.redis_mgt is not None:
+
+            self.redis_mgt.daemonize()
+        else:
+            LOG.warning(_LW("redis mgt is none"))
 
 
 class RedisSubscriberAgent(pub_sub_api.SubscriberAgentBase):
 
     def __init__(self):
         super(RedisSubscriberAgent, self).__init__()
-        self.remote = []
+        self.remote = None
         self.client = None
         self.ip = ""
         self.plugin_updates_port = ""
@@ -86,14 +123,26 @@ class RedisSubscriberAgent(pub_sub_api.SubscriberAgentBase):
         # find a subscriber server node and run daemon
         super(RedisSubscriberAgent, self).initialize(callback)
         self.redis_mgt = RedisMgt.get_instance(cfg.CONF.df.remote_db_ip,
-                                              cfg.CONF.df.remote_db_port)
-        self.remote = self.redis_mgt.pubsub_select_node_idx()
-        ip_port = self.remote.split(':')
-        self.client = \
-            redis.client.StrictRedis(host=ip_port[0], port=ip_port[1])
-        self.ip = ip_port[0]
-        self.plugin_updates_port = ip_port[1]
-        self.pub_sub = self.client.pubsub()
+                                               cfg.CONF.df.remote_db_port)
+        self._update_client()
+
+    def process_ha(self):
+        # None means that subscriber connection should be updated.
+        # If not None, the subscriber connection is still working and is not
+        # broken by DB single point failure.
+        if self.remote is None:
+            self._update_client()
+
+    def _update_client(self):
+        if self.redis_mgt is not None:
+            self.remote = self.redis_mgt.pubsub_select_node_idx()
+            if self.remote is not None:
+                ip_port = self.remote.split(':')
+                self.client = \
+                    redis.client.StrictRedis(host=ip_port[0], port=ip_port[1])
+                self.ip = ip_port[0]
+                self.plugin_updates_port = ip_port[1]
+                self.pub_sub = self.client.pubsub()
 
     def register_listen_address(self, uri):
         super(RedisSubscriberAgent, self).register_listen_address(uri)
@@ -107,32 +156,64 @@ class RedisSubscriberAgent(pub_sub_api.SubscriberAgentBase):
     def unregister_topic(self, topic):
         self.pub_sub.unsubscribe(topic)
 
+    def _set_subscriber_to_mgt(self, sub, callback):
+        self.redis_mgt.set_subscriber(sub, callback)
+
+    def _register_ha(self):
+        if self.redis_mgt is not None:
+            self.redis_mgt.register_ha_topic()
+        else:
+            LOG.warning(_LW("redis mgt is none"))
+
     def run(self):
         while True:
             eventlet.sleep(0)
             try:
-                for data in self.pub_sub.listen():
-                    if 'subscribe' == data['type']:
-                        continue
-                    if 'unsubscribe' == data['type']:
-                        continue
-                    if 'message' == data['type']:
-                        entry = pub_sub_api.unpack_message(data['data'])
-                        entry_json = jsonutils.loads(entry)
-                        self.db_changes_callback(
-                            entry_json['table'],
-                            entry_json['key'],
-                            entry_json['action'],
-                            entry_json['value'],
-                            entry_json['topic'])
+                if self.pub_sub is not None:
+                    for data in self.pub_sub.listen():
+                        if 'subscribe' == data['type']:
+                            continue
+                        if 'unsubscribe' == data['type']:
+                            continue
+                        if 'message' == data['type']:
+                            entry = pub_sub_api.unpack_message(data['data'])
+                            entry_json = jsonutils.loads(entry)
+
+                            if entry_json['table'] != 'ha':
+                                self.db_changes_callback(
+                                    entry_json['table'],
+                                    entry_json['key'],
+                                    entry_json['action'],
+                                    entry_json['value'],
+                                    entry_json['topic'])
+                            else:
+                                # redis ha message
+                                self.redis_mgt.redis_failover_callback(
+                                    entry_json['value'])
+
+                else:
+                    eventlet.sleep(1)
+                    LOG.warning(_LW("pubsub lost connection %(ip)s:"
+                                    "%(port)s")
+                                % {'ip': self.ip,
+                                   'port': self.plugin_updates_port})
 
             except Exception as e:
                 LOG.warning(e)
                 try:
                     connection = self.pub_sub.connection
                     connection.connect()
-                    self.db_changes_callback(None, None, 'sync', None, None)
+                    self.pub_sub.on_connect()
+                    # self.db_changes_callback(None, None, 'sync', None, None)
+                    # todo notify restart
+                    self.db_changes_callback(None, None, 'dbrestart', None,
+                                             None)
                 except Exception as e:
+                    self.redis_mgt.remove_node_from_master_list(self.remote)
+                    self._update_client()
+                    # todo if pubsub not none notify restart
+                    self.db_changes_callback(None, None, 'dbrestart', None,
+                                             None)
                     LOG.exception(_LE("reconnect error %(ip)s:%(port)s")
                                   % {'ip': self.ip,
                                      'port': self.plugin_updates_port})
--- dragonflow/tests/unit/test_redis_db.py
+++ dragonflow/tests/unit/test_redis_db.py
@@ -29,6 +29,10 @@ class TestRedisDB(tests_base.BaseTestCase):
         self.RedisDbDriver._get_client = mock.Mock(return_value=client)
         self.RedisDbDriver.clients[0] = client
         client.set.return_value = 1
+        client.wait.return_value = 1
+        redis_mgt = mock.Mock()
+        self.RedisDbDriver.redis_mgt = redis_mgt
+        redis_mgt.get_ip_by_key.return_value = '0.0.0.0:1000'
         result = self.RedisDbDriver.set_key('table', 'key', 'value', 'topic')
         self.assertEqual(result, 1)
 
@@ -37,7 +41,11 @@ class TestRedisDB(tests_base.BaseTestCase):
         self.RedisDbDriver._get_client = mock.Mock(return_value=client)
         self.RedisDbDriver.clients[0] = client
         client.set.return_value = 0
+        client.wait.return_value = 0
         client.delete.return_value = 1
+        redis_mgt = mock.Mock()
+        self.RedisDbDriver.redis_mgt = redis_mgt
+        redis_mgt.get_ip_by_key.return_value = '0.0.0.0:1000'
         result = self.RedisDbDriver.set_key('table', 'key', 'value', 'topic')
         self.assertEqual(result, 0)
 
@@ -45,6 +53,9 @@ class TestRedisDB(tests_base.BaseTestCase):
         client = mock.Mock()
         self.RedisDbDriver._get_client = mock.Mock(return_value=client)
         self.RedisDbDriver.clients[0] = client
+        redis_mgt = mock.Mock()
+        self.RedisDbDriver.redis_mgt = redis_mgt
+        redis_mgt.get_ip_by_key.return_value = '0.0.0.0:1000'
         client.get.return_value = 'value'
         result = self.RedisDbDriver.get_key('table', 'key', 'topic')
         self.assertEqual(result, 'value')
@@ -59,6 +70,9 @@ class TestRedisDB(tests_base.BaseTestCase):
         client.keys.return_value = 'a'
         client.mget.return_value = 'value'
         client.get.return_value = 'value'
+        redis_mgt = mock.Mock()
+        self.RedisDbDriver.redis_mgt = redis_mgt
+        redis_mgt.get_ip_by_key.return_value = '0.0.0.0:1000'
         result = self.RedisDbDriver.get_all_entries('table')
         self.assertEqual(result, ['value'])
         client.keys.return_value = 'a'
@@ -70,6 +84,10 @@ class TestRedisDB(tests_base.BaseTestCase):
         self.RedisDbDriver._get_client = mock.Mock(return_value=client)
         self.RedisDbDriver.clients[0] = client
         client.delete.return_value = 1
+        client.wait.return_value = 1
+        redis_mgt = mock.Mock()
+        self.RedisDbDriver.redis_mgt = redis_mgt
+        redis_mgt.get_ip_by_key.return_value = '0.0.0.0:1000'
         result = self.RedisDbDriver.delete_key('table', 'key', 'topic')
         self.assertEqual(result, 1)
 
@@ -78,6 +96,9 @@ class TestRedisDB(tests_base.BaseTestCase):
         self.RedisDbDriver._get_client = mock.Mock(return_value=client)
         self.RedisDbDriver.clients[0] = client
         client.incr.return_value = 1
+        redis_mgt = mock.Mock()
+        self.RedisDbDriver.redis_mgt = redis_mgt
+        redis_mgt.get_ip_by_key.return_value = '0.0.0.0:1000'
         result = self.RedisDbDriver.allocate_unique_key()
         self.assertEqual(result, 1)
 
