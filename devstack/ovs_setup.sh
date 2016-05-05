12c01e654509a299f2e2853bf69045835b67209f
Li Ma <skywalker.nick@gmail.com>
2016-05-05 09:31:28 +0800

    Support define ovs branch
    Change-Id: Ie7beaf21ce529462f22b41e38223b409e8531770

--- devstack/ovs_setup.sh
+++ devstack/ovs_setup.sh
@@ -40,12 +40,12 @@ function _neutron_ovs_get_rpm_file {
 function _neutron_ovs_clone_ovs {
     if [ -d $DEST/ovs ]; then
         pushd $DEST/ovs
-        git checkout master
+        git checkout $OVS_BRANCH
         git pull
         popd
     else
         pushd $DEST
-        git clone $OVS_REPO
+        git clone $OVS_REPO -b $OVS_BRANCH
         popd
     fi
 }
