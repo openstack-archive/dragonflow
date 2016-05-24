2effa0e389c8bf429c7c4be1cd9fc0bf4462abd7
wangyongben <wangyongben@huawei.com>
2016-05-24 15:22:43 +0800

    Support ML2 Plugin
    Fix input params when write df db
    
    Change-Id: I8ebebc40248374a424a14b93f186283a707b5a50

--- dragonflow/tests/unit/test_controller.py
+++ dragonflow/tests/unit/test_controller.py
@@ -12,7 +12,7 @@
 
 import mock
 
-from oslo_config import cfg
+# from oslo_config import cfg
 
 from dragonflow.controller import dispatcher
 from dragonflow.db import db_store
@@ -25,7 +25,7 @@ class TestDfController(tests_base.BaseTestCase):
         super(TestDfController, self).setUp()
         dispatcher.AppDispatcher = mock.Mock()
         db_store.DbStore = mock.Mock()
-        cfg.CONF = mock.Mock()
+        # cfg.CONF = mock.Mock()
         self.controller = mock.Mock()
         self.controller.nb_api = mock.Mock()
         self.controller.vswitch_api = mock.Mock()
