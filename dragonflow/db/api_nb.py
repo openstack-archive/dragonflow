08e05f1082728fd745348c237718e0d6ef223352
duankebo <duankebo@huawei.com>
2016-05-31 02:55:02 +0000

    [WIP]Add extra routes support for router
    This patch adds extra routes to router.
    Users can specify a set of nexthop IPs and destination CIDRs for router.
    
    Change-Id: Ic6d6c21852c0ca7eacaac20f0d1342f8d9e5ffb3

--- dragonflow/db/api_nb.py
+++ dragonflow/db/api_nb.py
@@ -931,7 +931,7 @@ class LogicalRouter(DbStoreObject):
 
     def get_ports(self):
         res = []
-        for port in self.lrouter.get('ports'):
+        for port in self.lrouter.get('ports', []):
             res.append(LogicalRouterPort(port))
         return res
 
@@ -940,6 +940,9 @@ class LogicalRouter(DbStoreObject):
 
     def get_version(self):
         return self.lrouter['version']
+
+    def get_routes(self):
+        return self.lrouter.get('routes', [])
 
     def __str__(self):
         return self.lrouter.__str__()
