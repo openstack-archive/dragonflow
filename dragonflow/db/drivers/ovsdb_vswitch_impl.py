58d648acbd47e835ee42c115c144f691168f8a80
yuanwei <wei.yuan@huawei.com>
2016-07-09 14:56:40 +0800

    add active detection app for allowed address pairs
    This patch implement the active detection app for allowed
    address pairs.
    The main changes are:
    1) Add a struct named 'activeport' in DF DB, and it is used to save
    the detected active VM port of a allowed address pairs.
    2) Add a app named active_port_detection_app. This app is used to
    detect the active VM port by sending ARP requests and receiving the
    replies or the gratuitous ARPs.
    3) Add fullstack cases about this feature.
    
    Change-Id: Iaba7e719770556468bf36df56d330100361d5f84

--- dragonflow/db/drivers/ovsdb_vswitch_impl.py
+++ dragonflow/db/drivers/ovsdb_vswitch_impl.py
@@ -266,6 +266,15 @@ class OvsdbSwitchApi(api_vswitch.SwitchApi):
 
         return chassis_to_ofport, lport_to_ofport
 
+    def get_local_port_mac_in_use(self, port_id):
+        interfaces = self.idl.tables['Interface'].rows.values()
+        for interface in interfaces:
+            ifaceid = interface.external_ids.get('iface-id')
+            if (ifaceid == port_id) and (interface.mac_in_use is not None):
+                mac_in_use = interface.mac_in_use
+                if not mac_in_use:
+                    return mac_in_use[0]
+
     def create_patch_port(self, bridge, port, remote_name):
         if not commands.BridgeExistsCommand(self, bridge).execute():
             commands.AddBridgeCommand(self, bridge, True,
