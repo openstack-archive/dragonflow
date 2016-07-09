320f13b09d8b848c4b71b534af22b4523734ed2c
yuanwei <wei.yuan@huawei.com>
2016-07-09 14:56:40 +0800

    add active detection app for allowed address pairs
    This patch implement the active detection app for allowed
    address pairs.
    The main changes are:
    1) Add a struct named 'activeport' in DF DB, and it is used to save
    the detected active VM port of a allowed address pairs.
    2) Add a app named active_port_detection_app. This app is used to
    detect the active VM port by sending ARP requests and receiving the
    replies or the gratuitous ARPs.
    3) Add fullstack cases about this feature.
    
    Change-Id: Iaba7e719770556468bf36df56d330100361d5f84

--- dragonflow/cli/df_db.py
+++ dragonflow/cli/df_db.py
@@ -25,7 +25,7 @@ cfg.CONF.register_opts(common_params.DF_OPTS, 'df')
 
 db_tables = ['lport', 'lswitch', 'lrouter', 'chassis', 'secgroup',
              'tunnel_key', 'floatingip', 'publisher', 'qospolicy',
-             'portstats']
+             'portstats', 'activeport']
 
 
 def print_tables():
