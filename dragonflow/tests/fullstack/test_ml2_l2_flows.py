fe3141364fc4d77de18aa554aba6eb449fbe7649
liuhaixia <liuhaixia315@huawei.com>
2016-07-13 09:49:34 +0800

    Implement L2 Communication for flat network.
    This patch implements L2 communication for flat network.
    
    Change-Id: Ifdf095a666ec71570039a47a544fc9125f0c9f62

--- dragonflow/tests/fullstack/test_ml2_l2_flows.py
+++ dragonflow/tests/fullstack/test_ml2_l2_flows.py
@@ -92,13 +92,23 @@ class TestL2FLows(test_base.DFTestBase):
         if self._check_l2_ml2_app_enable() is False:
             return
 
-        network_vlan_ranges = self._get_vlan_ranges()
+        network_vlan_ranges = self._get_config_values('ml2_type_vlan',
+                                                      'network_vlan_ranges')
 
         if network_vlan_ranges is None:
+            self.assertIsNotNone(None)
             return
-        vlan_params = network_vlan_ranges.split(':')
 
-        if vlan_params is None or len(vlan_params) == 0:
+        network_vlan_range_list = network_vlan_ranges.split(',')
+        if not network_vlan_range_list:
+            self.assertIsNotNone(None)
+            return
+
+        network_vlan_range = network_vlan_range_list[0]
+        vlan_params = network_vlan_range.split(':')
+
+        if not vlan_params:
+            self.assertIsNotNone(None)
             return
 
         physical_network = vlan_params[0]
@@ -267,23 +277,153 @@ class TestL2FLows(test_base.DFTestBase):
 
         return True
 
-    def _get_vlan_ranges(self):
+    def test_flat_network_flows(self):
+        if self._check_l2_ml2_app_enable() is False:
+            return
+
+        flat_networks = self._get_config_values('ml2_type_flat',
+                                                'flat_networks')
+        if flat_networks is None:
+            self.assertIsNotNone(None)
+            return
+
+        flat_networks_list = flat_networks.split(',')
+        if not flat_networks_list:
+            self.assertIsNotNone(None)
+            return
+
+        flat_network = flat_networks_list[0]
+
+        physical_network = 'phynet1'
+        if flat_network != '*':
+            physical_network = flat_network
+
+        # Create network
+        network = self.store(objects.NetworkTestObj(self.neutron, self.nb_api))
+        network_params = {"name": "vlan_1",
+                          "provider:network_type": "flat",
+                          "provider:physical_network": physical_network}
+        network_id = network.create(network=network_params)
+
+        # Create subnet
+        subnet_params = {'network_id': network_id,
+                         'cidr': '100.64.0.0/24',
+                         'gateway_ip': '10.64.0.1',
+                         'ip_version': 4,
+                         'name': 'private',
+                         'enable_dhcp': True}
+
+        subnet = self.neutron.create_subnet({'subnet': subnet_params})
+        self.assertIsNotNone(subnet)
+
+        # Create VM
+        ovs = utils.OvsFlowsParser()
+        vm = self.store(objects.VMTestObj(self, self.neutron))
+        vm.create(network=network)
+        ip = vm.get_first_ipv4()
+        self.assertIsNotNone(ip)
+
+        mac = vm.get_first_mac()
+        self.assertIsNotNone(mac)
+
+        metadataid = utils.wait_until_is_and_return(
+            lambda: self._get_metadata_id(ovs.dump(self.integration_bridge),
+                                          ip, mac),
+            exception=Exception('Metadata id was not found in OpenFlow rules')
+        )
+        port = utils.wait_until_is_and_return(
+            lambda: self._get_vm_port(ip, mac),
+            exception=Exception('No port assigned to VM')
+        )
+        port_key = port.get_tunnel_key()
+        port_key_hex = hex(port_key)
+        r = self._check_flat_flows(ovs.dump(self.integration_bridge),
+                                   metadataid, port_key_hex, mac)
+        self.assertIsNotNone(r)
+        vm.server.stop()
+        vm.close()
+        network.close()
+        return None
+
+    def _check_flat_flows(self, flows, metadtata,
+                          port_key_hex, mac):
+        l2_lookup_unicast_match = 'metadata=0x' + metadtata + \
+                                 ',dl_dst=' + mac
+        l2_lookup_unicast_action = 'goto_table:' + \
+                                   str(const.EGRESS_TABLE)
+        l2_lookup_unkown_match = 'metadata=0x' + metadtata + \
+                                 ',dl_dst=00:00:00:00:00:00/01:00:00:00:00:00'
+        l2_lookup_unkown_action = 'goto_table:' + \
+                                  str(const.EGRESS_TABLE)
+        l2_lookup_multicast_match = 'metadata=0x' + metadtata + ',dl_dst=' + \
+                                    '01:00:00:00:00:00/01:00:00:00:00:00'
+        l2_lookup_multicast_action = 'set_field:' + port_key_hex + \
+                                     '->reg7,resubmit(,' + \
+                                     str(const.EGRESS_TABLE) + ')' + \
+                                     ',set_field:0' + \
+                                     '->reg7,resubmit(,' + \
+                                     str(const.EGRESS_TABLE) + ')'
+
+        egress_match = 'metadata=0x' + metadtata
+        egress_action = 'goto_table:' + \
+                        str(const.EGRESS_EXTERNAL_TABLE)
+        ingress_match = 'vlan_tci=0x0000/0x1fff'
+        ingress_action = 'set_field:0x' + metadtata + \
+                         '->metadata,goto_table:' + \
+                         str(const.INGRESS_DESTINATION_PORT_LOOKUP_TABLE)
+        l2_lookup_unicast_check = None
+        l2_lookup_multicast_check = None
+        l2_lookup_unkown_check = None
+        ingress_check = None
+        egress_check = None
+
+        for flow in flows:
+            if flow['table'] == str(const.L2_LOOKUP_TABLE):
+                if (l2_lookup_multicast_match in flow['match']):
+                    if l2_lookup_multicast_action in flow['actions']:
+                        l2_lookup_multicast_check = True
+                if (l2_lookup_unicast_match in flow['match']):
+                    if l2_lookup_unicast_action in flow['actions']:
+                        l2_lookup_unicast_check = True
+                if (l2_lookup_unkown_match in flow['match']):
+                    if l2_lookup_unkown_action in flow['actions']:
+                        l2_lookup_unkown_check = True
+            if flow['table'] == str(const.EGRESS_TABLE):
+                if (egress_match in flow['match']):
+                    if egress_action in flow['actions']:
+                        egress_check = True
+
+            if flow['table'] == str(
+                    const.INGRESS_CLASSIFICATION_DISPATCH_TABLE):
+                if (ingress_match in flow['match']):
+                    if ingress_action in flow['actions']:
+                        ingress_check = True
+
+        if l2_lookup_multicast_check is None or \
+            l2_lookup_unicast_check is None or \
+            l2_lookup_unkown_check is None or \
+            egress_check is None or \
+            ingress_check is None:
+            return None
+        return True
+
+    def _get_config_values(self, section, key):
         readhandle = None
-        vlan_ranges = None
+        value = None
         try:
             config = ConfigParser.ConfigParser()
             readhandle = open(ML2_CONF_INI, 'r')
             config.readfp(readhandle)
-            vlan_ranges = config.get("ml2_type_vlan", 'network_vlan_ranges')
+            value = config.get(section, key)
         except Exception:
-            vlan_ranges = None
+            value = None
 
         if readhandle is not None:
             try:
                 readhandle.close()
             except Exception:
-                return vlan_ranges
-        return vlan_ranges
+                return value
+        return value
 
     def _check_l2_ml2_app_enable(self):
         apps_list = cfg.CONF.df.apps_list
