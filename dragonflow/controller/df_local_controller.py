55b751db7989827c0a064724c11dfb21461f2155
Shachar Snapiri <shachar.snapiri@huawei.com>
2016-09-25 15:29:44 +0300

    Separate the read/update and the delete of objects
    The most important aspect is that the removal of objects
    should be in reverse order of their creation/update
    
    Change-Id: I649f968d086153bd7800a9c0aa530a016e26871c
    Closes-Bug: #1627493

--- dragonflow/controller/df_local_controller.py
+++ dragonflow/controller/df_local_controller.py
@@ -29,6 +29,7 @@ from dragonflow._i18n import _LI, _LW, _
 from dragonflow.common import common_params
 from dragonflow.common import constants
 from dragonflow.common import utils as df_utils
+from dragonflow.controller.df_db_objects_refresh import DfObjectRefresher
 from dragonflow.controller import ryu_base_app
 from dragonflow.controller import topology
 from dragonflow.db import api_nb
@@ -128,19 +129,48 @@ class DfLocalController(object):
 
             self.register_chassis()
 
+            # TODO(snapiri): Use object refresher for tunnels too?
             self.create_tunnels()
 
             if not self.enable_selective_topo_dist:
+                # The order of the items here is meaningful, it is sorted
+                # by the objects dependency in each other
+                items = [
+                    DfObjectRefresher('Switches',
+                                      self.db_store.get_lswitch_keys,
+                                      self.nb_api.get_all_logical_switches,
+                                      self.logical_switch_updated,
+                                      self.logical_switch_deleted),
+                    DfObjectRefresher('Security Groups',
+                                      self.db_store.get_security_group_keys,
+                                      self.nb_api.get_security_groups,
+                                      self.security_group_updated,
+                                      self.security_group_deleted),
+                    DfObjectRefresher('Ports',
+                                      self.db_store.get_port_keys,
+                                      self.nb_api.get_all_logical_ports,
+                                      self.logical_port_updated,
+                                      self.logical_port_deleted),
+                    DfObjectRefresher('Routers',
+                                      self.db_store.get_router_keys,
+                                      self.nb_api.get_routers,
+                                      self.router_updated,
+                                      self.router_deleted),
+                    DfObjectRefresher('Floating IPs',
+                                      self.db_store.get_floatingip_keys,
+                                      self.nb_api.get_floatingips,
+                                      self.floatingip_updated,
+                                      self.floatingip_deleted),
+                         ]
 
-                self.read_switches()
+                # Refresh all the objects and find which ones should be removed
+                for item in items:
+                    item.read()
+                    item.update()
 
-                self.read_security_groups()
-
-                self.port_mappings()
-
-                self.read_routers()
-
-                self.read_floatingip()
+                # Remove obsolete objects in reverse order
+                for item in reversed(items):
+                    item.delete()
 
             self.sync_finished = True
 
@@ -172,16 +202,6 @@ class DfLocalController(object):
             if port.get_chassis_id() == chassis_id:
                 self.vswitch_api.delete_port(port).execute()
                 return
-
-    def read_switches(self):
-        lswitches_to_remove = self.db_store.get_lswitch_keys()
-        for lswitch in self.nb_api.get_all_logical_switches():
-            self.logical_switch_updated(lswitch)
-            if lswitch.get_id() in lswitches_to_remove:
-                lswitches_to_remove.remove(lswitch.get_id())
-
-        for lswitch_to_remove in lswitches_to_remove:
-            self.logical_switch_deleted(lswitch_to_remove)
 
     def logical_switch_updated(self, lswitch):
         old_lswitch = self.db_store.get_lswitch(lswitch.get_id())
@@ -364,16 +384,6 @@ class DfLocalController(object):
         for port in tunnel_ports.values():
             self.vswitch_api.delete_port(port).execute()
 
-    def port_mappings(self):
-        ports_to_remove = self.db_store.get_port_keys()
-        for lport in self.nb_api.get_all_logical_ports():
-            self.logical_port_updated(lport)
-            if lport.get_id() in ports_to_remove:
-                ports_to_remove.remove(lport.get_id())
-
-        for port_to_remove in ports_to_remove:
-            self.logical_port_deleted(port_to_remove)
-
     def get_network_id(self, logical_dp_id):
         network_id = self.db_store.get_network_id(logical_dp_id)
         if network_id is not None:
@@ -386,16 +396,6 @@ class DfLocalController(object):
                 self.next_network_id,
             )
             return self.next_network_id
-
-    def read_routers(self):
-        routers_to_remove = self.db_store.get_router_keys()
-        for lrouter in self.nb_api.get_routers():
-            self.router_updated(lrouter)
-            if lrouter.get_id() in routers_to_remove:
-                routers_to_remove.remove(lrouter.get_id())
-
-        for router_to_remove in routers_to_remove:
-            self.router_deleted(router_to_remove)
 
     def _update_router_interfaces(self, old_router, new_router):
         new_router_ports = new_router.get_ports()
@@ -432,17 +432,6 @@ class DfLocalController(object):
             self._add_new_router_port(lrouter, new_port)
         self.db_store.update_router(lrouter.get_id(), lrouter)
 
-    def read_security_groups(self):
-        secgroups_to_remove = self.db_store.get_security_group_keys()
-
-        for secgroup in self.nb_api.get_security_groups():
-            self.security_group_updated(secgroup)
-            if secgroup.get_id() in secgroups_to_remove:
-                secgroups_to_remove.remove(secgroup.get_id())
-
-        for secgroup_to_remove in secgroups_to_remove:
-            self.security_group_deleted(secgroup_to_remove)
-
     def _update_security_group_rules(self, old_secgroup, new_secgroup):
         new_secgroup_rules = new_secgroup.get_rules()
         old_secgroup_rules = old_secgroup.get_rules()
@@ -476,16 +465,6 @@ class DfLocalController(object):
                  secgroup_rule)
         self.open_flow_app.notify_remove_security_group_rule(
                  secgroup, secgroup_rule)
-
-    def read_floatingip(self):
-        fips_to_remove = self.db_store.get_floatingip_keys()
-        for floatingip in self.nb_api.get_floatingips():
-            self.floatingip_updated(floatingip)
-            if floatingip.get_id() in fips_to_remove:
-                fips_to_remove.remove(floatingip.get_id())
-
-        for fip_to_remove in fips_to_remove:
-            self.floatingip_deleted(fip_to_remove)
 
     def floatingip_updated(self, floatingip):
         # check whether this floatingip is associated with a lport or not
