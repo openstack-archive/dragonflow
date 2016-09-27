4686ee4f60a70c8a4377de96be6589e8b2cd25e1
Kebo Duan <duankebo@huawei.com>
2016-09-27 20:24:06 +0800

    Refactor router's code
    Move some service related code from df-local-controller to l3 app.
    By doing this, we want to make df-local-controller simpler and
    unaware of details of serive
    
    Change-Id: I1e9a9541e6c445611396f5ac7409fd2e4069cbce

--- dragonflow/tests/unit/test_l3_app.py
+++ dragonflow/tests/unit/test_l3_app.py
@@ -1,4 +1,3 @@
-# Copyright (c) 2015 OpenStack Foundation.
 # All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -13,47 +12,29 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import copy
 import mock
 
 from dragonflow.controller.common import constants as const
 from dragonflow.tests.unit import test_app_base
 
 
-class TestL3ProactiveApp(test_app_base.DFAppTestBase):
-    apps_list = "l3_proactive_app.L3ProactiveApp"
+class TestL3App(test_app_base.DFAppTestBase):
+    apps_list = "l3_app.L3App"
 
     def setUp(self):
-        super(TestL3ProactiveApp, self).setUp()
+        super(TestL3App, self).setUp()
         self.app = self.open_flow_app.dispatcher.apps[0]
         self.mock_mod_flow = mock.Mock(name='mod_flow')
         self.app.mod_flow = self.mock_mod_flow
         self.router = test_app_base.fake_logic_router1
 
-    def test_add_del_route(self):
-        # delete router
+    def test_add_del_router(self):
         self.controller.router_deleted(self.router.get_id())
-        assert self.mock_mod_flow.call_count == 4
-
-        # add router
+        #print self.mock_mod_flow.mock_calls
+        assert self.mock_mod_flow.call_count == 3
         self.mock_mod_flow.reset_mock()
         self.controller.router_updated(self.router)
-        assert self.mock_mod_flow.call_count == 3
+        #print self.mock_mod_flow.mock_calls
+        assert self.mock_mod_flow.call_count == 2
         args, kwargs = self.mock_mod_flow.call_args
         assert kwargs['table_id'] == const.L2_LOOKUP_TABLE
-        self.mock_mod_flow.reset_mock()
-
-        # add route
-        route = {"destination": "10.100.0.0/16",
-                 "nexthop": "10.0.0.6"}
-        router_with_route = copy.deepcopy(self.router)
-        router_with_route.lrouter['routes'] = [route]
-        self.controller.logical_port_created(test_app_base.fake_local_port1)
-        self.controller.router_updated(router_with_route)
-        assert self.mock_mod_flow.call_count == 2
-
-        # delete route
-        self.mock_mod_flow.reset_mock()
-        self.router.lrouter['routes'] = []
-        self.controller.router_updated(self.router)
-        assert self.mock_mod_flow.call_count == 1
