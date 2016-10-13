191da3152f699137c936769546b923e6d0648eb0
yuanwei <wei.yuan@huawei.com>
2016-10-13 20:36:37 +0800

    make addresses aggregation in SGApp
    The implement of '__sub__' and '__iter__' method in IPSet class are
    different than what we expected, so the addresses match item  in
    security group rule flows are not aggregated. This patch will convert
    cidrs from IPSet type to list type when it's needed to fix this bug.
    `
    
    Closes-Bug: #1632984
    Change-Id: I0f0cf8f9310a60e1947d891d9d00c7ebfb196f1b

--- dragonflow/controller/sg_app.py
+++ dragonflow/controller/sg_app.py
@@ -83,24 +83,33 @@ class SGApp(df_base_app.DFlowApp):
         )
 
     @staticmethod
-    def _remove_one_address(cidr_array, address):
-        """cidr_array - IPSet
-           address - IPAddress or string
-        """
-        new_cidr_array = cidr_array - netaddr.IPSet((address,))
-        added_cidr = new_cidr_array - cidr_array
-        removed_cidr = cidr_array - new_cidr_array
-        return new_cidr_array, added_cidr, removed_cidr
+    def _get_cidr_difference(cidr_set, new_cidr_set):
+        new_cidr_list = new_cidr_set.iter_cidrs()
+        old_cidr_list = cidr_set.iter_cidrs()
+
+        added_cidr = set(new_cidr_list) - set(old_cidr_list)
+        removed_cidr = set(old_cidr_list) - set(new_cidr_list)
+        return added_cidr, removed_cidr
 
     @staticmethod
-    def _add_one_address(cidr_array, address):
+    def _remove_one_address(cidr_set, address):
         """cidr_array - IPSet
            address - IPAddress or string
         """
-        new_cidr_array = cidr_array | (netaddr.IPAddress(address),)
-        added_cidr = new_cidr_array - cidr_array
-        removed_cidr = cidr_array - new_cidr_array
-        return new_cidr_array, added_cidr, removed_cidr
+        new_cidr_set = cidr_set - netaddr.IPSet((address,))
+        added_cidr, removed_cidr = SGApp._get_cidr_difference(cidr_set,
+                                                              new_cidr_set)
+        return new_cidr_set, added_cidr, removed_cidr
+
+    @staticmethod
+    def _add_one_address(cidr_set, address):
+        """cidr_array - IPSet
+           address - IPAddress or string
+        """
+        new_cidr_set = cidr_set | (netaddr.IPAddress(address),)
+        added_cidr, removed_cidr = SGApp._get_cidr_difference(cidr_set,
+                                                              new_cidr_set)
+        return new_cidr_set, added_cidr, removed_cidr
 
     @staticmethod
     def _get_network_and_mask(cidr):
@@ -704,11 +713,11 @@ class SGApp(df_base_app.DFlowApp):
         # update the record of aggregate addresses of ports associated
         # with this security group.
         addresses = self.secgroup_aggregate_addresses[secgroup_id]
-        new_cidr_array, added_cidr, removed_cidr = SGApp._add_one_address(
+        new_cidr_set, added_cidr, removed_cidr = SGApp._add_one_address(
             addresses,
             ip,
         )
-        self.secgroup_aggregate_addresses[secgroup_id] = new_cidr_array
+        self.secgroup_aggregate_addresses[secgroup_id] = new_cidr_set
 
         # update the flows representing those rules each of which specifies
         #  this security group as its parameter
@@ -728,15 +737,15 @@ class SGApp(df_base_app.DFlowApp):
         aggregate_addresses_range = \
             self.secgroup_aggregate_addresses[secgroup_id]
         if aggregate_addresses_range:
-            new_cidr_array, added_cidr, removed_cidr = \
+            new_cidr_set, added_cidr, removed_cidr = \
                 SGApp._remove_one_address(
                     aggregate_addresses_range,
                     ip,
                 )
-            if not new_cidr_array:
+            if not new_cidr_set:
                 del self.secgroup_aggregate_addresses[secgroup_id]
             else:
-                self.secgroup_aggregate_addresses[secgroup_id] = new_cidr_array
+                self.secgroup_aggregate_addresses[secgroup_id] = new_cidr_set
 
             # update the flows representing those rules each of which
             # specifies this security group as its
