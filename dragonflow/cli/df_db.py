5b22ab89d84701b4edc59596b265948faa13b8ef
Rajiv Kumar <rajiv.kumar@nectechnologies.in>
2016-11-04 11:30:12 +0530

    Output format of df-db changed to display output in table
    Currently format of df-db output does not print information in
    table format and format does not match with existing OpenStack
    components. Output of df-db has been converted to table format
    like other components.
    
    Change-Id: I43e658c77541681edf5085348d7714ae9c145256
    closes-bug: #1638786

--- dragonflow/cli/df_db.py
+++ dragonflow/cli/df_db.py
@@ -17,6 +17,7 @@ from neutron.common import config as common_config
 from oslo_config import cfg
 from oslo_serialization import jsonutils
 
+from dragonflow.cli import utils as cli_utils
 from dragonflow.common import common_params
 from dragonflow.common import exceptions as df_exceptions
 from dragonflow.common import utils as df_utils
@@ -39,12 +40,16 @@ usage_str = "The following commands are supported:\n" \
 
 
 def print_tables():
-    print(' ')
-    print('DB Tables')
-    print('----------')
+    tables = []
+    columns = ['table']
     for table in db_tables:
-        print table
-    print(' ')
+        tables.append({'table': table})
+
+    labels, formatters = \
+        cli_utils._get_list_table_columns_and_formatters(columns, tables)
+    labels[0] = 'DB Tables'
+    cli_utils.print_list(tables, columns, formatters=formatters,
+                         field_labels=labels)
 
 
 def print_table(db_driver, table):
@@ -52,12 +57,17 @@ def print_table(db_driver, table):
         keys = db_driver.get_all_keys(table)
     except df_exceptions.DBKeyNotFound:
         keys = []
-    print(' ')
-    print('Keys for table ' + table)
-    print('------------------------------------------------------------')
+
+    l = 0
     for key in keys:
-        print key
-    print(' ')
+        keys[l] = {'key': key}
+        l += 1
+
+    labels, formatters = \
+        cli_utils._get_list_table_columns_and_formatters(['key'], keys)
+    labels[0] = 'Keys for table'
+    cli_utils.print_list(keys, ['key'], formatters=formatters,
+                         field_labels=labels)
 
 
 def print_whole_table(db_driver, table):
@@ -66,15 +76,28 @@ def print_whole_table(db_driver, table):
     except df_exceptions.DBKeyNotFound:
         print('Table not found: ' + table)
         return
-    print(' ')
-    print('------------------------------------------------------------')
-    print('Table = ' + table)
-    print('------------------------------------------------------------')
+
+    values = []
     for key in keys:
         value = db_driver.get_key(table, key)
-        if value:
-            print('Key = ' + key + ' , Value = ' + value)
-    print(' ')
+        values.append(jsonutils.loads(value))
+
+    if isinstance(values[0], dict):
+        columns = values[0].keys()
+        labels, formatters = \
+            cli_utils._get_list_table_columns_and_formatters(columns, values)
+        cli_utils.print_list(values, columns, formatters=formatters,
+                             field_labels=labels)
+    elif isinstance(values[0], int):
+        l = 0
+        for value in values:
+            values[l] = {table: value}
+            l += 1
+        columns = [table]
+        labels, formatters = \
+            cli_utils._get_list_table_columns_and_formatters(columns, values)
+        cli_utils.print_list(values, columns, formatters=formatters,
+                             field_labels=columns)
 
 
 def print_key(db_driver, table, key):
@@ -83,11 +106,8 @@ def print_key(db_driver, table, key):
     except df_exceptions.DBKeyNotFound:
         print('Key not found: ' + table)
         return
-    print(' ')
-    print('Table = ' + table + ' , Key = ' + key)
-    print('------------------------------------------------------------')
-    print value
-    print(' ')
+
+    cli_utils.print_dict(jsonutils.loads(value))
 
 
 def bind_port_to_localhost(db_driver, port_id):
