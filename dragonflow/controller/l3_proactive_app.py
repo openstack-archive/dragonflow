c355a5a557a403388d5bdd1129e7822fb8080d0f
Hong Hui Xiao <honghui_xiao@yeah.net>
2016-11-05 18:35:04 +0800

    Store local network id to nb db
    Currently, the local network id is stored in the df controller cache. Everytime df
    controller restarts, the local network id will be re-generated. local network id
    is used as the network identifier in OpenFlow(the metadata field). If it changes
    after df controller restart, there might be duplicated flows, for example [1].
    
    This patch stores the local network id as unique key in df nb db. So that, even if
    df controller restarts, the local network id of network will not change.
    
    [1] http://paste.openstack.org/show/586501/
    
    Change-Id: Iaea5a95a996c4b4b90fffcf89c8a9dd2eaaa46a1
    Partial-Bug: #1633082

--- dragonflow/controller/l3_proactive_app.py
+++ dragonflow/controller/l3_proactive_app.py
@@ -88,9 +88,8 @@ class L3ProactiveApp(df_base_app.DFlowApp):
     def _add_new_router_port(self, router, router_port):
         LOG.info(_LI("Adding new logical router interface = %s"),
                  router_port)
-        local_network_id = self.db_store.get_network_id(
-            router_port.get_lswitch_id()
-        )
+        local_network_id = self.db_store.get_unique_key_by_id(
+            'lswitchs', router_port.get_lswitch_id())
         datapath = self.get_datapath()
         parser = datapath.ofproto_parser
         ofproto = datapath.ofproto
@@ -152,9 +151,8 @@ class L3ProactiveApp(df_base_app.DFlowApp):
         for port in router.get_ports():
             if port.get_id() != router_port.get_id():
 
-                port_net_id = self.db_store.get_network_id(
-                    port.get_lswitch_id(),
-                )
+                port_net_id = self.db_store.get_unique_key_by_id(
+                    'lswitchs', port.get_lswitch_id())
 
                 # From this router interface to all other interfaces
                 self._add_subnet_send_to_proactive_routing(
@@ -429,9 +427,8 @@ class L3ProactiveApp(df_base_app.DFlowApp):
     def _delete_router_port(self, router_port):
         LOG.info(_LI("Removing logical router interface = %s"),
                  router_port)
-        local_network_id = self.db_store.get_network_id(
-            router_port.get_lswitch_id()
-        )
+        local_network_id = self.db_store.get_unique_key_by_id(
+            'lswitchs', router_port.get_lswitch_id())
 
         parser = self.get_datapath().ofproto_parser
         ofproto = self.get_datapath().ofproto
