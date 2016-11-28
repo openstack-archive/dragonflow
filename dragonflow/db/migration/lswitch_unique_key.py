62cc29f310a68d56ed26e51ed7c8eb0fc9fad370
Hong Hui Xiao <honghui_xiao@yeah.net>
2016-11-28 14:27:51 +0800

    Add nb db migration mechanism and apply to lswitch unique_key
    This patch adds the nb db migration mechanism by providing a new
    command *df-db upgrade*
    
    unique_key is a new field for lswitch since [1], for the data that
    exists before [1]. It will not have unique_key, and will break the
    usecase of unique_key for lswitch.
    
    This patch also applies the db migration to unique_key of lswitch.
    
    [1] https://review.openstack.org/#/c/394017/
    
    Change-Id: Ibe1f04c4e8c09a706b4160a2fd729deafb408752
    Closes-Bug: #1644111

---
+++ dragonflow/db/migration/lswitch_unique_key.py
@@ -0,0 +1,36 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from dragonflow.db.migration import common
+from dragonflow.db import models as db_models
+
+VERSION = 1
+
+DESCRIPTION = "Add unique key to lswitch."
+
+OPENSTACK_VERSION = common.OCATA
+
+DATE = "2016-11-28 12:00"
+
+
+def upgrade(db_driver):
+    """Check if the metadata has newer version. If not, upgrade it."""
+
+    cur_ver_num = common.get_current_db_version(db_driver)
+    if cur_ver_num is not None and cur_ver_num >= VERSION:
+        return
+
+    # Do the migration
+    for lswitch_value in db_driver.get_all_entries(
+            db_models.LogicalSwitch.table_name):
+        lswitch = db_models.LogicalSwitch(lswitch_value)
+        common.ensure_unique_key(db_driver, lswitch)
