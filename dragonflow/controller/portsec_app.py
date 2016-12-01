95360b2b2732f1e8b33498fe20e19cbd5a9775d4
hujie <mike.hu@huawei.com>
2016-12-01 17:32:15 +0800

    Uniform constants in df
    There are many places that use definite number to identify
    some constant values, we should uniform them to use constant.
    
    Change-Id: I12ba06a691d54ff5748a5a5dbf037a2faa97fd4d

--- dragonflow/controller/portsec_app.py
+++ dragonflow/controller/portsec_app.py
@@ -24,11 +24,6 @@ from dragonflow.controller import df_base_app
 
 LOG = log.getLogger(__name__)
 
-ARP_OP_TYPE_REQUEST = 1
-UDP_PROTOCOL_NUMBER = 17
-DHCP_CLIENT_PORT = 68
-DHCP_SERVER_PORT = 67
-
 
 class PortSecApp(df_base_app.DFlowApp):
 
@@ -157,11 +152,11 @@ class PortSecApp(df_base_app.DFlowApp):
         # DHCP packets with the vm mac pass
         match = parser.OFPMatch(in_port=ofport,
                                 eth_src=vm_mac,
-                                eth_dst="ff:ff:ff:ff:ff:ff",
+                                eth_dst=const.BROADCAST_MAC,
                                 eth_type=ether.ETH_TYPE_IP,
-                                ip_proto=UDP_PROTOCOL_NUMBER,
-                                udp_src=68,
-                                udp_dst=67)
+                                ip_proto=const.UDP_PROTOCOL_NUMBER,
+                                udp_src=const.DHCP_CLIENT_PORT,
+                                udp_dst=const.DHCP_SERVER_PORT)
         self.add_flow_go_to_table(datapath,
                                   const.EGRESS_PORT_SECURITY_TABLE,
                                   const.PRIORITY_HIGH,
@@ -172,7 +167,7 @@ class PortSecApp(df_base_app.DFlowApp):
         match = parser.OFPMatch(in_port=ofport,
                                 eth_src=vm_mac,
                                 eth_type=ether.ETH_TYPE_ARP,
-                                arp_op=ARP_OP_TYPE_REQUEST,
+                                arp_op=const.ARP_OP_TYPE_REQUEST,
                                 arp_spa=0,
                                 arp_sha=vm_mac)
         self.add_flow_go_to_table(datapath,
@@ -187,11 +182,11 @@ class PortSecApp(df_base_app.DFlowApp):
         # Remove DHCP packets with the vm mac pass
         match = parser.OFPMatch(in_port=ofport,
                                 eth_src=vm_mac,
-                                eth_dst="ff:ff:ff:ff:ff:ff",
+                                eth_dst=const.BROADCAST_MAC,
                                 eth_type=ether.ETH_TYPE_IP,
-                                ip_proto=UDP_PROTOCOL_NUMBER,
-                                udp_src=68,
-                                udp_dst=67)
+                                ip_proto=const.UDP_PROTOCOL_NUMBER,
+                                udp_src=const.DHCP_CLIENT_PORT,
+                                udp_dst=const.DHCP_SERVER_PORT)
         self._remove_one_port_security_flow(datapath,
                                             const.PRIORITY_HIGH,
                                             match)
@@ -200,7 +195,7 @@ class PortSecApp(df_base_app.DFlowApp):
         match = parser.OFPMatch(in_port=ofport,
                                 eth_src=vm_mac,
                                 eth_type=ether.ETH_TYPE_ARP,
-                                arp_op=ARP_OP_TYPE_REQUEST,
+                                arp_op=const.ARP_OP_TYPE_REQUEST,
                                 arp_spa=0,
                                 arp_sha=vm_mac)
         self._remove_one_port_security_flow(datapath,
