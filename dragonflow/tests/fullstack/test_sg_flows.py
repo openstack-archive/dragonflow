7a2273921c05f5a673cc48168d4f4c43cca9c5bf
hujie <mike.hu@huawei.com>
2016-12-01 17:32:15 +0800

    Uniform constants in df
    There are many places that use definite number to identify
    some constant values, we should uniform them to use constant.
    
    Change-Id: I12ba06a691d54ff5748a5a5dbf037a2faa97fd4d

--- dragonflow/tests/fullstack/test_sg_flows.py
+++ dragonflow/tests/fullstack/test_sg_flows.py
@@ -12,6 +12,7 @@
 
 import time
 
+from neutron_lib import constants as n_const
 from oslo_log import log
 
 from dragonflow._i18n import _LI
@@ -369,7 +370,7 @@ class TestOVSFlowsForSecurityGroup(test_base.DFTestBase):
 
         egress_rule_info = {'ethertype': 'IPv4',
                             'direction': 'egress',
-                            'protocol': '17',
+                            'protocol': str(n_const.PROTO_NUM_UDP),
                             'port_range_min': '53',
                             'port_range_max': '53',
                             'remote_group_id': security_group_id}
