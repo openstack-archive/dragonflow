d675f6b75f5faafb22d34544f0c7ac3e9362408a
Will <wangjian38@huawei.com>
2016-12-03 22:06:33 +0800

    Refactor port-status-update NB API
    1. Before this patch, when there are more than one neutron-server
    workers on a single node, every worker holds a port_status_notifier
    instance and each of them subscribes to the same topic because they
    are on the same node. This is not necessary.
    
    This patch makes sure there is only one neutron-server worker
    subscribe to the topic(hostname of the node) no matter how many
    workers are created. Say this neutron-server is neutron listener
    on the node
    
    2. Before this patch, neutron-servers only register itself to df-db,
    but not update it with a timestamp periodically. When there is a
    event(e.g port-status up/down) to send, the df controller chooses
    a neutron-server randomly without caring it is dead or alive.
    
    This patch makes the neutron listeners reports its heart-beat with
    a timestamp periodically. When a df controller wants to send a
    notification, it gets all the listeners from df-db and sort them by
    the timestamp, then chooses from the lastest 50% neutron listeners.
    This can help to avoid chooing a dead one as far as possible
    
    For now, we don't remove neutron listeners from df-db automatically.
    But admins can do it manually.
    
    Change-Id: I9f643e95dc4da1d9ff5b2ee27729a293ff7f47e6
    Partially-implements: blueprint refactor-port-status-update
    Closes-Bug: #1636714

--- dragonflow/db/neutron/lockedobjects_db.py
+++ dragonflow/db/neutron/lockedobjects_db.py
@@ -48,7 +48,7 @@ RESOURCE_ML2_SECURITY_GROUP_RULE_DELETE = 6
 RESOURCE_FIP_UPDATE_OR_DELETE = 7
 RESOURCE_ROUTER_UPDATE_OR_DELETE = 8
 RESOURCE_QOS = 9
-
+RESOURCE_NEUTRON_LISTENER = 10
 
 LOG = log.getLogger(__name__)
 
@@ -117,8 +117,12 @@ def _get_lock_id_by_resource_type(resource_type, *args, **kwargs):
         lock_id = args[1]['security_group_id']
     elif RESOURCE_QOS == resource_type:
         lock_id = args[0][2]['id']
+    elif RESOURCE_NEUTRON_LISTENER == type:
+        lock_id = args[1]
     else:
         raise df_exc.UnknownResourceException(resource_type=resource_type)
+    if not lock_id:
+        lock_id = GLOBAL_LOCK_ID
 
     return lock_id
 
