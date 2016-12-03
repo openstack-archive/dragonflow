6126462f32cb66030b29ce47b110552a2c3ed5cc
Will <wangjian38@huawei.com>
2016-12-03 22:06:33 +0800

    Refactor port-status-update NB API
    1. Before this patch, when there are more than one neutron-server
    workers on a single node, every each worker holds a port_status_notifier
    instance and each of them subscribes the same topic because they
    are on the same node. This is not necessary.
    
    This patch makes sure there is only one neutron-server worker
    subscribe the topic(hostname of the node) no matter how many
    workers are created. Say this neutron-server is neutron listener
    on the node
    
    2. Before this patch, neutron-servers only register itself to df-db,
    but not update it with a timestamp periodically. When there is a
    event(e.g port-status up/down) to send, the df controller chooses
    a neutron-server randomly without caring it is dead or alive.
    
    This patch makes the neutron listeners reports its heart-beat with
    a timestamp periodically. When a df controller wants to send a
    notification, it gets all the listeners from df-db and sort them by
    the timestamp, then chooses from the lastest 50% neutron listeners.
    This can help to avoid chooing a dead one as far as possible
    
    For now, we don't remove neutron listeners from df-db automatically.
    But admins can do it manually.
    
    Change-Id: I9f643e95dc4da1d9ff5b2ee27729a293ff7f47e6
    Partially-implements: blueprint refactor-port-status-update
    Closes-Bug: #1636714

--- dragonflow/tests/common/utils.py
+++ dragonflow/tests/common/utils.py
@@ -10,6 +10,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import six
+
 from neutron.agent.common import utils as agent_utils
 from neutron.common import utils as n_utils
 import re
@@ -59,11 +61,11 @@ def wait_until_none(predicate, timeout=const.DEFAULT_CMD_TIMEOUT,
 
 
 def print_command(full_args, run_as_root=False):
-    print '{}'.format(agent_utils.execute(
+    print ('{}'.format(agent_utils.execute(
         full_args,
         run_as_root=run_as_root,
         process_input=None,
-    ))
+    )))
 
 
 class OvsFlowsParser(object):
@@ -188,3 +190,14 @@ class OvsDBParser(object):
                 if iface_id == port_id:
                     return item.get('ofport', None)
         return None
+
+
+class empty_wrapper(object):
+    def __init__(self, type):
+        pass
+
+    def __call__(self, f):
+        @six.wraps(f)
+        def wrapped_f(*args, **kwargs):
+            return f(*args, **kwargs)
+        return wrapped_f
