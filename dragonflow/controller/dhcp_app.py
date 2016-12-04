106810d5db0e6f950ded08620086662fbae3659e
hujie <mike.hu@huawei.com>
2016-12-04 21:48:08 +0800

    Fix wrong dhcp option value
    Change-Id: Ib5dc5cb4b7b1d74b61d5b860954a3c2e103cb44a
    Closes-bug: #1647152

--- dragonflow/controller/dhcp_app.py
+++ dragonflow/controller/dhcp_app.py
@@ -208,12 +208,13 @@ class DHCPApp(df_base_app.DFlowApp):
                                   src=dhcp_server_address,
                                   proto=pkt_ipv4.proto))
         dhcp_ack_pkt.add_protocol(udp.udp(src_port=67, dst_port=68))
-        dhcp_ack_pkt.add_protocol(dhcp.dhcp(op=2, chaddr=pkt_ethernet.src,
-                                         siaddr=dhcp_server_address,
-                                         boot_file=dhcp_packet.boot_file,
-                                         yiaddr=lport.get_ip(),
-                                         xid=dhcp_packet.xid,
-                                         options=options))
+        dhcp_ack_pkt.add_protocol(dhcp.dhcp(op=DHCP_ACK,
+                                            chaddr=pkt_ethernet.src,
+                                            siaddr=dhcp_server_address,
+                                            boot_file=dhcp_packet.boot_file,
+                                            yiaddr=lport.get_ip(),
+                                            xid=dhcp_packet.xid,
+                                            options=options))
         return dhcp_ack_pkt
 
     def _create_dhcp_offer(self, pkt, dhcp_packet, lport):
