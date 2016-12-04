a73114764998a807b16d4c2a30d8d21fb5ba6996
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2016-12-04 15:07:44 +0200

    [11/13] ObjectRefresher: support new models
    Partially-Implements: bp refactor-nb-api
    Change-Id: I70e1115d987efe813a516bb7175ff2cf16e2fbab

--- dragonflow/controller/df_db_objects_refresh.py
+++ dragonflow/controller/df_db_objects_refresh.py
@@ -38,6 +38,12 @@ class DfObjectRefresher(object):
         """Reads the objects IDs from the cache."""
         self.object_ids_to_remove = set(self.cache_read_ids_callback(topic))
 
+    def get_id(self, obj):
+        try:
+            return obj.id
+        except AttributeError:
+            return obj.get_id()
+
     def update(self, topic=None):
         """Updates existing objects and marks obsolete ones for removal.
 
@@ -48,7 +54,7 @@ class DfObjectRefresher(object):
         """
         for obj in self.db_read_objects_callback(topic):
             self.cache_update_object_callback(obj)
-            obj_id = obj.get_id()
+            obj_id = self.get_id(obj)
             self.object_ids_to_remove.discard(obj_id)
 
     def delete(self):
@@ -105,6 +111,10 @@ def initialize_object_refreshers(df_controller):
                                    nb_api.get_active_ports,
                                    df_controller.update_activeport,
                                    df_controller.delete_activeport))
+
+
+def add_refresher(refresher):
+    items.append(refresher)
 
 
 def sync_local_cache_from_nb_db(topics=None):
