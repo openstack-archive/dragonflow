031d95f91a6ee322bbbd044b8cb5f1d5ba4b5bf9
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2016-12-05 16:30:16 +0200

    [WIP] Refactor lport northbound API methods
    Partially-implements: blueprint refactor-nb-api
    Change-Id: I5faff30a9baec2e1e5ffbe0f7a3cb1578f4ea11d
    Signed-off-by: Dima Kuznetsov <dima.kuznetsov@toganetworks.com>

--- dragonflow/neutron/services/l3_router_plugin.py
+++ dragonflow/neutron/services/l3_router_plugin.py
@@ -207,7 +207,7 @@ class DFL3RouterPlugin(service_base.ServicePluginBase,
                 # delete the stale floatingip port
                 try:
                     if floatingip_port:
-                        self.nb_api.delete_lport(floatingip_port['id'],
+                        self.nb_api.lport.delete(floatingip_port['id'],
                                                  floatingip_port['tenant_id'])
                 except df_exceptions.DBKeyNotFound:
                     pass
@@ -276,8 +276,7 @@ class DFL3RouterPlugin(service_base.ServicePluginBase,
         cidr = netaddr.IPNetwork(subnet['cidr'])
         network = "%s/%s" % (port['fixed_ips'][0]['ip_address'],
                              str(cidr.prefixlen))
-        logical_port = self.nb_api.get_logical_port(port['id'],
-                                                    port['tenant_id'])
+        logical_port = self.nb_api.lport.get(port['id'], port['tenant_id'])
 
         self.nb_api.add_lrouter_port(result['port_id'],
                                      result['id'],
