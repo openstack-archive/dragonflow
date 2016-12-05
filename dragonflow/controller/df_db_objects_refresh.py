f24b898040979af9840d2e8bea7dcd42499f5d24
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2016-12-05 18:19:47 +0200

    [WIP] Refactor secgroup northbound API methods
    Partially-implements: blueprint refactor-nb-api
    Change-Id: Icfe34dbfc5bc48273763d05f4c7341d3ba7a9659
    Signed-off-by: Dima Kuznetsov <dima.kuznetsov@toganetworks.com>

--- dragonflow/controller/df_db_objects_refresh.py
+++ dragonflow/controller/df_db_objects_refresh.py
@@ -82,7 +82,7 @@ def initialize_object_refreshers(df_controller):
                                    df_controller.logical_switch_deleted))
     items.append(DfObjectRefresher('Security Groups',
                                    db_store.get_security_group_keys,
-                                   nb_api.get_security_groups,
+                                   nb_api.security_group.get_all,
                                    df_controller.security_group_updated,
                                    df_controller.security_group_deleted))
     items.append(DfObjectRefresher('Ports',
