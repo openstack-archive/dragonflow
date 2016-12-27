3dc743442db40bb785d194364e5b197ef495a3c1
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2016-12-27 14:52:10 +0200

    [02/13] Add namespace helper class
    Partially-implements: blueprint refactor-nb-api
    Change-Id: I1660fd6272dd7403dd315373e3096acf0743bad7

---
+++ dragonflow/utils/namespace.py
@@ -0,0 +1,49 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import six
+
+
+class Namespace(object):
+    '''A class that accepts keyword parameters on creation, and exposes access
+       through attributes
+
+       >>> ns = Namespace(a=1, b=2)
+       >>> ns.a
+       1
+       >>> ns.b
+       2
+       >>> tuple(ns)
+       (('a', 1), ('b', 2))
+
+    '''
+    def __init__(self, **kwargs):
+        self._dict = {}
+
+        for key, value in six.iteritems(kwargs):
+            self._dict[key] = value
+
+    def __iter__(self):
+        for key, value in six.iteritems(self._dict):
+            yield key, value
+
+    def impose_over(self, other):
+        for key, value in other:
+            if key not in self._dict:
+                self._dict[key] = value
+
+    def __getattr__(self, key):
+        if key in self._dict:
+            return self._dict[key]
+        raise AttributeError(key)
+
+    def copy(self):
+        return Namespace(**self._dict)
