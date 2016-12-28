3da98ce4a808d0c05caaf4f78c1f50efdf26c2f2
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2016-12-28 15:45:12 +0200

    [04/12] Add model-proxy helpers
    Added a utility to create 'proxy' objects to model instances.
    This is useful for models that want to refer to other models by 'id'
    (foreign key). The proxy is initialized by the model type and the id
    then can be used just like the object itself. It is useful for defining
    a reference-by-id field type (in a subsequent patch) that contains only
    an ID but lets you access the linked object as if it was nested model.
    
    Partially-Implements: blueprint refactor-nb-api
    Change-Id: I8af053f21af073370c18a573bfdf39c8ad15acd4

---
+++ dragonflow/tests/unit/test_model_proxy.py
@@ -0,0 +1,68 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from jsonmodels import fields
+import mock
+
+import dragonflow.db.model_framework as mf
+from dragonflow.db import model_proxy
+from dragonflow.tests import base as tests_base
+
+
+@mf.construct_nb_db_model
+class ModelTest(mf.ModelBase):
+    id = fields.StringField()
+    topic = fields.StringField()
+
+
+ModelTestProxy = model_proxy.create_model_proxy(ModelTest)
+
+
+class TestObjectProxy(tests_base.BaseTestCase):
+    def setUp(self):
+        super(TestObjectProxy, self).setUp()
+        self.db_store2 = mock.MagicMock()
+        self.get_inst_mock = mock.patch(
+            'dragonflow.db.db_store2.get_instance',
+            return_value=self.db_store2
+        )
+        self.get_inst_mock.start()
+
+    def tearDown(self):
+        super(TestObjectProxy, self).tearDown()
+        self.get_inst_mock.stop()
+
+    def test_proxied_attrs(self):
+        self.db_store2.get_one.return_value = ModelTest(
+            id='id1', topic='topic1')
+
+        mtp = ModelTestProxy(id='id1')
+        self.assertEqual('id1', mtp.id)
+        self.assertEqual('topic1', mtp.topic)
+        self.db_store2.get_one.assert_called_once_with(ModelTest(id='id1'))
+
+    def test_lazyness(self):
+        self.db_store2.get_one.return_value = ModelTest(
+            id='id1', topic='topic1')
+
+        mtp = ModelTestProxy(id='id1')
+        self.assertEqual('id1', mtp.id)
+        self.db_store2.get_one.assert_not_called()
+
+        self.assertEqual('topic1', mtp.topic)
+        self.db_store2.get_one.assert_called_once_with(ModelTest(id='id1'))
+
+    def test_eagerness(self):
+        self.db_store2.get_one.return_value = ModelTest(
+            id='id1', topic='topic1')
+
+        ModelTestProxy(id='id1', lazy=False)
+        self.db_store2.get_one.assert_called_once_with(ModelTest(id='id1'))
