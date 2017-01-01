556264efbd491c026d9e8930bd5fe1e1f666181b
rajiv <rajiv.kumar@nectechnologies.in>
2017-01-01 22:49:12 +0530

    Added support for service status reporting
    Partially-implements: blueprint services-status
    
    Change-Id: I5f78fe0c1cfe2f9bf11ee9a9b42b9f7a909a2cb3

---
+++ dragonflow/controller/service.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2015 OpenStack Foundation.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import functools
+
+from dragonflow.common import report_status
+from dragonflow import conf as cfg
+from dragonflow.db import api_nb
+from dragonflow.db.models import service
+
+
+class Service(object):
+    def __init__(self, binary):
+        self.nb_api = api_nb.NbApi.get_instance(False)
+        chassis_id = cfg.CONF.host
+        self.nb_api.create(service.Service(chassis=chassis_id, binary=binary))
+        callback = functools.partial(service.Service.update_last_seen,
+                                     self.nb_api, chassis_id, binary)
+        report_status.run_status_reporter(callback)
