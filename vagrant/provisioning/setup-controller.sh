904590f921dfeaacddea435b477753d8914994b1
liuxinrj <liuxinrj@inspur.com>
2017-01-13 16:07:30 +0800

    Fix failure while installing devstack by vagrant.
    When use vagrant to install devstack envrionment,
    configuration in local.conf did not work.
    Modify shell scripts to add `[[local|localrc]]` in local.conf.
    
    Co-Authored-By: Shachar Snapiri <shachar.snapiri@huawei.com>
    Change-Id: I0c1df8117393c5c65ce93c00604bca1ee7b38d20
    Closes-bug: #1655939

--- vagrant/provisioning/setup-controller.sh
+++ vagrant/provisioning/setup-controller.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
-cp /dragonflow/doc/source/multi-node-conf/controller_node_local_controller.conf ~/devstack/local.conf
+cp /dragonflow/doc/source/multi-node-conf/etcd_controller_node.conf ~/devstack/local.conf
 
 # Get the IP address
-ipaddress=$(/sbin/ifconfig eth1 | grep 'inet addr' | awk -F' ' '{print $2}' | awk -F':' '{print $2}')
+ipaddress=$(ifconfig eth1 2>/dev/null|awk '/inet addr:/ {split($2,a,":"); print a[2]}')
+
+SED_SCRIPT="s/^\(HOST_IP\)=.*/\1=$ipaddress/g"
+SED_SCRIPT="$SED_SCRIPT;/TUNNEL_ENDPOINT_IP/d"
+SED_SCRIPT="$SED_SCRIPT;s/df-zmq-publisher-service/df-etcd-pubsub-service/g"
+
+sed -i -e "$SED_SCRIPT" devstack/local.conf
 
 # Adjust some things in local.conf
 cat << DEVSTACKEOF >> devstack/local.conf
@@ -13,10 +19,13 @@ NETWORK_GATEWAY=10.100.100.100
 FIXED_RANGE=10.100.100.0/24
 
 # Good to set these
-HOST_IP=$ipaddress
 HOSTNAME=$(hostname)
 SERVICE_HOST_NAME=${HOSTNAME}
 SERVICE_HOST=$ipaddress
 DEVSTACKEOF
+
+# Patch to enable IPv6
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=0
+sudo echo "net.ipv6.conf.all.disable_ipv6 = 0" >> /etc/sysctl.conf
 
 ~/devstack/stack.sh
