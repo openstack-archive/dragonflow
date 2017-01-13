5b57a4d7d16cac062c06a7b5bf5c37809ef2a5a7
liuxinrj <liuxinrj@inspur.com>
2017-01-13 16:07:30 +0800

    Fix failure while installing devstack by vagrant.
    Changed box for the different hypervisors to ubuntu 16.04LTS (xenial).
    Updated the etcd checksum, as we are using a different version.
    Changed the VM creation to be serial, as the compute nodes must be
    started after the controller node is already up.
    Updated the script to take the correct configuration file as their
    names were changed over time.
    Changed configuration to use etcd entirely.
    
    Co-Authored-By: Shachar Snapiri <shachar.snapiri@huawei.com>
    Change-Id: I0c1df8117393c5c65ce93c00604bca1ee7b38d20
    Closes-bug: #1655939

--- vagrant/provisioning/setup-compute.sh
+++ vagrant/provisioning/setup-compute.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
-cp /dragonflow/doc/source/multi-node-conf/compute_node_local_controller.conf ~/devstack/local.conf
-if [ "$1" != "" ]; then
-    sed -i -e 's/<IP address of host running everything else>/'$1'/g' ~/devstack/local.conf
-fi
+cp /dragonflow/doc/source/multi-node-conf/etcd_compute_node.conf ~/devstack/local.conf
 
 # Get the IP address
-ipaddress=$(/sbin/ifconfig eth1 | grep 'inet addr' | awk -F' ' '{print $2}' | awk -F':' '{print $2}')
+ipaddress=$(ifconfig eth1 2>/dev/null|awk '/inet addr:/ {split($2,a,":"); print a[2]}')
+
+SED_SCRIPT="s/^\(HOST_IP\)=.*/\1=$ipaddress/g"
+SED_SCRIPT="$SED_SCRIPT;/TUNNEL_ENDPOINT_IP/d"
+SED_SCRIPT="$SED_SCRIPT;s/^\(SERVICE_HOST\)=.*/\1=$1/g"
+SED_SCRIPT="$SED_SCRIPT;s/zmq_pubsub_driver/etcd_pubsub_driver/g"
+
+sed -i -e "$SED_SCRIPT" devstack/local.conf
 
 # Adjust some things in local.conf
 cat << DEVSTACKEOF >> devstack/local.conf
 
 # Set this to the address of the main DevStack host running the rest of the
 # OpenStack services.
-Q_HOST=$1
-HOST_IP=$ipaddress
 HOSTNAME=$(hostname)
 DEVSTACKEOF
+
+# Patch to enable IPv6
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=0
+sudo echo "net.ipv6.conf.all.disable_ipv6 = 0" >> /etc/sysctl.conf
 
 ~/devstack/stack.sh
