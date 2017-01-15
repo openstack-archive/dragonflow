24372bc35be96b1e4cec70c94daebcf29c6647c9
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2017-01-15 16:39:32 +0200

    [8/8] Refactor Chassis model
    Partially-Implements: bp refactor-nb-api
    Change-Id: Id5acc31f5e8bc7a97de536938939224ec903e0a3

--- dragonflow/db/models2.py
+++ dragonflow/db/models2.py
@@ -11,6 +11,7 @@
 #    under the License.
 from jsonmodels import fields
 
+import dragonflow.db.field_types as df_fields
 import dragonflow.db.model_framework as mf
 from dragonflow.utils import namespace
 
@@ -42,3 +43,12 @@ class UniqueKeyMixin(mf.MixinBase):
         # FIXME get nb_api and allocate ID
         super(UniqueKeyMixin, self).on_create()
 
+
+@mf.register_model
+@mf.construct_nb_db_model
+class Chassis(NbModelBase, VersionMixin):
+    table_name = 'chassis'
+
+    ip = df_fields.IpAddressField(required=True)
+    tunnel_types = df_fields.EnumListField(('vxlan', 'gre', 'geneve'),
+                                           required=True)
