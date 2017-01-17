48c6a687964418da718aeef3c2209afdfbcdd6fa
itamaro <itamar.ofek@huawei.com>
2017-01-17 11:35:06 +0200

    classifier application
    patch include unit test for classifier_app
    Partially-implements: blueprint calssification_app
    
    Change-Id: Ie80eda71abfee3ccbca9300cab0f8c1cc52bf627

---
+++ dragonflow/controller/classifier_app.py
@@ -0,0 +1,86 @@
+# Copyright (c) 2016 OpenStack Foundation.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from dragonflow.controller.common import constants as const
+from dragonflow.controller import df_base_app
+from oslo_log import log
+
+
+LOG = log.getLogger(__name__)
+
+
+class Classifier(df_base_app.DFlowApp):
+    def __init__(self, *args, **kwargs):
+        super(Classifier, self).__init__(*args, **kwargs)
+
+    def add_local_port(self, lport):
+        ofport = lport.get_external_value('ofport')
+        self._make_ingress_classification_flow(lport, ofport)
+        self._make_ingress_dispatch_flow(lport, ofport)
+
+    def _make_ingress_dispatch_flow(self, lport,
+                                    ofport):
+        port_key = lport.get_unique_key()
+        match = self.parser.OFPMatch(reg7=port_key)
+        actions = [self.parser.OFPActionOutput(ofport,
+                                          self.ofproto.OFPCML_NO_BUFFER)]
+        action_inst = self.parser.OFPInstructionActions(
+            self.ofproto.OFPIT_APPLY_ACTIONS, actions)
+        inst = [action_inst]
+        self.mod_flow(
+            inst=inst,
+            table_id=const.INGRESS_DISPATCH_TABLE,
+            priority=const.PRIORITY_MEDIUM,
+            match=match)
+
+    def _make_ingress_classification_flow(self, lport, ofport):
+        match = self.parser.OFPMatch(in_port=ofport)
+        network_id = lport.get_external_value('local_network_id')
+        actions = [
+            self.parser.OFPActionSetField(reg6=lport.get_unique_key()),
+            self.parser.OFPActionSetField(metadata=network_id)]
+        action_inst = self.parser.OFPInstructionActions(
+                self.ofproto.OFPIT_APPLY_ACTIONS, actions)
+
+        goto_inst = self.parser.OFPInstructionGotoTable(
+            const.EGRESS_PORT_SECURITY_TABLE)
+        inst = [action_inst, goto_inst]
+        self.mod_flow(
+            inst=inst,
+            table_id=const.INGRESS_CLASSIFICATION_DISPATCH_TABLE,
+            priority=const.PRIORITY_MEDIUM,
+            match=match)
+
+    def remove_local_port(self, lport):
+        self._del_ingress_dispatch_flow(lport)
+        self._del_ingress_classification_flow(lport)
+
+    def _del_ingress_dispatch_flow(self, lport):
+        port_key = lport.get_unique_key()
+        match = self.parser.OFPMatch(reg7=port_key)
+        self.mod_flow(
+            table_id=const.INGRESS_DISPATCH_TABLE,
+            command=self.ofproto.OFPFC_DELETE,
+            priority=const.PRIORITY_MEDIUM,
+            match=match)
+
+    def _del_ingress_classification_flow(self, lport):
+        ofport = lport.get_external_value('ofport')
+        match = self.parser.OFPMatch(in_port=ofport)
+        self.mod_flow(
+            table_id=const.INGRESS_CLASSIFICATION_DISPATCH_TABLE,
+            command=self.ofproto.OFPFC_DELETE,
+            priority=const.PRIORITY_MEDIUM,
+            match=match)
---
+++ dragonflow/tests/unit/test_classifier_app.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2016 OpenStack Foundation.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from dragonflow.controller.common import constants as const
+from dragonflow.tests.unit import test_app_base
+import mock
+
+make_fake_local_port = test_app_base.make_fake_local_port
+make_fake_logic_switch = test_app_base.make_fake_logic_switch
+
+
+class TestClassifierApp(test_app_base.DFAppTestBase):
+    apps_list = "classifier_app.Classifier"
+
+    def setUp(self):
+        super(TestClassifierApp, self).setUp()
+        fake_vlan_switch1 = make_fake_logic_switch(
+                subnets=test_app_base.fake_lswitch_default_subnets,
+                network_type='vlan',
+                id='fake_vlan_switch1', mtu=1500,
+                router_external=False, segmentation_id=41,
+                topic='fake_tenant1', unique_key=2,
+                name='private')
+        self.controller.update_lswitch(fake_vlan_switch1)
+        self.app = self.open_flow_app.dispatcher.apps[0]
+
+    def test_classifier_for_vlan_port(self):
+        fake_local_vlan_port = make_fake_local_port(network_type='vlan',
+                lswitch='fake_vlan_switch1')
+        port_key = fake_local_vlan_port.get_unique_key()
+        match = self.app.parser.OFPMatch(reg7=port_key)
+        self.controller.update_lport(fake_local_vlan_port)
+        self.app.mod_flow.assert_called_with(
+            inst=mock.ANY,
+            table_id=const.INGRESS_DISPATCH_TABLE,
+            priority=const.PRIORITY_MEDIUM,
+            match=match)
+        self.app.mod_flow.reset_mock()
+        ofport = fake_local_vlan_port.get_external_value('ofport')
+        match = self.app.parser.OFPMatch(in_port=ofport)
+        self.controller.delete_lport(fake_local_vlan_port.get_id())
+        self.app.mod_flow.assert_called_with(
+            table_id=const.INGRESS_CLASSIFICATION_DISPATCH_TABLE,
+            command=self.datapath.ofproto.OFPFC_DELETE,
+            priority=const.PRIORITY_MEDIUM,
+            match=match)
+        self.app.mod_flow.reset_mock()
