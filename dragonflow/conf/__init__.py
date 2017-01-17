3d1a3a8287480c1f745f8684f2c270ddfd59b51f
itamaro <itamar.ofek@huawei.com>
2017-01-17 17:16:29 +0200

    provider_networks_app
    patch include:
    L2 app modifcation as a result of having provider_networks app
    Unit test for provider-networks app and the provider_networks
    application.
    
    Change-Id: I390402cde57106c7076ab3d6cc932744d3c40bca
    Partially-implements: bp Provider-networks-app

--- dragonflow/conf/__init__.py
+++ dragonflow/conf/__init__.py
@@ -20,6 +20,7 @@ from dragonflow.conf import df_dnat
 from dragonflow.conf import df_l2
 from dragonflow.conf import df_l3
 from dragonflow.conf import df_metadata_service
+from dragonflow.conf import df_provider_networks
 from dragonflow.conf import df_ryu
 
 
@@ -35,3 +36,4 @@ df_l2.register_opts()
 df_l3.register_opts()
 df_dnat.register_opts()
 df_ryu.register_opts()
+df_provider_networks.register_opts()
