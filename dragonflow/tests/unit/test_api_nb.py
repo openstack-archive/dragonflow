e86092ab993aabee2d90f4b92e8bb5a3a728c27e
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2017-01-19 11:03:58 +0200

    NbApi: Use SEND_ALL_TOPIC when topic is omitted
    Some models do not have a specific topic for their objects (because they
    do not belong to a specific tenant), but it still makes sense to
    distribute them (e.g. a chassis is updated). This patch enables the
    distribution of None-topic objects resulting from northbound refactor.
    
    Change-Id: I3e5364e12baade924394a4113f620106f500422e

--- dragonflow/tests/unit/test_api_nb.py
+++ dragonflow/tests/unit/test_api_nb.py
@@ -12,11 +12,38 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+import mock
+
 from dragonflow.db import api_nb
+from dragonflow.db import db_common
 from dragonflow.tests import base as tests_base
 
 
 class TestNbApi(tests_base.BaseTestCase):
+    def setUp(self):
+        super(TestNbApi, self).setUp()
+        self.api_nb = api_nb.NbApi(
+            db_driver=mock.Mock(),
+            use_pubsub=True,
+            is_neutron_server=True
+        )
+        self.api_nb.publisher = mock.Mock()
+        self.api_nb.enable_selective_topo_dist = True
+
+    def test_topicless_send_event(self):
+        self.api_nb._send_db_change_event('table', 'key', 'action',
+                                          'value', None)
+        self.api_nb.publisher.send_event.assert_called()
+        update, = self.api_nb.publisher.send_event.call_args_list[0][0]
+        self.assertEqual(db_common.SEND_ALL_TOPIC, update.topic)
+
+    def test_send_event_with_topic(self):
+        self.api_nb._send_db_change_event('table', 'key', 'action',
+                                          'value', 'topic')
+        self.api_nb.publisher.send_event.assert_called()
+        update, = self.api_nb.publisher.send_event.call_args_list[0][0]
+        self.assertEqual('topic', update.topic)
+
     def test_get_instance_without_init(self):
         api_nb._nb_api = None
         self.assertRaises(RuntimeError, api_nb.NbApi.get_instance)
