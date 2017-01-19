22aaf3d5d70eb460c73a3af417bec40708bac877
itamaro <itamar.ofek@huawei.com>
2017-01-19 09:30:13 +0200

    Unit test for providier_networks application
    Change-Id: I411800cb25aa3570af0bad95ba8abeb6ce728b19

---
+++ dragonflow/tests/unit/test_provider_net_app.py
@@ -0,0 +1,69 @@
+# Copyright (c) 2016 OpenStack Foundation.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from dragonflow.controller.common import constants as const
+from dragonflow.tests.unit import test_app_base
+import mock
+
+
+make_fake_local_port = test_app_base.make_fake_local_port
+make_fake_logic_switch = test_app_base.make_fake_logic_switch
+make_fake_remote_port = test_app_base.make_fake_remote_port
+
+
+class TestProviderNetsApp(test_app_base.DFAppTestBase):
+    apps_list = "provider_networks_app.ProviderNetworks"
+
+    def setUp(self):
+        super(TestProviderNetsApp, self).setUp()
+        fake_vlan_switch1 = make_fake_logic_switch(network_type='vlan',
+                id='fake_vlan_switch1', mtu=1454,physical_network='phynet')
+        self.controller.update_lswitch(fake_vlan_switch1)
+        self.app = self.open_flow_app.dispatcher.apps[0]
+
+    def test_provider_vlan_port(self):
+        fake_local_vlan_port1 = make_fake_local_port(network_type='vlan',
+                lswitch='fake_vlan_switch1')
+        self.app.int_ofports['phynet'] = 1
+        self.controller.update_lport(fake_local_vlan_port1)
+        self.app.mod_flow.assert_called_with(
+            datapath=self.datapath,
+            inst=mock.ANY,
+            match=mock.ANY,
+            priority=const.PRIORITY_HIGH,
+            table_id=const.EGRESS_EXTERNAL_TABLE)
+        self.app.mod_flow.reset_mock()
+
+        fake_local_vlan_port2 = make_fake_local_port(network_type='vlan',
+                lswitch='fake_vlan_switch1',
+                macs=['1a:0b:0c:0d:0f:0f'],
+                ips=['10.0.0.112'],
+                ofport=12)
+        self.controller.update_lport(fake_local_vlan_port2)
+        self.app.mod_flow.assert_not_called()
+        self.app.mod_flow.reset_mock()
+
+        self.controller.delete_lport(fake_local_vlan_port1.get_id())
+        self.app.mod_flow.assert_not_called()
+        self.app.mod_flow.reset_mock()
+
+        self.controller.delete_lport(fake_local_vlan_port2.get_id())
+        self.app.mod_flow.assert_called_with(
+            datapath=self.datapath,
+            command=self.app.ofproto.OFPFC_DELETE,
+            table_id=const.EGRESS_EXTERNAL_TABLE,
+            priority=const.PRIORITY_HIGH,
+            match=mock.ANY)
+        self.app.mod_flow.reset_mock()
