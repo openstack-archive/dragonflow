d458c426a38508c47088ce9e54c869ae74dcf663
Hong Hui Xiao <honghui_xiao@yeah.net>
2017-01-22 16:53:21 +0800

    Add an uniform way to manage external network
    Since dragonflow only supports one external network, we can manage it
    in an uniform way. All the fullstack tests can use this external network.
    
    Change-Id: I0c09d4b9ce23935100d2568fe45fb2948c875b76
    Closes-bug: #1657705

--- dragonflow/tests/fullstack/test_object_version.py
+++ dragonflow/tests/fullstack/test_object_version.py
@@ -10,8 +10,6 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import contextlib
-
 from oslo_concurrency import lockutils
 
 from dragonflow.tests.fullstack import test_base
@@ -143,102 +141,74 @@ class TestObjectVersion(test_base.DFTestBase):
         qospolicy.close()
         self.assertFalse(qospolicy.exists())
 
-    @contextlib.contextmanager
-    def _prepare_ext_net(self):
-        external_net = objects.find_first_network(self.neutron,
-                                                  {'router:external': True})
-        if not external_net:
-            network = self.store(
-                objects.NetworkTestObj(self.neutron, self.nb_api))
-            external_net_para = {'name': 'public', 'router:external': True}
-            external_network_id = network.create(network=external_net_para)
-            self.assertTrue(network.exists())
-            ext_subnet = self.store(objects.SubnetTestObj(
-                self.neutron,
-                self.nb_api,
-                external_network_id,
-            ))
-            external_subnet_para = {'cidr': '192.168.199.0/24',
-                      'ip_version': 4, 'network_id': external_network_id}
-            ext_subnet.create(external_subnet_para)
-            self.assertTrue(ext_subnet.exists())
-        else:
-            external_network_id = external_net['id']
-        self.assertIsNotNone(external_network_id)
-
-        # return external network
-        yield external_network_id
-
-        if not external_net:
-            ext_subnet.close()
-            self.assertFalse(ext_subnet.exists())
-            network.close()
-            self.assertFalse(network.exists())
-
     @lockutils.synchronized('need-external-net')
     def test_floatingip_version(self):
-        with self._prepare_ext_net() as external_network_id:
-            private_network = self.store(
-                objects.NetworkTestObj(self.neutron, self.nb_api))
-            private_network_id = private_network.create(
-                network={'name': 'private'})
-            self.assertTrue(private_network.exists())
-            priv_subnet = self.store(objects.SubnetTestObj(
-                self.neutron,
-                self.nb_api,
-                private_network_id,
-            ))
-            router = self.store(
-                objects.RouterTestObj(self.neutron, self.nb_api))
-            port = self.store(
-                objects.PortTestObj(self.neutron,
+        external_net = objects.find_first_network(self.neutron,
+                                                  {'router:external': True})
+        self.assertIsNotNone(external_net)
+        external_network_id = external_net['id']
+
+        private_network = self.store(
+            objects.NetworkTestObj(self.neutron, self.nb_api))
+        private_network_id = private_network.create(
+            network={'name': 'private'})
+        self.assertTrue(private_network.exists())
+        priv_subnet = self.store(objects.SubnetTestObj(
+            self.neutron,
+            self.nb_api,
+            private_network_id,
+        ))
+        router = self.store(
+            objects.RouterTestObj(self.neutron, self.nb_api))
+        port = self.store(
+            objects.PortTestObj(self.neutron,
                                 self.nb_api, private_network_id))
-            fip = self.store(
-                objects.FloatingipTestObj(self.neutron, self.nb_api))
+        fip = self.store(
+            objects.FloatingipTestObj(self.neutron, self.nb_api))
 
-            router_para = {'name': 'myrouter1', 'admin_state_up': True,
-                 'external_gateway_info': {"network_id": external_network_id}}
-            router.create(router=router_para)
-            self.assertTrue(router.exists())
+        router_para = {'name': 'myrouter1', 'admin_state_up': True,
+             'external_gateway_info': {"network_id": external_network_id}}
+        router.create(router=router_para)
+        self.assertTrue(router.exists())
 
-            private_subnet_para = {'cidr': '10.0.0.0/24',
-                  'ip_version': 4, 'network_id': private_network_id}
-            priv_subnet_id = priv_subnet.create(private_subnet_para)
-            self.assertTrue(priv_subnet.exists())
-            router_interface = router.add_interface(subnet_id=priv_subnet_id)
-            router_lport = self.nb_api.get_logical_port(
-                router_interface['port_id'])
-            self.assertIsNotNone(router_lport)
+        private_subnet_para = {'cidr': '10.0.0.0/24',
+                'ip_version': 4, 'network_id': private_network_id}
+        priv_subnet_id = priv_subnet.create(private_subnet_para)
+        self.assertTrue(priv_subnet.exists())
+        router_interface = router.add_interface(subnet_id=priv_subnet_id)
+        router_lport = self.nb_api.get_logical_port(
+            router_interface['port_id'])
+        self.assertIsNotNone(router_lport)
 
-            port_id = port.create()
-            self.assertIsNotNone(port.get_logical_port())
+        port_id = port.create()
+        self.assertIsNotNone(port.get_logical_port())
 
-            fip_para = {'floating_network_id': external_network_id}
-            # create
-            new_fip = fip.create(fip_para)
-            self.assertTrue(fip.exists())
-            fip_id = new_fip['id']
-            prev_version = self.nb_api.get_floatingip(fip_id).get_version()
+        fip_para = {'floating_network_id': external_network_id}
+        # create
+        new_fip = fip.create(fip_para)
+        self.assertTrue(fip.exists())
+        fip_id = new_fip['id']
+        prev_version = self.nb_api.get_floatingip(fip_id).get_version()
 
-            # associate with port
-            fip.update({'port_id': port_id})
-            fip_obj = fip.get_floatingip()
-            self.assertEqual(fip_obj.get_lport_id(), port_id)
-            version = self.nb_api.get_floatingip(fip_id).get_version()
-            self.assertGreater(version, prev_version)
-            prev_version = version
+        # associate with port
+        fip.update({'port_id': port_id})
+        fip_obj = fip.get_floatingip()
+        self.assertEqual(fip_obj.get_lport_id(), port_id)
+        version = self.nb_api.get_floatingip(fip_id).get_version()
+        self.assertGreater(version, prev_version)
+        prev_version = version
 
-            fip.update({})
-            fip_obj = fip.get_floatingip()
-            self.assertIsNone(fip_obj.get_lport_id())
-            version = self.nb_api.get_floatingip(fip_id).get_version()
-            self.assertGreater(version, prev_version)
+        fip.update({})
+        fip_obj = fip.get_floatingip()
+        self.assertIsNone(fip_obj.get_lport_id())
+        version = self.nb_api.get_floatingip(fip_id).get_version()
+        self.assertGreater(version, prev_version)
 
-            fip.close()
-            self.assertFalse(fip.exists())
-            port.close()
-            self.assertFalse(port.exists())
-            router.close()
-            self.assertFalse(router.exists())
-            priv_subnet.close()
-            self.assertFalse(priv_subnet.exists())
+        fip.close()
+        self.assertFalse(fip.exists())
+        port.close()
+        self.assertFalse(port.exists())
+        router.close()
+        self.assertFalse(router.exists())
+        priv_subnet.close()
+        self.assertFalse(priv_subnet.exists())
