1bb92502b65b648d925583e67a49c05b6c06e663
Igor Shafran <igor.shafran@gmail.com>
2017-01-23 09:41:56 +0200

    SNAT application patch #2
    Change-Id: I587a4f8bfb0825a31cb926bf34eced6741406276

--- dragonflow/tests/unit/test_dnat_app.py
+++ dragonflow/tests/unit/test_dnat_app.py
@@ -15,6 +15,7 @@
 
 import mock
 
+from dragonflow.controller.common import constants
 from dragonflow.tests.unit import test_app_base
 
 
@@ -26,70 +27,30 @@ class TestDNATApp(test_app_base.DFAppTestBase):
         self.dnat_app = self.open_flow_app.dispatcher.apps[0]
         self.dnat_app.external_ofport = 99
 
-    def test_external_bridge_online(self):
+    def test_add_local_port(self):
         self.dnat_app.local_floatingips[
             test_app_base.fake_floatingip1.get_id()] = (
                 test_app_base.fake_floatingip1)
-
-        with mock.patch.object(self.dnat_app,
-                               '_install_dnat_egress_rules') as mock_func:
-            fake_ovs_port = mock.Mock()
-            fake_ovs_port.get_ofport = mock.Mock(return_value=-1)
-            fake_ovs_port.get_name = mock.Mock(
-                return_value=self.dnat_app.external_network_bridge)
-            # Device without mac will not trigger update flow
-            fake_ovs_port.get_mac_in_use = mock.Mock(return_value="")
-            self.controller.ovs_port_updated(fake_ovs_port)
-            mock_func.assert_not_called()
-            mock_func.reset_mock()
-
-            # Other device update will not trigger update flow
-            fake_ovs_port.get_mac_in_use = mock.Mock(
-                return_value="aa:bb:cc:dd:ee:ff")
-            fake_ovs_port.get_name = mock.Mock(return_value="no-bridge")
-            self.controller.ovs_port_updated(fake_ovs_port)
-            mock_func.assert_not_called()
-            mock_func.reset_mock()
-
-            # Device with mac will trigger update flow
-            fake_ovs_port.get_name = mock.Mock(
-                return_value=self.dnat_app.external_network_bridge)
-            self.controller.ovs_port_updated(fake_ovs_port)
-            mock_func.assert_called_once_with(test_app_base.fake_floatingip1,
-                                              "aa:bb:cc:dd:ee:ff")
-            mock_func.reset_mock()
-
-            # Duplicated updated will not trigger update flow
-            self.controller.ovs_port_updated(fake_ovs_port)
-            mock_func.assert_not_called()
-
-    def test_delete_port_with_deleted_floatingip(self):
         self.controller.update_lport(test_app_base.fake_local_port1)
-        self.controller.update_floatingip(test_app_base.fake_floatingip1)
-        self.controller.delete_floatingip(
-            test_app_base.fake_floatingip1.get_id())
 
-        self.assertFalse(self.dnat_app.local_floatingips)
+        # Assert calls have been placed
+        self.arp_responder.assert_called_once_with(
+            self.dnat_app, None,
+            test_app_base.fake_floatingip1.get_ip_address(),
+            test_app_base.fake_floatingip1.get_mac_address(),
+            constants.INGRESS_NAT_TABLE)
+        self.dnat_app.add_flow_go_to_table.assert_has_calls(
+            [mock.call(self.datapath,
+                       constants.INGRESS_CLASSIFICATION_DISPATCH_TABLE,
+                       constants.PRIORITY_LOW,
+                       constants.INGRESS_NAT_TABLE,
+                       match=mock.ANY),
+             mock.call(self.datapath,
+                       constants.L3_LOOKUP_TABLE,
+                       constants.PRIORITY_MEDIUM,
+                       constants.EGRESS_NAT_TABLE,
+                       match=mock.ANY)])
+        self.dnat_app.mod_flow.assert_called_once_with(
+            self.datapath, inst=mock.ANY, table_id=constants.INGRESS_NAT_TABLE,
+            priority=constants.PRIORITY_MEDIUM, match=mock.ANY)
 
-        with mock.patch.object(
-            self.dnat_app,
-            'delete_floatingip',
-        ) as mock_func:
-            self.dnat_app.remove_local_port(test_app_base.fake_local_port1)
-            mock_func.assert_not_called()
-
-    def test_floatingip_removed_only_once(self):
-        self.controller.update_lport(test_app_base.fake_local_port1)
-        self.controller.topology.ovs_port_updated(test_app_base.fake_ovs_port1)
-        self.controller.update_floatingip(test_app_base.fake_floatingip1)
-        self.controller.delete_floatingip(
-            test_app_base.fake_floatingip1.get_id())
-        self.controller.delete_lport(
-            test_app_base.fake_local_port1.get_id())
-        with mock.patch.object(
-            self.controller,
-            'delete_floatingip'
-        ) as mock_func:
-            self.controller.topology.ovs_port_deleted(
-                test_app_base.fake_ovs_port1.get_id())
-            mock_func.assert_not_called()
