fc97af7f137149adda1d82da88d8c3b13465d3f5
Omer Anson <omer.anson@toganetworks.com>
2017-02-01 13:08:34 +0200

    Refactor Aging application
    As part of the work on cookies throughout Dragonflow, it was decided
    that this application would benefit from moving cookie information to be
    private, rather than sit in an external utility module.
    
    Additionally, the code used to connect to the OVS was made available to
    all applications via the shared base class.
    
    Lastly, ryu's openflow service necessary for synchronous communication
    was moved to df-local-controller.
    
    Change-Id: Ia52f3e49b077090fb6d6264628a10bc4af93c664
    Related-Bug: #1471552

--- dragonflow/controller/aging.py
+++ dragonflow/controller/aging.py
@@ -13,27 +13,28 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import eventlet
 from oslo_log import log
 
 from dragonflow._i18n import _LI
 from dragonflow.controller.common import constants as const
 from dragonflow.controller.common import cookies
-from dragonflow.controller.common import utils as cookie
 from dragonflow.controller import df_base_app
-from dragonflow.controller import ofswitch
 
-LOG = log.getLogger("dragonflow.controller.aging")
+LOG = log.getLogger(__name__)
 
 
-class Aging(df_base_app.DFlowApp, ofswitch.OpenFlowSwitchMixin):
+AGING_COOKIE_NAME = 'aging'
+
+
+class Aging(df_base_app.DFlowApp):
 
     def __init__(self, *args, **kwargs):
-        df_base_app.DFlowApp.__init__(self, *args, **kwargs)
-        ofswitch.OpenFlowSwitchMixin.__init__(self, args[0])
-        self.aging_mask = const.GLOBAL_AGING_COOKIE_MASK
+        super(Aging, self).__init__(*args, **kwargs)
         self.do_aging = True
-        cookies.add_global_cookie_modifier('aging', 1,
-                                           lambda x: cookie.get_aging_cookie())
+        self._aging_cookie = 0
+        cookies.add_global_cookie_modifier(AGING_COOKIE_NAME, 1,
+                                           lambda x: self._aging_cookie)
 
     """
     check_aging_needed()
@@ -48,15 +49,19 @@ class Aging(df_base_app.DFlowApp, ofswitch.OpenFlowSwitchMixin):
     """
     def ovs_sync_started(self):
         LOG.info(_LI("start aging"))
-        canary_flow = self.get_canary_flow()
+        canary_flow = self._get_canary_flow()
         if not canary_flow:
             self.do_aging = False
-            cookie.set_aging_cookie(const.GLOBAL_INIT_AGING_COOKIE)
+            self._aging_cookie = 1
             LOG.info(_LI("no canary table, don't do aging"))
         else:
             self.do_aging = True
-            self._renew_aging_cookie(canary_flow.cookie)
-        self.add_canary_flow(cookie.get_aging_cookie())
+            canary_cookie = canary_flow.cookie
+            self._aging_cookie = self._invert_cookie(canary_cookie)
+            LOG.info(_LI("renew cookie, old: %(old)x new: %(new)x"),
+                     {'old': canary_cookie, 'new': self._aging_cookie})
+        # NOTE(oanson) cookie will be set automatically with _aging_cookie
+        self.mod_flow(table_id=const.CANARY_TABLE)
 
     """
     now all apps had flushed flows with new cookie
@@ -64,15 +69,30 @@ class Aging(df_base_app.DFlowApp, ofswitch.OpenFlowSwitchMixin):
     """
     def ovs_sync_finished(self):
         if self.do_aging:
-            self._start_aging()
-            LOG.info(_LI("do aging"))
+            # Give apps a few more seconds to finish their magic
+            eventlet.spawn_after(5, self._start_aging)
+            LOG.info(_LI("Scheduled aged flows deletion"))
+
+    def _invert_cookie(self, cookie):
+        return 1 ^ cookie
 
     def _start_aging(self):
-        old_cookie = cookie.get_xor_cookie(cookie.get_aging_cookie())
-        self.cleanup_flows(old_cookie, self.aging_mask)
+        inverted_cookie = self._invert_cookie(self._aging_cookie)
+        old_cookie, old_mask = cookies.get_cookie(AGING_COOKIE_NAME,
+                                                  inverted_cookie)
+        self._cleanup_flows(old_cookie, old_mask)
+        LOG.info(_LI("Scheduled aged flows deletion completed!"))
 
-    def _renew_aging_cookie(self, cur_c):
-        LOG.info(_LI("renew cookie, current cookie is %x"), cur_c)
-        new_c = cookie.get_xor_cookie(cur_c)
-        cookie.set_aging_cookie(new_c & self.aging_mask)
-        return cookie.get_aging_cookie()
+    def _cleanup_flows(self, cookie, cookie_mask):
+        self.mod_flow(cookie=cookie,
+                      cookie_mask=cookie_mask,
+                      table_id=self.ofproto.OFPTT_ALL,
+                      match=self.parser.OFPMatch(),
+                      command=self.ofproto.OFPFC_DELETE,
+                      priority=0)
+
+    def _get_canary_flow(self):
+        canary_flow = self.get_flows(table_id=const.CANARY_TABLE)
+        if not canary_flow:
+            return None
+        return canary_flow[0]
