8705713cc4428c682c3b07a95e16cdcd51ebd265
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2017-02-06 13:25:38 +0200

    [04/xx] Split L2 and egress tables
    Split L2 lookup and egress to intercept packets for classification
    
    Partially-implements: blueprint service-function-chaining
    Change-Id: I81c096331c25129552766ec109de5f357763320b

--- dragonflow/tests/unit/test_tunneling_app.py
+++ dragonflow/tests/unit/test_tunneling_app.py
@@ -101,7 +101,7 @@ class TestTunnelingApp(test_app_base.DFAppTestBase):
         self.app.mod_flow.assert_called_with(
             inst=inst,
             command=self.datapath.ofproto.OFPFC_ADD,
-            table_id=const.EGRESS_TABLE,
+            table_id=const.EGRESS_CONT_TABLE,
             priority=const.PRIORITY_LOW,
             match=match)
         self.app.mod_flow.reset_mock()
@@ -120,7 +120,7 @@ class TestTunnelingApp(test_app_base.DFAppTestBase):
         # The multicast flow will be modified to EGRESS_TABLE with priority low
         self.app.mod_flow.assert_called_with(
             inst=inst,
-            table_id=const.EGRESS_TABLE,
+            table_id=const.EGRESS_CONT_TABLE,
             command=self.datapath.ofproto.OFPFC_MODIFY,
             priority=const.PRIORITY_LOW,
             match=match)
@@ -132,7 +132,7 @@ class TestTunnelingApp(test_app_base.DFAppTestBase):
         # The multicast flow will be modified to EGRESS_TABLE with priority low
         self.app.mod_flow.assert_called_with(
             inst=inst,
-            table_id=const.EGRESS_TABLE,
+            table_id=const.EGRESS_CONT_TABLE,
             command=self.datapath.ofproto.OFPFC_MODIFY,
             priority=const.PRIORITY_LOW,
             match=match)
@@ -142,7 +142,7 @@ class TestTunnelingApp(test_app_base.DFAppTestBase):
         # The multicast flow will be deleted to EGRESS_TABLE with priority low
         self.app.mod_flow.assert_called_with(
             inst=None,
-            table_id=const.EGRESS_TABLE,
+            table_id=const.EGRESS_CONT_TABLE,
             command=self.datapath.ofproto.OFPFC_DELETE,
             priority=const.PRIORITY_LOW,
             match=match)
