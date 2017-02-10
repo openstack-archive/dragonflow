c66d4d46c2e54093e9c3fa7e327caaf751ec23d8
Hong Hui Xiao <honghui_xiao@yeah.net>
2017-02-10 21:48:15 +0800

    Add TTL invalid handler with rate limit(L3 app)
    Currently, the L3_LOOKUP_TABLE will decrease ttl when route an packet.
    This patch will create/reply an ICMP Time exceed message if the ttl
    is invalid after Decrement.
    
    Change-Id: Ibfacfd2d11fe31eaeb942528076486b574847f7c
    Partially-implements: blueprint traceroute-support

--- dragonflow/conf/__init__.py
+++ dragonflow/conf/__init__.py
@@ -18,6 +18,7 @@ from dragonflow.conf import df_common_params
 from dragonflow.conf import df_dhcp
 from dragonflow.conf import df_dnat
 from dragonflow.conf import df_l2
+from dragonflow.conf import df_l3
 from dragonflow.conf import df_metadata_service
 from dragonflow.conf import df_ryu
 
@@ -31,5 +32,6 @@ df_dhcp.register_opts()
 df_metadata_service.register_opts()
 df_active_port_detection.register_opts()
 df_l2.register_opts()
+df_l3.register_opts()
 df_dnat.register_opts()
 df_ryu.register_opts()
---
+++ dragonflow/conf/df_l3.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2017 Huawei Tech. Co., Ltd. .
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_config import cfg
+
+from dragonflow._i18n import _
+
+df_l3_app_opts = [
+    cfg.IntOpt('router_ttl_invalid_max_rate', default=3,
+               help=_('Max rate to reply ICMP time exceeded message per '
+                      'second.')),
+]
+
+
+def register_opts():
+    cfg.CONF.register_opts(df_l3_app_opts, group='df_l3_app')
+
+
+def list_opts():
+    return {'df_l3_app': df_l3_app_opts}
