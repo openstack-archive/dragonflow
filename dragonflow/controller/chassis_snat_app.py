c86484f4a66385cf9d23cddbcb1e8dc576810cd2
Igor Shafran <igor.shafran@gmail.com>
2017-02-12 14:47:12 +0200

    SNAT application - infra and chassis sNAT strategy
    This patch combines infrastructure required changes and
    new sNAT application (chassis based sNAT).
    There is another strategy mode (tenant sNAT to be delivered separately
    
    Chassis sNAT mode has 2 configuration parameters
     - external_host_ip (no default)
     - enable_goto_flows (default: True)
    
    -Added support for devstack SNAT_HOST_IP variable to
     set 'external_host_ip' parameter from devstack local.conf
    -Added mixin class instead common sNAT base class
    
    UpgradeImpact: OVS version>=2.6
    Partially-Implements: blueprint snat-distribution
    Change-Id: I7aadb10489d2bee7b2662be83da05a206afed786

---
+++ dragonflow/controller/chassis_snat_app.py
@@ -0,0 +1,161 @@
+# Copyright (c) 2017 OpenStack Foundation.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from oslo_log import log
+from ryu.ofproto import ether
+
+from dragonflow._i18n import _LI
+from dragonflow import conf as cfg
+from dragonflow.controller.common import constants as const
+from dragonflow.controller import df_base_app
+from dragonflow.controller import snat_app_mixin as snat_mixin
+
+LOG = log.getLogger(__name__)
+
+CHASSIS_MAC_PREFIX = '91:92:'
+
+
+class ChassisSNATApp(df_base_app.DFlowApp, snat_mixin.SNATApp_mixin):
+    """Implements single global IP allocation strategy for all hosted VMs
+
+    Methods provide strategy specific operations
+    Application has extra parameters
+    - external_host_ip - should be defined in provider range
+    -external_host_mac - optional
+    """
+    def __init__(self, *args, **kwargs):
+        super(ChassisSNATApp, self).__init__(*args, **kwargs)
+        LOG.info(_LI("Loading SNAT application ... "))
+        self.external_network_bridge = (
+            cfg.CONF.df_dnat_app.external_network_bridge)
+        self.ex_peer_patch_port = (
+            cfg.CONF.df_dnat_app.ex_peer_patch_port)
+        self.external_bridge_mac = const.EMPTY_MAC
+        self.chassis = None
+
+        # new application configuration
+        self.external_host_ip = cfg.CONF.df_snat_app.external_host_ip
+        self.enable_goto_flows = cfg.CONF.df_snat_app.enable_goto_flows
+
+        # create mac address based on given 'external_host_ip'
+        if self.external_host_ip is not None:
+            split_ip = self.external_host_ip.split('.')
+            ip2mac = '{:02x}:{:02x}:{:02x}:{:02x}'.format(*map(int, split_ip))
+            self.external_host_mac = CHASSIS_MAC_PREFIX + ip2mac
+        else:
+            raise Exception(_('Please set external_host_ip conf. parameter '
+                              'to enable SNAT application'))
+
+    def switch_features_handler(self, ev):
+        self.external_ofport = self.vswitch_api.get_port_ofport(
+                self.ex_peer_patch_port)
+
+        # install strategy flows on first VM port
+        if self.external_host_ip is None:
+            raise Exception(_('Please set external_host_ip conf. parameter '
+                              'to enable SNAT application'))
+        else:
+            self.install_strategy_based_flows()
+
+    def ovs_port_updated(self, ovs_port):
+        if ovs_port.get_name() != self.external_network_bridge:
+            return
+
+        LOG.debug("Ex. Bridge port update is called ... ")
+        mac = ovs_port.get_mac_in_use()
+        if mac in (None, const.EMPTY_MAC, self.external_bridge_mac):
+            return
+
+        self.external_bridge_mac = mac
+
+        if self.chassis is None:
+            return
+
+        parser = self.parser
+        match = parser.OFPMatch(eth_type=ether.ETH_TYPE_IP)
+        self._install_snat_egress_after_conntrack(
+            match,
+            self.external_host_mac)
+
+    def remove_local_port(self, lport):
+        """override remove_local_port method to remove installed flows
+
+        :param lport:  local logical port which is being removed
+        """
+        LOG.info(_LI("SNAT application: remove local port %(lport)s"),
+                 {'lport': lport})
+        if self.external_host_mac is not None:
+            # remove VM specific flows
+            if self.is_data_port(lport):
+                self.remove_lport_based_flows(lport)
+            else:
+                LOG.info(_LI('SNAT application: not a compute port, skipped'))
+
+    def add_local_port(self, lport):
+        """override add_local_port method to install sNAT related flows
+
+        :param lport:  local logical port which is being added
+        """
+        LOG.info(_LI("SNAT application: add local port %(lport)s"),
+                 {'lport': lport})
+
+        if self.external_host_mac is not None:
+            # install flows only when compute port is added
+            if self.is_data_port(lport):
+                self.chassis = lport.get_chassis()
+
+                self.install_lport_based_flows(lport)
+            else:
+                LOG.info(_LI('SNAT application: not a compute port, skipped'))
+
+    def install_strategy_based_flows(self):
+
+        if self.enable_goto_flows is True:
+            self._install_ingress_goto_rules()
+            self._install_egress_goto_rules()
+
+        self._install_snat_ingress_conntrack()
+
+        parser = self.parser
+        match = parser.OFPMatch(eth_type=ether.ETH_TYPE_IP)
+        self._install_snat_egress_conntrack(
+            match,
+            self.external_host_ip)
+        self._install_snat_egress_after_conntrack(
+            match,
+            self.external_host_mac)
+
+        self._install_arp_responder(
+            self.external_host_ip,
+            self.external_host_mac)
+
+    def install_lport_based_flows(self, lport):
+        # instance specific flows
+        self._install_snat_ingress_after_conntrack(
+                                        lport.get_unique_key(),
+                                        lport.get_mac(),
+                                        self.external_host_mac)
+
+    def remove_lport_based_flows(self, lport):
+        parser = self.parser
+        ofproto = self.ofproto
+        unique_key = lport.get_unique_key()
+        match = parser.OFPMatch(eth_type=ether.ETH_TYPE_IP,
+                                ct_mark=int(unique_key))
+
+        self.mod_flow(
+            command=ofproto.OFPFC_DELETE_STRICT,
+            table_id=const.INGRESS_SNAT_TABLE,
+            priority=const.PRIORITY_LOW,
+            match=match)
---
+++ dragonflow/tests/fullstack/test_snat_flows.py
@@ -0,0 +1,104 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from oslo_config import cfg
+
+from dragonflow.controller.common import constants as const
+from dragonflow.tests.common import utils
+from dragonflow.tests.fullstack import test_base
+from dragonflow.tests.fullstack import test_objects as objects
+
+SNAT_APP_NAME = 'chassis_snat_app.ChassisSNATApp'
+
+
+class TestSnatFlows(test_base.DFTestBase):
+    apps_list = SNAT_APP_NAME
+
+    def _get_vm_port(self, ip, mac):
+        ports = self.nb_api.get_all_logical_ports()
+        for port in ports:
+            if port.get_device_owner() == 'compute:None':
+                if port.get_ip() == ip and port.get_mac() == mac:
+                    return port
+        return None
+
+    def _check_if_app_enabled(self):
+        if SNAT_APP_NAME in cfg.CONF.df.apps_list:
+            return True
+        return False
+
+    def _check_port_based_flows(self,
+                                flows, hex_port_key, external_host_mac, mac):
+        match = 'ct_mark=' + hex_port_key + ',ip'
+        action = 'set_field:' + external_host_mac + '->eth_src' \
+            ',set_field:' + mac + '->eth_dst' \
+            ',load:' + hex_port_key + '->NXM_NX_REG7[]' + \
+            ',move:NXM_NX_CT_LABEL[0..31]->NXM_OF_IP_DST[]' + \
+            ',goto_table:' + str(const.INGRESS_DISPATCH_TABLE)
+
+        port_based_ingress = None
+        for flow in flows:
+            if flow['table'] == str(const.INGRESS_SNAT_TABLE):
+                if (match in flow['match']):
+                    if action in flow['actions']:
+                        port_based_ingress = True
+
+        return {'port_based_ingress': port_based_ingress}
+
+    def test_port_based_flows(self):
+        if not self._check_if_app_enabled():
+            return
+
+        network = self.store(objects.NetworkTestObj(self.neutron, self.nb_api))
+        network_id = network.create()
+        subnet = {'network_id': network_id,
+                  'cidr': '10.200.0.0/24',
+                  'gateway_ip': '10.200.0.1',
+                  'ip_version': 4,
+                  'name': 'private',
+                  'enable_dhcp': True}
+
+        external_host_ip = cfg.CONF.df_snat_app.external_host_ip
+        self.assertIsNotNone(external_host_ip)
+        split_ip = external_host_ip.split('.')
+        ip2mac = '{:02x}:{:02x}:{:02x}:{:02x}'.format(*map(int, split_ip))
+        external_host_mac = '91:92:' + ip2mac
+
+        subnet = self.neutron.create_subnet({'subnet': subnet})
+        self.assertIsNotNone(subnet)
+
+        # Create VM
+        ovs = utils.OvsFlowsParser()
+        vm = self.store(objects.VMTestObj(self, self.neutron))
+        vm.create(network=network)
+        ip = vm.get_first_ipv4()
+        self.assertIsNotNone(ip)
+
+        mac = vm.get_first_mac()
+        self.assertIsNotNone(mac)
+
+        port = utils.wait_until_is_and_return(
+            lambda: self._get_vm_port(ip, mac),
+            exception=Exception('No port assigned to VM')
+        )
+        port_key = port.get_unique_key()
+        r = self._check_port_based_flows(
+            ovs.dump(self.integration_bridge),
+            hex(port_key),
+            external_host_mac,
+            mac)
+        for key, value in r.items():
+            self.assertIsNotNone(value, key)
+
+        vm.server.stop()
+        vm.close()
+        network.close()
+        return None
