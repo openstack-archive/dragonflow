2437984ee1e2cc12db43a1398130059614e74b07
yuanwei <wei.yuan@huawei.com>
2017-02-15 16:02:59 +0800

    Save the aggregated addresses by IPNetwork list instead of IPSet
    Currently, those aggregated addresses are saved by IPSet, causes
    all addresses need be took into count when computing the CIDRs
    if any address is added to or removed from those addresses.
    In large scale, that will cause performance problem as described
    in the bug comments.
    This patch will change to use a list of IPNetwork/CIDR to save those
    addresses, so only related CIDRs will be renew computed when changes
    happened.
    
    Closes-Bug: 1642895
    Change-Id: Iab1f9c74cadc8ffd2db6bcf2286780f2317678f7

--- dragonflow/controller/sg_app.py
+++ dragonflow/controller/sg_app.py
@@ -73,53 +73,86 @@ class SGApp(df_base_app.DFlowApp):
         self.next_secgroup_rule_id = 0
         self.remote_secgroup_ref = {}
         self.secgroup_associate_local_ports = {}
-        self.secgroup_aggregate_addresses = collections.defaultdict(
-            netaddr.IPSet
-        )
+        self.secgroup_aggregate_addresses = collections.defaultdict(list)
         self.secgroup_ip_refs = collections.defaultdict(set)
         self.register_local_cookie_bits(COOKIE_NAME, 32)
 
     @staticmethod
-    def _get_cidr_difference(cidr_set, new_cidr_set):
-        new_cidr_list = new_cidr_set.iter_cidrs()
-        old_cidr_list = cidr_set.iter_cidrs()
-
-        added_cidr = set(new_cidr_list) - set(old_cidr_list)
-        removed_cidr = set(old_cidr_list) - set(new_cidr_list)
-        return added_cidr, removed_cidr
-
-    @staticmethod
-    def _get_cidr_changes_after_removing_addresses(cidr_set, address_list):
-        """cidr_set - IPSet
+    def _get_cidr_changes_after_removing_addresses(cidr_list, address_list):
+        """cidr_list - IPNetwork list
            address_list - IPAddress or string list
         """
-        new_cidr_set = cidr_set - netaddr.IPSet(address_list)
-        added_cidr, removed_cidr = SGApp._get_cidr_difference(cidr_set,
-                                                              new_cidr_set)
-        return new_cidr_set, added_cidr, removed_cidr
+        added_cidr = []
+        removed_cidr = []
+        new_cidr_list = cidr_list
+        for removed_ip in address_list:
+            new_cidr_set, temp_added_cidr, temp_removed_cidr = \
+                SGApp._remove_one_address(new_cidr_list,
+                                          netaddr.IPAddress(removed_ip))
+            added_cidr.extend(temp_added_cidr)
+            removed_cidr.extend(temp_removed_cidr)
+
+        filtered_added_cidr = \
+            [cidr for cidr in added_cidr if cidr not in removed_cidr]
+        filtered_removed_cidr = \
+            [cidr for cidr in removed_cidr if cidr not in added_cidr]
+
+        return new_cidr_list, filtered_added_cidr, filtered_removed_cidr
 
     @staticmethod
-    def _get_cidr_changes_after_adding_addresses(cidr_set, address_list):
-        """cidr_set - IPSet
+    def _get_cidr_changes_after_adding_addresses(cidr_list, address_list):
+        """cidr_list - IPNetwork list
            address_list - IPAddress or string list
         """
-        new_cidr_set = cidr_set | netaddr.IPSet(address_list)
-        added_cidr, removed_cidr = SGApp._get_cidr_difference(cidr_set,
-                                                              new_cidr_set)
-        return new_cidr_set, added_cidr, removed_cidr
+        added_cidr = []
+        removed_cidr = []
+        new_cidr_list = cidr_list
+        for added_ip in address_list:
+            new_cidr_list, temp_added_cidr, temp_removed_cidr = \
+                SGApp._add_one_address(new_cidr_list,
+                                       netaddr.IPAddress(added_ip))
+            added_cidr.extend(temp_added_cidr)
+            removed_cidr.extend(temp_removed_cidr)
+
+        filtered_added_cidr = \
+            [cidr for cidr in added_cidr if cidr not in removed_cidr]
+        filtered_removed_cidr = \
+            [cidr for cidr in removed_cidr if cidr not in added_cidr]
+
+        return new_cidr_list, filtered_added_cidr, filtered_removed_cidr
 
     @staticmethod
-    def _get_cidr_changes_after_updating_addresses(cidr_set, addresses_to_add,
+    def _get_cidr_changes_after_updating_addresses(cidr_list, addresses_to_add,
                                                    addresses_to_remove):
-        """cidr_set - IPSet
+        """cidr_list - IPNetwork list
            addresses_to_add - IPAddress or string list
            addresses_to_remove - IPAddress or string list
         """
-        new_cidr_set = ((cidr_set | netaddr.IPSet(addresses_to_add)) -
-                        (netaddr.IPSet(addresses_to_remove)))
-        added_cidr, removed_cidr = SGApp._get_cidr_difference(cidr_set,
-                                                              new_cidr_set)
-        return new_cidr_set, added_cidr, removed_cidr
+        added_cidr = []
+        removed_cidr = []
+        new_cidr_list = cidr_list
+        for removed_ip in addresses_to_remove:
+            new_cidr_set, temp_added_cidr, temp_removed_cidr = \
+                SGApp._remove_one_address(
+                    new_cidr_list, netaddr.IPAddress(removed_ip)
+                )
+            added_cidr.extend(temp_added_cidr)
+            removed_cidr.extend(temp_removed_cidr)
+
+        for added_ip in addresses_to_add:
+            new_cidr_set, temp_added_cidr, temp_removed_cidr = \
+                SGApp._add_one_address(
+                    new_cidr_list, netaddr.IPAddress(added_ip)
+                )
+            added_cidr.extend(temp_added_cidr)
+            removed_cidr.extend(temp_removed_cidr)
+
+        filtered_added_cidr = \
+            [cidr for cidr in added_cidr if cidr not in removed_cidr]
+        filtered_removed_cidr = \
+            [cidr for cidr in removed_cidr if cidr not in added_cidr]
+
+        return new_cidr_list, filtered_added_cidr, filtered_removed_cidr
 
     @staticmethod
     def _get_port_match_list_from_port_range(port_range_min, port_range_max):
@@ -133,8 +166,94 @@ class SGApp(df_base_app.DFlowApp):
 
     @staticmethod
     def _get_network_and_mask(cidr):
-        result = netaddr.IPNetwork(cidr)
-        return (int(result.network), int(result.netmask))
+        result = cidr
+        if not isinstance(result, netaddr.IPNetwork):
+            result = netaddr.IPNetwork(result)
+        return int(result.network), int(result.netmask)
+
+    @staticmethod
+    def _if_cidrs_can_be_merged(cidr1, cidr2):
+        if (cidr1.last + 1 == cidr2.first and
+                cidr1.last - cidr1.first == cidr2.last - cidr2.first):
+            return True
+
+        return False
+
+    @staticmethod
+    def _remove_one_address(cidr_list, address_to_remove):
+        """cidr_list - IPNetwork list
+           address_to_remove - IPAddress
+        """
+        added_cidr = []
+        removed_cidr = []
+        new_cidr_list = cidr_list
+
+        for index in range(len(cidr_list)):
+            cidr_item = cidr_list[index]
+            if address_to_remove in cidr_item:
+                removed_cidr = [cidr_item]
+                added_cidr = netaddr.cidr_exclude(cidr_item, address_to_remove)
+
+                new_cidr_list = cidr_list[:index]
+                new_cidr_list.extend(added_cidr)
+                new_cidr_list.extend(cidr_list[(index + 1):])
+                break
+
+        return new_cidr_list, added_cidr, removed_cidr
+
+    @staticmethod
+    def _add_one_address(cidr_list, address_to_add):
+        """cidr_list - IPNetwork list
+           address_to_add - IPAddress
+        """
+        position = None
+        for index in range(len(cidr_list)):
+            cidr_item = cidr_list[index]
+            if cidr_item.last >= address_to_add:
+                if cidr_item.first <= address_to_add:
+                    return cidr_list, [], []
+                position = index
+                break
+
+        if position is None:
+            left_position = len(cidr_list) - 1
+            right_position = len(cidr_list)
+        else:
+            left_position = position - 1
+            right_position = position
+
+        current_cidr = netaddr.IPNetwork(address_to_add)
+        removed_cidr = []
+        continue_flag = True
+        while continue_flag:
+            continue_flag = False
+            if left_position >= 0:
+                left_item = cidr_list[left_position]
+                if SGApp._if_cidrs_can_be_merged(left_item, current_cidr):
+                    removed_cidr.append(left_item)
+                    new_cidr_tuple = (left_item.first,
+                                      left_item.prefixlen - 1)
+                    current_cidr = netaddr.IPNetwork(new_cidr_tuple)
+                    left_position -= 1
+                    continue_flag = True
+                    continue
+            if right_position < len(cidr_list):
+                right_item = cidr_list[right_position]
+                if SGApp._if_cidrs_can_be_merged(current_cidr, right_item):
+                    removed_cidr.append(right_item)
+                    new_cidr_tuple = (current_cidr.first,
+                                      current_cidr.prefixlen - 1)
+                    current_cidr = netaddr.IPNetwork(new_cidr_tuple)
+                    right_position += 1
+                    continue_flag = True
+                    continue
+
+        added_cidr = [current_cidr]
+        new_cidr_list = cidr_list[:(left_position + 1)]
+        new_cidr_list.extend(added_cidr)
+        new_cidr_list.extend(cidr_list[right_position:])
+
+        return new_cidr_list, added_cidr, removed_cidr
 
     def _protocol_number_by_name(self, name):
         return PROTOCOL_NUMBER_BY_NAME.get(name) or int(name)
@@ -627,8 +746,7 @@ class SGApp(df_base_app.DFlowApp):
                     self.secgroup_aggregate_addresses.get(remote_group_id)
                 addresses_list = []
                 if aggregate_addresses_range is not None:
-                    cidr_list = aggregate_addresses_range.iter_cidrs()
-                    for aggregate_address in cidr_list:
+                    for aggregate_address in aggregate_addresses_range:
                         addresses_list.append({
                             ipv4_match_item: SGApp._get_network_and_mask(
                                 aggregate_address
@@ -783,12 +901,12 @@ class SGApp(df_base_app.DFlowApp):
         # with this security group.
         addresses = self.secgroup_aggregate_addresses[secgroup_id]
         added_ips = self._get_lport_added_ips_for_secgroup(secgroup_id, lport)
-        new_cidr_set, added_cidr, removed_cidr = \
+        new_cidr_list, added_cidr, removed_cidr = \
             SGApp._get_cidr_changes_after_adding_addresses(
                 addresses,
                 added_ips,
             )
-        self.secgroup_aggregate_addresses[secgroup_id] = new_cidr_set
+        self.secgroup_aggregate_addresses[secgroup_id] = new_cidr_list
 
         # update the flows representing those rules each of which specifies
         #  this security group as its parameter
@@ -810,15 +928,15 @@ class SGApp(df_base_app.DFlowApp):
         if aggregate_addresses_range:
             removed_ips = self._get_lport_removed_ips_for_secgroup(
                 secgroup_id, lport)
-            new_cidr_set, added_cidr, removed_cidr = \
+            new_cidr_list, added_cidr, removed_cidr = \
                 SGApp._get_cidr_changes_after_removing_addresses(
                     aggregate_addresses_range,
                     removed_ips,
                 )
-            if not new_cidr_set:
+            if not new_cidr_list:
                 del self.secgroup_aggregate_addresses[secgroup_id]
             else:
-                self.secgroup_aggregate_addresses[secgroup_id] = new_cidr_set
+                self.secgroup_aggregate_addresses[secgroup_id] = new_cidr_list
 
             # update the flows representing those rules each of which
             # specifies this security group as its
