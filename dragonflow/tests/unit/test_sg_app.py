e9b91f80233409fc09b16a0fd7ec723ec330374f
yuanwei <wei.yuan@huawei.com>
2017-02-15 16:02:59 +0800

    Save the aggregated addresses by IPNetwork list instead of IPSet
    Currently, those aggregated addresses are saved by IPSet, causes
    all addresses need be took into count when computing the CIDRs
    if any address is added to or removed from those addresses.
    In large scale, that will cause performance problem as described
    in the bug comments.
    This patch will change to use a new class named CIDRList to save those
    addresses, so only related CIDRs will be renew computed when changes
    happened.
    CIDRList class provides those methods:
    1) get_cidr_list
    2) add_addresses_and_get_changes
    3) remove_addresses_and_get_changes
    4) update_addresses_and_get_changes
    
    Closes-Bug: 1642895
    Change-Id: Iab1f9c74cadc8ffd2db6bcf2286780f2317678f7

--- dragonflow/tests/unit/test_sg_app.py
+++ dragonflow/tests/unit/test_sg_app.py
@@ -15,9 +15,13 @@
 
 import mock
 import netaddr
+import random
+import string
+
 from neutron.agent.common import utils
 from neutron_lib import constants as n_const
 
+from dragonflow.controller.common import cidr_list
 from dragonflow.db import models as db_models
 from dragonflow.tests.unit import test_app_base
 
@@ -524,38 +528,95 @@ class TestSGApp(test_app_base.DFAppTestBase):
         self.controller.delete_secgroup(self.security_group.get_id())
 
     def test_aggregating_flows_for_addresses(self):
+        # initial aggregate addresses list
+        aggreate_addresses = cidr_list.CIDRList()
+        aggreate_addresses.add_addresses_and_get_changes(['192.168.10.6'])
+
         # add one address
-        old_cidr_set = netaddr.IPSet(['192.168.10.6'])
-        new_cidr_set, added_cidr, deleted_cidr = \
-            self.app._get_cidr_changes_after_adding_addresses(
-                old_cidr_set, ['192.168.10.7'])
-        expected_new_cidr_set = netaddr.IPSet(['192.168.10.6/31'])
-        expected_added_cidr = {netaddr.IPNetwork('192.168.10.6/31')}
-        expected_deleted_cidr = {netaddr.IPNetwork('192.168.10.6/32')}
-        self.assertEqual(new_cidr_set, expected_new_cidr_set)
+        added_cidr, deleted_cidr = \
+            aggreate_addresses.add_addresses_and_get_changes(['192.168.10.7'])
+        expected_new_cidr_list = [netaddr.IPNetwork('192.168.10.6/31')]
+        expected_added_cidr = [netaddr.IPNetwork('192.168.10.6/31')]
+        expected_deleted_cidr = [netaddr.IPNetwork('192.168.10.6/32')]
+        self.assertEqual(aggreate_addresses.get_cidr_list(),
+                         expected_new_cidr_list)
         self.assertEqual(added_cidr, expected_added_cidr)
         self.assertEqual(deleted_cidr, expected_deleted_cidr)
 
         # remove one address
-        old_cidr_set = new_cidr_set
-        new_cidr_set, added_cidr, deleted_cidr = \
-            self.app._get_cidr_changes_after_removing_addresses(
-                old_cidr_set, ['192.168.10.7'])
-        expected_new_cidr_set = netaddr.IPSet(['192.168.10.6/32'])
-        expected_added_cidr = {netaddr.IPNetwork('192.168.10.6/32')}
-        expected_deleted_cidr = {netaddr.IPNetwork('192.168.10.6/31')}
-        self.assertEqual(new_cidr_set, expected_new_cidr_set)
+        added_cidr, deleted_cidr = \
+            aggreate_addresses.remove_addresses_and_get_changes(
+                ['192.168.10.7'])
+        expected_new_cidr_list = [netaddr.IPNetwork('192.168.10.6/32')]
+        expected_added_cidr = [netaddr.IPNetwork('192.168.10.6/32')]
+        expected_deleted_cidr = [netaddr.IPNetwork('192.168.10.6/31')]
+        self.assertEqual(aggreate_addresses.get_cidr_list(),
+                         expected_new_cidr_list)
         self.assertEqual(added_cidr, expected_added_cidr)
         self.assertEqual(deleted_cidr, expected_deleted_cidr)
 
         # update addresses
-        old_cidr_set = new_cidr_set
-        new_cidr_set, added_cidr, deleted_cidr = \
-            self.app._get_cidr_changes_after_updating_addresses(
-                old_cidr_set, ['192.168.10.7'], ['192.168.10.6'])
-        expected_new_cidr_set = netaddr.IPSet(['192.168.10.7/32'])
-        expected_added_cidr = {netaddr.IPNetwork('192.168.10.7/32')}
-        expected_deleted_cidr = {netaddr.IPNetwork('192.168.10.6/32')}
-        self.assertEqual(new_cidr_set, expected_new_cidr_set)
+        added_cidr, deleted_cidr = \
+            aggreate_addresses.update_addresses_and_get_changes(
+                ['192.168.10.7'], ['192.168.10.6'])
+        expected_new_cidr_list = [netaddr.IPNetwork('192.168.10.7/32')]
+        expected_added_cidr = [netaddr.IPNetwork('192.168.10.7/32')]
+        expected_deleted_cidr = [netaddr.IPNetwork('192.168.10.6/32')]
+        self.assertEqual(aggreate_addresses.get_cidr_list(),
+                         expected_new_cidr_list)
         self.assertEqual(added_cidr, expected_added_cidr)
         self.assertEqual(deleted_cidr, expected_deleted_cidr)
+
+        # create lots of random IPv4 / IPv6 addresses
+        lots_addreses = set()
+        to_be_removed_addresses = set()
+        to_be_removed_addresses2 = set()
+        for loop1 in range(1, 6):
+            for loop2 in range(0, 100):
+                tail = str(random.randint(1, 254))
+                address = '1.1.' + str(loop1) + '.' + tail
+                lots_addreses.add(address)
+                if loop2 < 10:
+                    to_be_removed_addresses.add(address)
+                elif loop2 < 20:
+                    to_be_removed_addresses2.add(address)
+        for loop1 in range(1, 3):
+            for loop2 in range(0, 100):
+                tail = ''.join(random.sample(string.hexdigits, 2))
+                address = '::' + str(loop1) + ':' + tail.upper()
+                lots_addreses.add(address)
+                if loop2 < 10:
+                    to_be_removed_addresses.add(address)
+                elif loop2 < 20:
+                    to_be_removed_addresses2.add(address)
+        to_be_removed_addresses2 = (to_be_removed_addresses2 -
+                                    to_be_removed_addresses)
+
+        # compare cidr list after adding lots of IPv4 / IPv6 addresses
+        aggreate_addresses = cidr_list.CIDRList()
+        aggreate_addresses.add_addresses_and_get_changes(lots_addreses)
+        expected_addresses_set = netaddr.IPSet(
+            [netaddr.IPAddress(item) for item in lots_addreses]
+        )
+        self.assertEqual(aggreate_addresses.get_cidr_list(),
+                         expected_addresses_set.iter_cidrs())
+
+        # compare cidr list after removing lots of IPv4 / IPv6 addresses
+        aggreate_addresses.remove_addresses_and_get_changes(
+            to_be_removed_addresses)
+        new_lots_addresses = lots_addreses - to_be_removed_addresses
+        expected_addresses_set = netaddr.IPSet(
+            [netaddr.IPAddress(item) for item in new_lots_addresses]
+        )
+        self.assertEqual(aggreate_addresses.get_cidr_list(),
+                         expected_addresses_set.iter_cidrs())
+
+        # compare cidr list after updating lots of IPv4 / IPv6 addresses
+        aggreate_addresses.update_addresses_and_get_changes(
+            to_be_removed_addresses, to_be_removed_addresses2)
+        new_lots_addresses = lots_addreses - to_be_removed_addresses2
+        expected_addresses_set = netaddr.IPSet(
+            [netaddr.IPAddress(item) for item in new_lots_addresses]
+        )
+        self.assertEqual(aggreate_addresses.get_cidr_list(),
+                         expected_addresses_set.iter_cidrs())
