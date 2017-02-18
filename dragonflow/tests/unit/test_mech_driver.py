182f84c6e137720863b6f863d3643df607f6fcb4
jingting <kangjingting@huawei.com>
2017-02-18 17:02:46 +0800

    Use neutron_lib for context module in mech driver
    Depends-On: I48cf45dc1b07035d952152eac2548a3bd9fc2832
    
    Change-Id: I1f8170a76bf00c096495d40900b123215aa54bc1

--- dragonflow/tests/unit/test_mech_driver.py
+++ dragonflow/tests/unit/test_mech_driver.py
@@ -13,7 +13,7 @@
 """Unit testing for dragonflow mechanism driver."""
 
 import mock
-from neutron import context
+from neutron_lib import context
 from neutron.plugins.ml2 import config
 from neutron.tests.unit.extensions import test_portsecurity
 from neutron.tests.unit.plugins.ml2 import test_ext_portsecurity
