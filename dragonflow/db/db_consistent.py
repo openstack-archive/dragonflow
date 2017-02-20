db62b299109bf137b97d8a9b9374b5ee6edce450
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2017-02-20 17:37:12 +0200

    [16/xx] DbConsistent: Adapt to new style ID/Version fields
    Partially-Implements: blueprint refactor-nb-api
    Change-Id: I7133625367d4ac786d38c7bd8df6b89b5c09d8e9

--- dragonflow/db/db_consistent.py
+++ dragonflow/db/db_consistent.py
@@ -111,8 +111,8 @@ class DBConsistencyManager(object):
         :param df_object:  Object from df db
         :param local_object:  Object from local cache
         """
-        df_version = df_object.get_version() if df_object else None
-        local_version = local_object.get_version() if local_object else None
+        df_version = df_object.version if df_object else None
+        local_version = local_object.version if local_object else None
 
         old_cache_obj = self.cache_manager.get(table, id)
         if not old_cache_obj or old_cache_obj.get_action() != action:
@@ -178,18 +178,18 @@ class DBConsistencyManager(object):
         directly after this comparison, if False, we'll go into the verify
         process which need twice comparison to do the operation.
         """
-        local_object_map = {}
-        for local_object in local_objects:
-            local_object_map[local_object.get_id()] = local_object
+        local_object_map = {o.id: o for o in local_objects}
+
         for df_object in df_objects[:]:
-            df_id = df_object.get_id()
-            df_version = df_object.get_version()
+            df_id = df_object.id
+            df_version = df_object.version
+
             if df_version is None:
                 LOG.error(_LE("Version is None in df_object: %s"), df_object)
                 continue
             local_object = local_object_map.pop(df_id, None)
             if local_object:
-                local_version = local_object.get_version()
+                local_version = local_object.version
                 if local_version is None:
                     LOG.debug("Version is None in local_object: %s",
                               local_object)
@@ -216,10 +216,10 @@ class DBConsistencyManager(object):
             LOG.debug("Find a redundant local object: %s", local_object)
             if direct:
                 self.controller.process_object(table, 'delete',
-                                               local_object.get_id())
+                                               local_object.id)
             else:
                 self._verify_object(
-                        table, local_object.get_id(),
+                        table, local_object.id,
                         'delete', None, local_object)
 
     def _get_and_compare_df_and_local_data(self, table, direct, topic=None):
