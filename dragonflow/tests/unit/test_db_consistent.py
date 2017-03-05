6f9c0dd111a85bcf9c26f0c8e4e0e3c45566c850
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2017-03-05 13:05:12 +0200

    Refactor DB consistent (part 2)
    Partially-Implements: blueprint refactor-nb-api
    Change-Id: I8d9c9590e96f32ac23cadd1ede8db5c06342c82e

--- dragonflow/tests/unit/test_db_consistent.py
+++ dragonflow/tests/unit/test_db_consistent.py
@@ -23,43 +23,196 @@ class TestDBConsistent(tests_base.BaseTestCase):
         self.controller = mock.MagicMock()
 
         self.topic = '111-222-333'
-        self.lport_id0 = '0'
-        self.lport_id1 = '1'
-        self.lport_id2 = '2'
-        self.lport_id3 = '3'
-        self.lport_id4 = '4'
+        self.obj_id0 = '0'
+        self.obj_id1 = '1'
+        self.obj_id2 = '2'
+        self.obj_id3 = '3'
+        self.obj_id4 = '4'
         self.db_consistent = db_consistent.DBConsistencyManager(
                 self.controller)
 
-    def test_db_comparison(self):
-        df_obj0 = FakeDfLocalObj(self.lport_id0, 0)
-        df_obj1 = FakeDfLocalObj(self.lport_id1, 1)
-        df_obj2 = FakeDfLocalObj(self.lport_id2, 2)
-        df_obj3 = FakeDfLocalObj(self.lport_id4, 1)
-
-        local_obj1 = FakeDfLocalObj(self.lport_id2, 1)
-        local_obj2 = FakeDfLocalObj(self.lport_id3, 1)
-        local_obj3 = FakeDfLocalObj(self.lport_id4, 0)
-
-        handler = db_consistent.ModelHandler(
+    def _create_handler(self, local, nb):
+        return db_consistent.VersionedModelHandler(
             model=mock.MagicMock(),
-            db_store_func=mock.MagicMock(
-                return_value=[
-                    local_obj1,
-                    local_obj2,
-                    local_obj3,
-                ]
-            ),
-            nb_api_func=mock.MagicMock(
-                return_value=[
-                    df_obj0,
-                    df_obj1,
-                    df_obj2,
-                    df_obj3,
-                ],
-            ),
+            db_store_func=mock.MagicMock(return_value=local),
+            nb_api_func=mock.MagicMock(return_value=nb),
             update_handler=mock.MagicMock(),
             delete_handler=mock.MagicMock(),
+        )
+
+    def _create_versionneless_handler(self, local, nb):
+        return db_consistent.ModelHandler(
+            model=mock.MagicMock(),
+            db_store_func=mock.MagicMock(return_value=local),
+            nb_api_func=mock.MagicMock(return_value=nb),
+            update_handler=mock.MagicMock(),
+            delete_handler=mock.MagicMock(),
+        )
+
+    def test_direct_create(self):
+        nb_obj = FakeDfLocalObj('1', 1)
+        handler = self._create_handler([], [nb_obj])
+
+        self.db_consistent.handle_data_comparison([self.topic], handler, True)
+        handler._update_handler.assert_called_once_with(nb_obj)
+
+    def test_indirect_create(self):
+        nb_obj = FakeDfLocalObj('1', 1)
+        handler = self._create_handler([], [nb_obj])
+
+        self.db_consistent.handle_data_comparison([self.topic], handler, False)
+        handler._update_handler.assert_not_called()
+
+        self.db_consistent.handle_data_comparison([self.topic], handler, False)
+        handler._update_handler.assert_called_once_with(nb_obj)
+
+    def test_indirect_create_versionless(self):
+        nb_obj = FakeDfLocalObj('1', None)
+        handler = self._create_versionneless_handler([], [nb_obj])
+
+        self.db_consistent.handle_data_comparison([self.topic], handler, False)
+        handler._update_handler.assert_not_called()
+
+        self.db_consistent.handle_data_comparison([self.topic], handler, False)
+        handler._update_handler.assert_called_once_with(nb_obj)
+
+    def test_indirect_create_older_version(self):
+        new_obj = FakeDfLocalObj('1', 2)
+        older_obj = FakeDfLocalObj('1', 1)
+        handler = self._create_handler([], [new_obj])
+
+        self.db_consistent.handle_data_comparison([self.topic], handler, False)
+        handler._update_handler.assert_not_called()
+
+        handler._nb_api_func.return_value = [older_obj]
+        self.db_consistent.handle_data_comparison([self.topic], handler, False)
+        handler._update_handler.assert_not_called()
+
+    def test_indirect_create_aborted(self):
+        nb_obj = FakeDfLocalObj('1', 2)
+        handler = self._create_handler([], [nb_obj])
+
+        self.db_consistent.handle_data_comparison([self.topic], handler, False)
+        handler._update_handler.assert_not_called()
+
+        handler._db_store_func.return_value = [nb_obj]
+        self.db_consistent.handle_data_comparison([self.topic], handler, False)
+        handler._update_handler.assert_not_called()
+
+    def test_direct_delete(self):
+        local_obj = FakeDfLocalObj('1', 1)
+        handler = self._create_handler([local_obj], [])
+
+        self.db_consistent.handle_data_comparison([self.topic], handler, True)
+        handler._delete_handler.assert_called_once_with(local_obj)
+
+    def test_indirect_delete(self):
+        local_obj = FakeDfLocalObj('1', 1)
+        handler = self._create_handler([local_obj], [])
+
+        self.db_consistent.handle_data_comparison([self.topic], handler, False)
+        handler._delete_handler.assert_not_called()
+
+        self.db_consistent.handle_data_comparison([self.topic], handler, False)
+        handler._delete_handler.assert_called_once_with(local_obj)
+
+    def test_indirect_delete_aborted(self):
+        local_obj = FakeDfLocalObj('1', 1)
+        handler = self._create_handler([local_obj], [])
+
+        self.db_consistent.handle_data_comparison([self.topic], handler, False)
+        handler._delete_handler.assert_not_called()
+
+        handler._nb_api_func.return_value = [local_obj]
+        self.db_consistent.handle_data_comparison([self.topic], handler, False)
+        handler._delete_handler.assert_not_called()
+
+    def test_direct_update(self):
+        local_obj = FakeDfLocalObj('1', 1)
+        nb_obj = FakeDfLocalObj('1', 2)
+        handler = self._create_handler([local_obj], [nb_obj])
+
+        self.db_consistent.handle_data_comparison([self.topic], handler, True)
+        handler._update_handler.assert_called_once_with(nb_obj)
+
+    def test_direct_update_same_version(self):
+        obj = FakeDfLocalObj('1', 2)
+
+        handler = self._create_handler([obj], [obj])
+        self.db_consistent.handle_data_comparison([self.topic], handler, True)
+        handler._update_handler.assert_not_called()
+
+    def test_direct_update_versionless(self):
+        local_obj = FakeDfLocalObj('1', None)
+        nb_obj = FakeDfLocalObj('1', None)
+
+        handler = self._create_versionneless_handler([local_obj], [nb_obj])
+        self.db_consistent.handle_data_comparison([self.topic], handler, True)
+        handler._update_handler.assert_called_once_with(nb_obj)
+
+    def test_indirect_update(self):
+        local_obj = FakeDfLocalObj('1', 1)
+        nb_obj = FakeDfLocalObj('1', 2)
+        handler = self._create_handler([local_obj], [nb_obj])
+
+        self.db_consistent.handle_data_comparison([self.topic], handler, False)
+        handler._update_handler.assert_not_called()
+
+        self.db_consistent.handle_data_comparison([self.topic], handler, False)
+        handler._update_handler.assert_called_once_with(nb_obj)
+
+    def test_indirect_update_versionless(self):
+        local_obj = FakeDfLocalObj('1', None)
+        nb_obj = FakeDfLocalObj('1', None)
+
+        handler = self._create_versionneless_handler([local_obj], [nb_obj])
+        self.db_consistent.handle_data_comparison([self.topic], handler, False)
+        handler._update_handler.assert_not_called()
+
+        self.db_consistent.handle_data_comparison([self.topic], handler, False)
+        handler._update_handler.assert_called_once_with(nb_obj)
+
+    def test_indirect_update_aborted(self):
+        local_obj = FakeDfLocalObj('1', 1)
+        nb_obj = FakeDfLocalObj('1', 2)
+        handler = self._create_handler([local_obj], [nb_obj])
+
+        self.db_consistent.handle_data_comparison([self.topic], handler, False)
+        handler._update_handler.assert_not_called()
+
+        handler._db_store_func.return_value = [nb_obj]
+        self.db_consistent.handle_data_comparison([self.topic], handler, False)
+        handler._update_handler.assert_not_called()
+
+    def test_indirect_update_local_updated(self):
+        old_obj = FakeDfLocalObj('1', 1)
+        newer_obj = FakeDfLocalObj('1', 2)
+        newest_obj = FakeDfLocalObj('1', 3)
+
+        handler = self._create_handler([old_obj], [newest_obj])
+        self.db_consistent.handle_data_comparison([self.topic], handler, False)
+        handler._update_handler.assert_not_called()
+
+        handler._db_store_func.return_value = [newer_obj]
+        self.db_consistent.handle_data_comparison([self.topic], handler, False)
+        handler._update_handler.assert_not_called()
+
+        self.db_consistent.handle_data_comparison([self.topic], handler, False)
+        handler._update_handler.assert_called_once_with(newest_obj)
+
+    def test_direct_db_comparison(self):
+        df_obj0 = FakeDfLocalObj(self.obj_id0, 0)
+        df_obj1 = FakeDfLocalObj(self.obj_id1, 1)
+        df_obj2 = FakeDfLocalObj(self.obj_id2, 2)
+        df_obj3 = FakeDfLocalObj(self.obj_id4, 1)
+
+        local_obj1 = FakeDfLocalObj(self.obj_id2, 1)
+        local_obj2 = FakeDfLocalObj(self.obj_id3, 1)
+        local_obj3 = FakeDfLocalObj(self.obj_id4, 0)
+
+        handler = self._create_handler(
+            [local_obj1, local_obj2, local_obj3],
+            [df_obj0, df_obj1, df_obj2, df_obj3],
         )
 
         self.db_consistent.handle_data_comparison([self.topic], handler, True)
@@ -69,10 +222,7 @@ class TestDBConsistent(tests_base.BaseTestCase):
         handler._update_handler.assert_any_call(df_obj1)
         handler._update_handler.assert_any_call(df_obj2)
         handler._update_handler.assert_any_call(df_obj3)
-        handler._delete_handler.assert_any_call(
-            handler._model,
-            self.lport_id3,
-        )
+        handler._delete_handler.assert_called_once_with(local_obj2)
 
 
 class FakeDfLocalObj(object):
@@ -80,3 +230,6 @@ class FakeDfLocalObj(object):
     def __init__(self, id, version):
         self.id = id
         self.version = version
+
+    def is_newer_than(self, other):
+        return self.version > other.version
