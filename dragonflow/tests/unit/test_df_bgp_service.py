ffe0b8717a1fb234a460a0ae23f79825ff2e7f98
Hong Hui Xiao <honghui_xiao@yeah.net>
2017-03-08 09:11:42 +0800

    Add BGP db model and service
    This patch adds BGP db model and BGP service. Currently, BGP service
    will sync local cache periodically.
    
    Change-Id: I73455fb442c75b8a81ccf371b25b4823c91b85a8
    Partially-implements: blueprint bgp-dynamic-routing

---
+++ dragonflow/tests/unit/test_df_bgp_service.py
@@ -0,0 +1,50 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import eventlet
+import mock
+
+from dragonflow.controller import df_bgp_service
+from dragonflow.db.models import bgp
+from dragonflow.tests import base as tests_base
+
+
+class TestDFBGPService(tests_base.BaseTestCase):
+
+    def setUp(self):
+        super(TestDFBGPService, self).setUp()
+        self.bgp = df_bgp_service.BGPService()
+        self.nb_api = self.bgp.nb_api = mock.Mock()
+
+    def test_sync_bgp_data_to_db_store(self):
+
+        def get_all_side_effect(model, topic):
+            if model == bgp.BGPPeer:
+                return [bgp.BGPPeer(id="peer1",
+                                    topic="topic1",
+                                    name="peer1",
+                                    peer_ip="172.24.4.88",
+                                    remote_as=4321)]
+
+            if model == bgp.BGPSpeaker:
+                return [bgp.BGPSpeaker(id="speaker1",
+                                       topic="topic1",
+                                       name="speaker1",
+                                       local_as=1234,
+                                       peers=["peer1"],
+                                       ip_version="4")]
+
+        self.nb_api.get_all.side_effect = get_all_side_effect
+        self.bgp.start()
+        # Give fixed interval a chance to run.
+        eventlet.sleep(0)
+        self.assertTrue(self.bgp.db_store.get_one(bgp.BGPPeer(id="peer1")))
