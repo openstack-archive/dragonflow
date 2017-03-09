6fd9d64020a7852e896c57f63ed8d0340c05cf8f
Lihi Wishnitzer <lihiwish@gmail.com>
2017-03-09 14:47:25 +0200

    Migrate Security Groups to new model structure
    Migrate SecurityGroup and SecurityGroupRule to
    new model structure
    
    Partially-Implements: blueprint refactor-nb-api
    Change-Id: I9e1970d6b67214c4b66264d1acb66fc13d1deb8b

--- dragonflow/db/models/all.py
+++ dragonflow/db/models/all.py
@@ -13,3 +13,4 @@ from dragonflow.db.models import core  # noqa
 from dragonflow.db.models import l2  # noqa
 from dragonflow.db.models import legacy  # noqa
 from dragonflow.db.models import qos  # noqa
+from dragonflow.db.models import secgroups  # noqa
--- dragonflow/tests/unit/test_df_qos_notification_driver.py
+++ dragonflow/tests/unit/test_df_qos_notification_driver.py
@@ -112,9 +112,10 @@ class TestDFQosNotificationDriver(test_mech_driver.DFMechanismDriverTestCase):
         with self.network(arg_list=('qos_policy_id',), **kwargs) as n:
             network_id = n['network']['id']
             self.assertTrue(nb_api.create.called)
-            # nb_api.create(LogicalSwitch) will be called after
-            # nb_api.create(QosPolicy), so the index here is 1.
-            called_args = nb_api.create.call_args_list[1][0][0]
+            # Excepted call order for nb_api.create:
+            # QosPolicy, SecuiryGroup, LogicalSwitch.
+            # Expected index for LogicalSwitch is 2.
+            called_args = nb_api.create.call_args_list[2][0][0]
             self.assertEqual(qos_obj['id'], called_args.qos_policy.id)
 
             data = {'network': {'qos_policy_id': None}}
