141c76a261773e1fc52b59d1ccdee4679f3ea7ed
Li Ma <skywalker.nick@gmail.com>
2017-03-31 18:47:42 +0800

    Add reconnection for PUB zmq socket
    1. Implement reconnection for PUB socket.
    2. Reuse zmq context.
    3. Refactor some codes to make it more stable.
    4. Add zmq pubsub unit tests
    
    Change-Id: Iba94fd5d0d5fa8958b508a9ceb2eccd4b062913e
    Closes-Bug: #1678054

---
+++ dragonflow/tests/unit/test_zmq_pubsub.py
@@ -0,0 +1,87 @@
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import mock
+
+from dragonflow.db import db_common
+from dragonflow.db.pubsub_drivers import zmq_pubsub_driver
+from dragonflow.tests import base as tests_base
+
+
+class TestZMQPubSub(tests_base.BaseTestCase):
+
+    def setUp(self):
+        super(TestZMQPubSub, self).setUp()
+        self.ZMQPublisherAgent = zmq_pubsub_driver.ZMQPublisherAgent()
+        self.ZMQSubscriberAgent = \
+            zmq_pubsub_driver.ZMQSubscriberAgent()
+
+        self.ZMQPublisherAgent.context = mock.Mock()
+        self.ZMQPublisherAgent.context.socket = mock.Mock()
+        self.ZMQPublisherAgent.context.socket.setsockopt = mock.Mock()
+        self.ZMQPublisherAgent.context.socket.bind = mock.Mock()
+        self.ZMQPublisherAgent.context.socket.send_multipart = mock.Mock()
+        self.ZMQSubscriberAgent.context = mock.Mock()
+        self.ZMQSubscriberAgent.context.socket = mock.Mock()
+        self.ZMQSubscriberAgent.context.socket.connect = mock.Mock()
+
+    def test_publish_success_with_topic(self):
+        update = db_common.DbUpdate("router",
+                                    "key",
+                                    "action",
+                                    "value",
+                                    topic='teststring')
+        with mock.patch.object(zmq_pubsub_driver.LOG, 'debug') as log_debug:
+            result = self.ZMQPublisherAgent.send_event(update, 'teststring')
+            log_debug.assert_called()
+            self.ZMQPublisherAgent.socket.send_multipart.assert_called_once()
+            self.assertIsNone(result)
+
+    def test_publish_success_without_topic(self):
+        update = db_common.DbUpdate("router",
+                                    "key",
+                                    "action",
+                                    "value",
+                                    topic=None)
+        with mock.patch.object(zmq_pubsub_driver.LOG, 'debug') as log_debug:
+            result = self.ZMQPublisherAgent.send_event(update, None)
+            log_debug.assert_called()
+            args = self.ZMQPublisherAgent.socket.send_multipart.call_args
+            self.ZMQPublisherAgent.socket.send_multipart.assert_called_once()
+            self.assertEqual(db_common.SEND_ALL_TOPIC, args[0][0][0])
+            self.assertIsNone(result)
+
+    def test_publisher_reconnection(self):
+        update = db_common.DbUpdate("router",
+                                    "key",
+                                    "action",
+                                    "value",
+                                    topic='teststring')
+        self.ZMQPublisherAgent.socket = None
+        with mock.patch.object(zmq_pubsub_driver.LOG, 'debug') as log_debug:
+            result = self.ZMQPublisherAgent.send_event(update, 'teststring')
+            self.ZMQPublisherAgent.socket.bind.assert_called_once()
+            self.ZMQPublisherAgent.socket.send_multipart.assert_called_once()
+            log_debug.assert_called()
+            self.assertEqual(2, log_debug.call_count)
+            self.assertIsNone(result)
+
+    def test_subscribe_success(self):
+        result = self.ZMQSubscriberAgent.register_topic('teststring')
+        self.assertTrue(
+            b'teststring' in self.ZMQSubscriberAgent.topic_list)
+        self.assertIsNone(result)
+        result = self.ZMQSubscriberAgent.unregister_topic('teststring')
+        self.assertFalse(
+            b'teststring' in self.ZMQSubscriberAgent.topic_list)
+        self.assertIsNone(result)
