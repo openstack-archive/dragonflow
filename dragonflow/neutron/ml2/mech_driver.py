95c19520bd66d8557cca240e517762e44ffcf352
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2017-04-20 13:33:43 +0300

    Migrate QoS driver to new driver base
    Closes-Bug: #1683784
    Change-Id: I8a6a603bea463853d07379329684ca3d6a5dc16a

--- dragonflow/neutron/ml2/mech_driver.py
+++ dragonflow/neutron/ml2/mech_driver.py
@@ -37,6 +37,7 @@ from dragonflow.db.models import l2
 from dragonflow.db.neutron import lockedobjects_db as lock_db
 from dragonflow.neutron.common import constants as df_const
 from dragonflow.neutron.db.models import l2 as neutron_l2
+from dragonflow.neutron.services.qos.drivers import df_qos
 
 LOG = log.getLogger(__name__)
 
@@ -66,6 +67,8 @@ class DFMechDriver(driver_api.MechanismDriver):
         self._set_base_port_binding()
         self.port_status = n_const.PORT_STATUS_ACTIVE
         self.subscribe_registries()
+        if 'qos' in cfg.CONF.ml2.extension_drivers:
+            df_qos.register()
 
     def post_fork_initialize(self, resource, event, trigger, **kwargs):
         # NOTE(nick-ma-z): This will initialize all workers (API, RPC,
--- dragonflow/neutron/services/qos/drivers/df_qos.py
+++ dragonflow/neutron/services/qos/drivers/df_qos.py
@@ -9,38 +9,51 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+import threading
 
+from neutron.common import constants
 from neutron.plugins.common import constants as service_constants
-from neutron.services.qos.notification_drivers import qos_base
+from neutron.services.qos.drivers import base
+from neutron.services.qos import qos_consts
+from neutron_lib.api.definitions import portbindings
 from neutron_lib.plugins import directory
+from oslo_log import log
 
+from dragonflow.db import api_nb
 from dragonflow.db.models import qos
 from dragonflow.db.neutron import lockedobjects_db as lock_db
 from dragonflow.neutron.db.models import qos as n_qos
 
+LOG = log.getLogger(__name__)
 
-class DFQosServiceNotificationDriver(
-        qos_base.QosServiceNotificationDriverBase):
-    """Dragonflow notification driver for QoS."""
+SUPPORTED_RULES = {
+    qos_consts.RULE_TYPE_BANDWIDTH_LIMIT: {
+        qos_consts.MAX_KBPS: {
+            'type:range': [0, constants.DB_INTEGER_MAX_VALUE],
+        },
+        qos_consts.MAX_BURST: {
+            'type:range': [0, constants.DB_INTEGER_MAX_VALUE],
+        },
+    },
+    qos_consts.RULE_TYPE_DSCP_MARKING: {
+        qos_consts.DSCP_MARK: {'type:values': constants.VALID_DSCP_MARKS},
+    }
+}
+VIF_TYPES = [
+    portbindings.VIF_TYPE_OVS,
+    portbindings.VIF_TYPE_VHOST_USER,
+]
+VNIC_TYPES = [portbindings.VNIC_NORMAL],
 
-    def __init__(self):
-        self._nb_api = None
 
-    def get_description(self):
-        return "Notification driver for Dragonflow"
+class DfQosDriver(base.DriverBase):
+    def __init__(self, *args, **kwargs):
+        super(DfQosDriver, self).__init__(*args, **kwargs)
+        self.nb_api = api_nb.NbApi.get_instance(True)
 
     @property
     def _plugin(self):
         return directory.get_plugin(service_constants.QOS)
-
-    @property
-    def nb_api(self):
-        if self._nb_api is None:
-            plugin = directory.get_plugin()
-            mech_driver = plugin.mechanism_manager.mech_drivers['df'].obj
-            self._nb_api = mech_driver.nb_api
-
-        return self._nb_api
 
     @lock_db.wrap_db_lock(lock_db.RESOURCE_QOS)
     def create_policy(self, context, policy):
@@ -60,3 +73,25 @@ class DFQosServiceNotificationDriver(
     def delete_policy(self, context, policy):
         policy_id = policy['id']
         self.nb_api.delete(qos.QosPolicy(id=policy_id))
+
+    @classmethod
+    def create(cls):
+        return cls(
+            name='df',
+            requires_rpc_notifications=False,
+            supported_rules=SUPPORTED_RULES,
+            vif_types=VIF_TYPES,
+            vnic_types=VNIC_TYPES,
+        )
+
+
+_driver = None
+_init_lock = threading.Lock()
+
+
+def register():
+    global _driver
+    with _init_lock:
+        if _driver is None:
+            _driver = DfQosDriver.create()
+            LOG.error('DF QoS driver registered')
