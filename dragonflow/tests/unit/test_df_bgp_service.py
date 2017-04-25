6b2c4bf97e34116da9295f8831d853a9ffe69383
Hong Hui Xiao <honghui_xiao@yeah.net>
2017-04-25 10:24:27 +0800

    Add bgp driver
    Add the functional bgp driver. Currently, only driver based on ryu
    is supported.
    
    Change-Id: I85206e387de79a3da8c2929cd4163cf341fea704
    Partially-implements: blueprint bgp-dynamic-routing

--- dragonflow/tests/unit/test_df_bgp_service.py
+++ dragonflow/tests/unit/test_df_bgp_service.py
@@ -13,18 +13,83 @@
 import eventlet
 import mock
 
+from dragonflow import conf as cfg
 from dragonflow.controller import df_bgp_service
 from dragonflow.db.models import bgp
 from dragonflow.tests import base as tests_base
 
 
+def get_all_side_effect(model, topic):
+    if model == bgp.BGPPeer:
+        return [bgp.BGPPeer(id="peer1",
+                            topic="topic1",
+                            name="peer1",
+                            peer_ip="172.24.4.88",
+                            remote_as=4321)]
+
+    if model == bgp.BGPSpeaker:
+        return [bgp.BGPSpeaker(id="speaker1",
+                               topic="topic1",
+                               name="speaker1",
+                               local_as=1234,
+                               peers=["peer1"],
+                               routes=[],
+                               ip_version=4)]
+
+
 class TestDFBGPService(tests_base.BaseTestCase):
 
-    @mock.patch('dragonflow.db.api_nb.NbApi.get_instance')
-    def test_sync_bgp_data_to_db_store(self, get_instance):
-        bgp_service = df_bgp_service.BGPService()
+    def setUp(self):
+        super(TestDFBGPService, self).setUp()
+        mock.patch('dragonflow.controller.df_bgp_service.'
+                   'BGPService.initialize_driver').start()
+        mock_nb_api = mock.patch('dragonflow.db.api_nb.NbApi.get_instance')
+        mock_nb_api.start()
+        self.addCleanup(mock_nb_api.stop)
+        self.bgp_service = df_bgp_service.BGPService()
+        self.bgp_service.bgp_driver = mock.Mock()
 
-        def get_all_side_effect(model, topic):
+        iter_models = mock.patch('dragonflow.db.model_framework.iter_models',
+                                 return_value={bgp.BGPSpeaker, bgp.BGPPeer})
+        iter_models.start()
+        self.addCleanup(iter_models.stop)
+        self.bgp_service.start()
+        self.addCleanup(self.bgp_service.stop)
+
+    def test_sync_bgp_data_to_db_store(self):
+        self.bgp_service.nb_api.get_all.side_effect = get_all_side_effect
+        # Give fixed interval a chance to run.
+        eventlet.sleep(0)
+
+        self.assertTrue(
+            self.bgp_service.db_store.get_one(bgp.BGPPeer(id="peer1")))
+        self.assertTrue(
+            self.bgp_service.db_store.get_one(bgp.BGPSpeaker(id="speaker1")))
+
+    def test_add_remove_bgp_peer_speaker(self):
+        self.bgp_service.nb_api.get_all.side_effect = get_all_side_effect
+        # Give fixed interval a chance to run.
+        eventlet.sleep(0)
+
+        self.bgp_service.bgp_driver.add_bgp_speaker.assert_called_once_with(
+            1234)
+        self.bgp_service.bgp_driver.add_bgp_peer.assert_called_once_with(
+            1234, "172.24.4.88", 4321)
+
+        self.bgp_service.nb_api.get_all.side_effect = lambda x, y: []
+        # Give fixed interval another round.
+        eventlet.sleep(cfg.CONF.df_bgp.pulse_interval + 1)
+        self.bgp_service.bgp_driver.delete_bgp_peer.assert_called_once_with(
+            1234, "172.24.4.88")
+        self.bgp_service.bgp_driver.delete_bgp_speaker.assert_called_once_with(
+            1234)
+
+    def test_advertise_withdraw_routes(self):
+        self.bgp_service.nb_api.get_all.side_effect = get_all_side_effect
+        # Give fixed interval a chance to run.
+        eventlet.sleep(0)
+
+        def get_all_with_routes_side_effect(model, topic):
             if model == bgp.BGPPeer:
                 return [bgp.BGPPeer(id="peer1",
                                     topic="topic1",
@@ -33,21 +98,25 @@ class TestDFBGPService(tests_base.BaseTestCase):
                                     remote_as=4321)]
 
             if model == bgp.BGPSpeaker:
+                routes = [{'destination': "10.0.0.0/24",
+                           'nexthop': "172.24.4.66"}]
                 return [bgp.BGPSpeaker(id="speaker1",
                                        topic="topic1",
                                        name="speaker1",
                                        local_as=1234,
                                        peers=["peer1"],
+                                       routes=routes,
                                        ip_version=4)]
 
-        bgp_service.nb_api.get_all.side_effect = get_all_side_effect
-        with mock.patch('dragonflow.db.model_framework.iter_models',
-                        return_value={bgp.BGPSpeaker, bgp.BGPPeer}):
-            bgp_service.start()
-            self.addCleanup(bgp_service.stop)
-            # Give fixed interval a chance to run.
-            eventlet.sleep(0)
-            self.assertTrue(
-                bgp_service.db_store.get_one(bgp.BGPPeer(id="peer1")))
-            self.assertTrue(
-                bgp_service.db_store.get_one(bgp.BGPSpeaker(id="speaker1")))
+        self.bgp_service.nb_api.get_all.side_effect = (
+            get_all_with_routes_side_effect)
+        # Give fixed interval another round.
+        eventlet.sleep(cfg.CONF.df_bgp.pulse_interval + 1)
+        self.bgp_service.bgp_driver.advertise_route.assert_called_once_with(
+            1234, "10.0.0.0/24", "172.24.4.66")
+
+        self.bgp_service.nb_api.get_all.side_effect = get_all_side_effect
+        # Give fixed interval another round.
+        eventlet.sleep(cfg.CONF.df_bgp.pulse_interval + 1)
+        self.bgp_service.bgp_driver.withdraw_route.assert_called_once_with(
+            1234, "10.0.0.0/24")
