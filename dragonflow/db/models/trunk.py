d6d1f939854b40dc9fb8552843a0ae30f782f2bf
Omer Anson <omer.anson@toganetworks.com>
2017-05-23 14:42:01 +0300

    Add VLAN aware VMs support to Dragonflow
    Add support for VLAN aware VMs to Dragonflow. This patch includes:
    * Dragonflow app
    * Neutron driver
    * devstack updates to use q-trunk
    * unit tests
    * fullstack tests
    
    Change-Id: I3b940635853525d55fec67a09383083d6ab3adb8
    Implements: bp vlan-trunk

---
+++ dragonflow/db/models/trunk.py
@@ -0,0 +1,39 @@
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import uuid
+
+from jsonmodels import fields
+
+import dragonflow.db.field_types as df_fields
+import dragonflow.db.model_framework as mf
+from dragonflow.db.models import l2
+from dragonflow.db.models import mixins
+
+
+SEGMENTATION_TYPE_VLAN = 'vlan'
+SUPPORTED_SEGMENTATION_TYPES = (SEGMENTATION_TYPE_VLAN, )
+UUID_NAMESPACE = uuid.UUID('a11fee2a-d833-4e22-be31-f915b55f1f77')
+
+
+@mf.register_model
+@mf.construct_nb_db_model(indexes={'lport_id': 'port.id'})
+class ChildPortSegmentation(mf.ModelBase, mixins.Topic, mixins.BasicEvents):
+    table_name = 'child_port_segmentation'
+
+    parent = df_fields.ReferenceField(l2.LogicalPort, required=True)
+    port = df_fields.ReferenceField(l2.LogicalPort, required=True)
+    segmentation_type = df_fields.EnumField(SUPPORTED_SEGMENTATION_TYPES,
+                                            required=True)
+    segmentation_id = fields.IntField(required=True)
