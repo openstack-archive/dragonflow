0b54975f6803eaa65f8c7366f41793ce7bc6683e
Omer Anson <omer.anson@toganetworks.com>
2017-05-23 14:39:13 +0300

    Migrate lport in metadata service daemon
    This change was missed in the original commit.
    
    Change-Id: I84aafaca28c531bcc8f6c27bf83407515a6a57c4
    Partially-Implements: blueprint refactor-nb-api

--- dragonflow/tests/unit/test_metadata_service_app.py
+++ dragonflow/tests/unit/test_metadata_service_app.py
@@ -14,7 +14,12 @@
 #    under the License.
 
 import mock
+import netaddr
+from neutron.conf.agent.metadata import config as metadata_config
+from oslo_config import fixture as cfg_fixture
 
+from dragonflow.controller import metadata_service_app
+from dragonflow.tests import base as tests_base
 from dragonflow.tests.unit import test_app_base
 
 
@@ -57,3 +62,44 @@ class TestMetadataServiceApp(test_app_base.DFAppTestBase):
             # Duplicated updated will not trigger update flow
             self.controller.ovs_port_updated(fake_ovs_port)
             mock_func.assert_not_called()
+
+
+class TestMetadataServiceProxy(tests_base.BaseTestCase):
+    def setUp(self):
+        super(TestMetadataServiceProxy, self).setUp()
+        self.nb_api = mock.Mock()
+        self.cfg = self.useFixture(cfg_fixture.Config())
+        self.cfg.register_opts(metadata_config.METADATA_PROXY_HANDLER_OPTS)
+        self.cfg.config(nova_metadata_host='nova-host',
+                        nova_metadata_port=443,
+                        nova_metadata_protocol='https')
+        self.proxy = metadata_service_app.DFMetadataProxyHandler(self.cfg.conf,
+                                                                 self.nb_api)
+
+    def test_proxy_get_headers(self):
+        req = mock.Mock()
+        req.remote_addr = '128.0.0.3'
+        req.headers = {}
+        lport = mock.Mock()
+        lport.topic = 'tenant1'
+        lport.device_id = 'device_id1'
+        lport.ip = netaddr.IPAddress('10.0.0.3')
+        lport.unique_key = 3
+        self.nb_api.get_all.return_value = [lport]
+        with mock.patch.object(self.proxy, '_sign_instance_id') as sign:
+            sign.return_value = 'instance_id'
+            headers = self.proxy.get_headers(req)
+            self.assertEqual({
+                              'X-Forwarded-For': '10.0.0.3',
+                              'X-Tenant-ID': 'tenant1',
+                              'X-Instance-ID': 'device_id1',
+                              'X-Instance-ID-Signature': 'instance_id',
+                             }, headers)
+
+    def test_proxy_get_host(self):
+        host = self.proxy.get_host(mock.sentinel)
+        self.assertEqual('nova-host:443', host)
+
+    def test_proxy_get_scheme(self):
+        scheme = self.proxy.get_scheme(mock.sentinel)
+        self.assertEqual('https', scheme)
