cc975ccaa016b9d458dd9121277d4e4cc5d5ee3f
Omer Anson <omer.anson@toganetworks.com>
2017-06-11 16:47:19 +0300

    An instance update also sends an update on all referred instances
    When an instance is updated/created in the DF local controller, the DF local
    controller now iterates all references within the instance, and sends an
    update/create event for all instances referenced by that instance.
    
    Related-Bug: #1690775
    Related-Bug: #1708178
    Change-Id: Ic2ee535c0898b37c200719381f61c954b9ff7ddf

--- dragonflow/db/model_framework.py
+++ dragonflow/db/model_framework.py
@@ -41,6 +41,11 @@ def _normalize_tuple(v):
     return tuple(v)
 
 
+def is_submodel(instance):
+    return (isinstance(instance, ModelBase) or
+            hasattr(instance, 'get_object'))
+
+
 class _CommonBase(models.Base):
     '''Base class for extending jsonmodels' Base
 
@@ -267,6 +272,23 @@ class _CommonBase(models.Base):
             for subobj in subobjs:
                 if isinstance(subobj, ModelBase):
                     yield subobj
+
+    def iter_submodels(self):
+        for name, field in self:
+            member = getattr(self, name)
+            if isinstance(member, list):
+                for instance in member:
+                    if is_submodel(instance):
+                        yield instance
+            elif is_submodel(member):
+                yield member
+
+    def iter_references(self):
+        for submodel in self.iter_submodels():
+            try:
+                yield submodel
+            except AttributeError:
+                pass  # Not a reference
 
     @classmethod
     def get_index(cls, index):
