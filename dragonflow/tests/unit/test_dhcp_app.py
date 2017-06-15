043b1218392961cacd96f67f12c35708b236bd4b
Eyal Leshem <eyal.leshem@toganetworks.com>
2017-06-15 17:10:26 +0300

    Move add-option part when reciving  DHCP-packet to function
    (also add unit test that create fake-l2-port
    and send fake-dhcp-packet to this port and check that
    we are getting an answer
    
    Change-Id: Id787bfac497fab0bec767871557f7aacb45fcef1
    Related-bug: #1585170

--- dragonflow/tests/unit/test_dhcp_app.py
+++ dragonflow/tests/unit/test_dhcp_app.py
@@ -144,3 +144,31 @@ class TestDHCPApp(test_app_base.DFAppTestBase):
         json = fake_lport.to_json()
         loaded_lport = l2.LogicalPort.from_json(json)
         loaded_lport.validate()
+
+    def _create_fake_empty_packet(self):
+        pkt = ryu_packet.Packet()
+        pkt.add_protocol(ethernet.ethernet(
+            ethertype=ether.ETH_TYPE_IP))
+        pkt.add_protocol(ipv4.ipv4())
+        return pkt
+
+    def test_dhcp_repsonse(self):
+
+        req = dhcp.dhcp(op=dhcp.DHCP_DISCOVER, chaddr='aa:aa:aa:aa:aa:aa')
+
+        fake_lport = test_app_base.make_fake_local_port(
+            lswitch=test_app_base.fake_logic_switch1,
+            subnets=test_app_base.fake_lswitch_default_subnets,
+            ips=('1.2.3.4',)
+        )
+
+        pkt = self._create_fake_empty_packet()
+        dhcp_response_pkt = self.app._create_dhcp_response(pkt,
+                                                           req,
+                                                           dhcp.DHCP_OFFER,
+                                                           fake_lport)
+
+        self.assertTrue(dhcp_response_pkt)
+
+        dhcp_response = dhcp_response_pkt.get_protocol(dhcp.dhcp)
+        self.assertEqual(str(dhcp_response.yiaddr), '1.2.3.4')
