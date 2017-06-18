7c92a70a62f5833930bb1632c5faa9b683ac8ae5
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2017-06-18 08:38:46 +0300

    Change DNAT to rely on Provider app for bridge access
    Until now, DNAT app maintained its own patch ports towards the external
    network and classified the relevant traffic in table 0. This created a
    conflict when provider network.
    
    This removes a lot of the functionality done by provider / L2 apps. L2
    app classifies the network from the external network, marks a relevant reg7
    and forwards it to reg7. DNAT app now intercepts the packets in
    EGRESS_TABLE, translates the addresses and reinjects it into L2 again,
    where it will be forwarded to by the datapath.
    
    On VM egress, the packet is again intercepted in L3 table and translated
    again.
    
    Closes-Bug: #1636829
    Depends-On: Ia75c52428fff1c17819fa02f3834b2f1d1c839f9
    Change-Id: I8fb9e0e85510c0814e47cc7bdfd27648fd4b88ba

--- dragonflow/tests/fullstack/test_apps.py
+++ dragonflow/tests/fullstack/test_apps.py
@@ -2180,6 +2180,19 @@ class TestAllowedAddressPairsDetectActive(test_base.DFTestBase):
 
 
 class TestDNATApp(test_base.DFTestBase):
+    def trace(self):
+        test_utils.print_command(['ip', 'addr'])
+        test_utils.print_command(['ip', 'link'])
+        test_utils.print_command(['ovs-vsctl', 'show'], True)
+        test_utils.print_command(
+            ['ovs-ofctl', 'show', self.integration_bridge],
+            True
+        )
+        test_utils.print_command(
+            ['ovs-ofctl', 'dump-flows', self.integration_bridge],
+            True
+        )
+
     def setUp(self):
         super(TestDNATApp, self).setUp()
 
@@ -2299,6 +2312,10 @@ class TestDNATApp(test_base.DFTestBase):
                 unknown_port_action=ignore_action
             )
         )
+
+        # FIXME remove
+        time.sleep(10)
+        self.trace()
         policy.start(self.topology)
         policy.wait(const.DEFAULT_RESOURCE_READY_TIMEOUT)
         if len(policy.exceptions) > 0:
@@ -2391,6 +2408,9 @@ class TestDNATApp(test_base.DFTestBase):
                 unknown_port_action=ignore_action
             )
         )
+        # FIXME remove
+        time.sleep(10)
+        self.trace()
         policy.start(self.topology)
         policy.wait(const.DEFAULT_RESOURCE_READY_TIMEOUT)
         if len(policy.exceptions) > 0:
