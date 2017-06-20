eea3293ef3cdf2200b66678a587c7ca8058533dc
Eyal Leshem <eyal.leshem@toganetworks.com>
2017-06-20 12:42:59 +0300

    Answer to "Parameter Request List" option in DHCP app
    According to RFC 2131 the dhcp-server should try to answer
    as many of the  requested parametrs as possibole.
    
    This commit implement this behaviour by answering according
    to the extra-dhcp-opts that defined by neutron.
    (https://specs.openstack.org/openstack/neutron-specs/specs/api/extra_dhcp_options__extra-dhcp-opt_.html)
    
    that should enable PXE boot - by answring the "TFTP server name"
    and "Bootfile name" accodring to the user configuration
    
    Change-Id: If513ece70d409cabb71a46f14cd8b1e8c8325e1c
    Related-bug: #1585170

--- dragonflow/tests/unit/test_dhcp_app.py
+++ dragonflow/tests/unit/test_dhcp_app.py
@@ -141,6 +141,28 @@ class TestDHCPApp(test_app_base.DFAppTestBase):
             subnet, test_app_base.fake_local_port1)
         self.assertEqual('10.0.0.1', str(gateway_ip))
 
+    def _build_dhcp_test_fake_lport(self, dhcp_params=None):
+        fake_loprt = test_app_base.make_fake_local_port(
+            lswitch=test_app_base.fake_logic_switch1,
+            subnets=test_app_base.fake_lswitch_default_subnets,
+            ips=('10.0.0.1',),
+            dhcp_params=dhcp_params
+        )
+
+        return fake_loprt
+
+    def _send_dhcp_req_to_app(self, lport, options=None):
+        req = dhcp.dhcp(op=dhcp.DHCP_DISCOVER,
+                        chaddr='aa:aa:aa:aa:aa:aa',
+                        options=dhcp.options(options))
+        pkt = self._create_fake_empty_packet()
+        dhcp_response_pkt = self.app._create_dhcp_response(pkt,
+                                                           req,
+                                                           dhcp.DHCP_OFFER,
+                                                           lport)
+
+        return dhcp_response_pkt
+
     def _create_fake_empty_packet(self):
         pkt = ryu_packet.Packet()
         pkt.add_protocol(ethernet.ethernet(
@@ -150,19 +172,45 @@ class TestDHCPApp(test_app_base.DFAppTestBase):
 
     def test_dhcp_repsonse(self):
 
-        req = dhcp.dhcp(op=dhcp.DHCP_DISCOVER, chaddr='aa:aa:aa:aa:aa:aa')
-
-        fake_lport = test_app_base.make_fake_local_port(
-            lswitch=test_app_base.fake_logic_switch1,
-            subnets=test_app_base.fake_lswitch_default_subnets,
-            ips=('1.2.3.4',)
-        )
-
-        pkt = self._create_fake_empty_packet()
-        dhcp_response_pkt = self.app._create_dhcp_response(pkt,
-                                                           req,
-                                                           dhcp.DHCP_OFFER,
-                                                           fake_lport)
-
+        fake_loprt = self._build_dhcp_test_fake_lport()
+        dhcp_response_pkt = self._send_dhcp_req_to_app(fake_loprt)
+        self.assertTrue(dhcp_response_pkt)
         dhcp_response = dhcp_response_pkt.get_protocol(dhcp.dhcp)
-        self.assertEqual(str(dhcp_response.yiaddr), '1.2.3.4')
+        self.assertEqual(str(dhcp_response.yiaddr), '10.0.0.1')
+
+    def test_dhcp_request_parmas_response(self):
+
+        dhcp_opts = {
+            "1": "error",
+            "31": "a",
+            "32": "b"
+        }
+
+        dhcp_params = {"opts": dhcp_opts}
+
+        fake_lport = self._build_dhcp_test_fake_lport(dhcp_params)
+        requested_option = [chr(1), chr(31), chr(33)]
+        requested_option_connected = ''.join(requested_option)
+        option_list = [dhcp.option(dhcp.DHCP_PARAMETER_REQUEST_LIST_OPT,
+                                   requested_option_connected,
+                                   len(requested_option))
+                       ]
+
+        dhcp_response_pkt = self._send_dhcp_req_to_app(fake_lport,
+                                                       option_list)
+
+        dhcp_res = dhcp_response_pkt.get_protocol(dhcp.dhcp)
+        self.assertTrue(dhcp_res.options)
+        self.assertTrue(dhcp_res.options.option_list)
+
+        # don't override default config
+        val = self.app._get_dhcp_option_by_tag(dhcp_res, 1)
+        self.assertNotEqual(val, "error")
+        # normal behaviour
+        val = self.app._get_dhcp_option_by_tag(dhcp_res, 31)
+        self.assertEqual(val.decode("ascii"), "a")
+        # don't reponse on parameter that not requested
+        val = self.app._get_dhcp_option_by_tag(dhcp_res, 32)
+        self.assertFalse(val)
+        # don't response on requested parameter that not configured
+        val = self.app._get_dhcp_option_by_tag(dhcp_res, 33)
