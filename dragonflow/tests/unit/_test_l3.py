ab360211b7964ac2f0af9f7e3dfff683e2668987
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2017-06-23 18:35:01 +0300

    L3 app: use port/router keys in PACKET_IN handler
    L3 app used packet's MAC address to find the relevant port IP. Since
    MACs are not unique across projects or networks, we can resolve an
    incorrect IP address. This uses router/port keys to find the relevant
    resource.
    
    Closes-Bug: #1697439
    Change-Id: I2925db5c00fcad256159306fec455b8b3c4cf8bc

--- dragonflow/tests/unit/_test_l3.py
+++ dragonflow/tests/unit/_test_l3.py
@@ -13,9 +13,17 @@
 import copy
 
 import mock
+from ryu.controller import ofp_event
+from ryu.lib.packet import ethernet
 from ryu.lib.packet import icmp
+from ryu.lib.packet import in_proto
+from ryu.lib.packet import ipv4
+from ryu.lib.packet import packet
+from ryu.lib.packet import udp
+from ryu.ofproto import ofproto_v1_3_parser as ofproto_parser
 
 from dragonflow.controller.common import constants as const
+from dragonflow.db.models import l2
 from dragonflow.db.models import l3
 from dragonflow.tests.unit import test_app_base
 
@@ -63,51 +71,106 @@ class L3AppTestCaseMixin(object):
         self.assertEqual(const.L3_LOOKUP_TABLE, kwargs['table_id'])
 
     def test_reply_ttl_invalid_message_with_rate_limit(self):
-        event = mock.Mock()
-        event.msg.reason = self.app.ofproto.OFPR_INVALID_TTL
-        with mock.patch.object(self.app, "router_port_rarp_cache") as rarp:
-            rarp.get = mock.Mock(return_value="10.0.0.1")
-            with mock.patch("ryu.lib.packet.packet.Packet"):
-                with mock.patch("dragonflow.controller.common."
-                                "icmp_error_generator.generate") as icmp_error:
-                    eui_patcher = mock.patch("netaddr.EUI")
-                    eui_patcher.start()
-                    self.addCleanup(eui_patcher.stop)
-                    self.app.packet_in_handler(event)
-                    self.app.packet_in_handler(event)
-                    self.app.packet_in_handler(event)
-                    self.app.packet_in_handler(event)
+        pkt = packet.Packet()
+        pkt.add_protocol(ethernet.ethernet(dst='aa:bb:cc:dd:ee:ff'))
+        pkt.add_protocol(ipv4.ipv4(proto=in_proto.IPPROTO_UDP))
+        pkt.add_protocol(udp.udp())
+        pkt.serialize()
 
-                    self.assertEqual(self.app.conf.router_ttl_invalid_max_rate,
-                                     icmp_error.call_count)
-                    icmp_error.assert_called_with(icmp.ICMP_TIME_EXCEEDED,
-                                                  icmp.ICMP_TTL_EXPIRED_CODE,
-                                                  mock.ANY, "10.0.0.1",
-                                                  mock.ANY)
+        lswitch = l2.LogicalSwitch(
+            id='lswitch1',
+            topic='topic1',
+            unique_key=9,
+            version=1,
+        )
+        self.app.db_store.update(lswitch)
+
+        lrouter = l3.LogicalRouter(
+            id='lrouter1',
+            topic='topic1',
+            version=1,
+            unique_key=22,
+            ports=[
+                l3.LogicalRouterPort(
+                    id='lrouter1-port1',
+                    unique_key=55,
+                    topic='topic1',
+                    mac='aa:bb:cc:dd:ee:ff',
+                    network='10.0.0.1/24',
+                    lswitch='lswitch1',
+                ),
+            ],
+        )
+        self.app.db_store.update(lrouter)
+
+        event = ofp_event.EventOFPMsgBase(
+            msg=ofproto_parser.OFPPacketIn(
+                datapath=mock.Mock(),
+                reason=self.app.ofproto.OFPR_INVALID_TTL,
+                match=ofproto_parser.OFPMatch(
+                    metadata=lswitch.unique_key,
+                    reg5=lrouter.unique_key,
+                ),
+                data=pkt.data,
+            )
+        )
+
+        with mock.patch("dragonflow.controller.common."
+                        "icmp_error_generator.generate") as icmp_error:
+            for _ in range(self.app.conf.router_ttl_invalid_max_rate * 2):
+                self.app.packet_in_handler(event)
+
+            self.assertEqual(self.app.conf.router_ttl_invalid_max_rate,
+                             icmp_error.call_count)
+            icmp_error.assert_called_with(icmp.ICMP_TIME_EXCEEDED,
+                                          icmp.ICMP_TTL_EXPIRED_CODE,
+                                          mock.ANY, "10.0.0.1", mock.ANY)
 
     def test_reply_icmp_unreachable_with_rate_limit(self):
-        with mock.patch.object(self.app, "router_port_rarp_cache") as rarp:
-            rarp.values.return_value = ["10.0.0.1"]
-            event = mock.Mock()
-            fake_ip_pkt = mock.Mock()
-            fake_ip_pkt.dst = "10.0.0.1"
-            fake_pkt = mock.Mock()
-            fake_pkt.get_protocol.return_value = fake_ip_pkt
-            with mock.patch("ryu.lib.packet.packet.Packet",
-                            return_value=fake_pkt):
-                with mock.patch("dragonflow.controller.common."
-                                "icmp_error_generator.generate") as icmp_error:
-                    self.app.packet_in_handler(event)
-                    self.app.packet_in_handler(event)
-                    self.app.packet_in_handler(event)
-                    self.app.packet_in_handler(event)
+        pkt = packet.Packet()
+        pkt.add_protocol(ethernet.ethernet(dst='aa:bb:cc:dd:ee:ff'))
+        pkt.add_protocol(ipv4.ipv4(dst='10.0.0.1', proto=in_proto.IPPROTO_UDP))
+        pkt.add_protocol(udp.udp())
+        pkt.serialize()
 
-                    self.assertEqual(
-                        self.app.conf.router_port_unreach_max_rate,
-                        icmp_error.call_count)
-                    icmp_error.assert_called_with(icmp.ICMP_DEST_UNREACH,
-                                                  icmp.ICMP_PORT_UNREACH_CODE,
-                                                  mock.ANY, pkt=fake_pkt)
+        lrouter = l3.LogicalRouter(
+            id='lrouter1',
+            topic='topic1',
+            version=1,
+            unique_key=22,
+            ports=[
+                l3.LogicalRouterPort(
+                    id='lrouter1-port1',
+                    unique_key=55,
+                    topic='topic1',
+                    mac='aa:bb:cc:dd:ee:ff',
+                    network='10.0.0.1/24',
+                ),
+            ],
+        )
+        self.app.db_store.update(lrouter)
+
+        event = ofp_event.EventOFPMsgBase(
+            msg=ofproto_parser.OFPPacketIn(
+                datapath=mock.Mock(),
+                reason=self.app.ofproto.OFPR_PACKET_IN,
+                match=ofproto_parser.OFPMatch(
+                    reg7=lrouter.ports[0].unique_key,
+                ),
+                data=pkt.data,
+            )
+        )
+        with mock.patch("dragonflow.controller.common."
+                        "icmp_error_generator.generate") as icmp_error:
+            for _ in range(self.app.conf.router_port_unreach_max_rate * 2):
+                self.app.packet_in_handler(event)
+
+            self.assertEqual(
+                self.app.conf.router_port_unreach_max_rate,
+                icmp_error.call_count)
+            icmp_error.assert_called_with(icmp.ICMP_DEST_UNREACH,
+                                          icmp.ICMP_PORT_UNREACH_CODE,
+                                          pkt.data, pkt=mock.ANY)
 
     def test_add_del_router_route_after_lport(self):
         self.controller.update_lport(test_app_base.fake_local_port1)
