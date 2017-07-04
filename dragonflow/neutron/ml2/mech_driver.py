49561f7ec0196177fea6ae478a4b08bc32e7aeba
Eyal Leshem <eyal.leshem@toganetworks.com>
2017-07-04 16:45:56 +0300

    Add support for dnsmsq and dhcpd dhcp-option-names
    Change-Id: Ic00449abfba9f1104876b1d68d61d7023d7348c4
    Related-bug: #1585170

--- dragonflow/neutron/ml2/mech_driver.py
+++ dragonflow/neutron/ml2/mech_driver.py
@@ -476,6 +476,10 @@ class DFMechDriver(api.MechanismDriver):
         owner = port['device_owner']
         return owner == n_const.DEVICE_OWNER_DHCP and subnet_id is None
 
+    def update_port_precommit(self, context):
+        port = context.current
+        neutron_l2.validate_extra_dhcp_option(port)
+
     @lock_db.wrap_db_lock(lock_db.RESOURCE_ML2_NETWORK_OR_PORT)
     def update_port_postcommit(self, context):
         updated_port = context.current
