105928eec4ba14cad0d2ba87f9e77d30eb358132
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2017-07-16 09:31:23 +0300

    Update tox_install.sh to allow fetching several projects.
    Update tox_install.sh/tox_install_project.sh to ones used in
    networking-odl. Needed for [1], which adds dependency on
    networking-sfc tree
    
    [1] https://review.openstack.org/#/c/406597
    
    Change-Id: I842d23f09930ed4d7c31e5e60af23ec20850f45c

--- tools/tox_install.sh
+++ tools/tox_install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Many of neutron's repos suffer from the problem of depending on neutron,
 # but it not existing on pypi.
@@ -12,41 +12,16 @@
 # install_command=ARGV
 # default:
 # pip install {opts} {packages}
-
-ZUUL_CLONER=/usr/zuul-env/bin/zuul-cloner
-neutron_installed=$(echo "import neutron" | python 2>/dev/null ; echo $?)
-BRANCH_NAME=master
-
 set -ex
 
-cwd=$(/bin/pwd)
-
+DIR=$(dirname $0)
+${DIR}/tox_install_project.sh neutron neutron $*
 CONSTRAINTS_FILE=$1
 shift
 
 install_cmd="pip install"
 if [ $CONSTRAINTS_FILE != "unconstrained" ]; then
     install_cmd="$install_cmd -c$CONSTRAINTS_FILE"
-fi
-
-if [ $neutron_installed -eq 0 ]; then
-    echo "ALREADY INSTALLED" > /tmp/tox_install.txt
-    echo "Neutron already installed; using existing package"
-elif [ -x "$ZUUL_CLONER" ]; then
-    echo "ZUUL CLONER" > /tmp/tox_install.txt
-    cd /tmp
-    export ZUUL_BRANCH=${ZUUL_BRANCH-$BRANCH}
-    $ZUUL_CLONER --cache-dir \
-        /opt/git \
-        --branch $BRANCH_NAME \
-        git://git.openstack.org \
-        openstack/neutron
-    cd openstack/neutron
-    $install_cmd -e .
-    cd "$cwd"
-else
-    echo "PIP HARDCODE" > /tmp/tox_install.txt
-    $install_cmd -U -egit+https://git.openstack.org/openstack/neutron@$BRANCH_NAME#egg=neutron
 fi
 
 $install_cmd -U $*
