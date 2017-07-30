691365dac72a9a31a2e06d9e8b271e6605a1c68e
Eyal Leshem <eyal.leshem@toganetworks.com>
2017-07-30 17:36:08 +0300

    Security-groups - support also number as ip-protocol
    currently our mech-driver support only limited number of
    protocols that fowarward by names (e.g "tcp","udp","icmp").
    
    For fully support neutron-api we need to support also all valid
    protocols numbers.
    (see : https://github.com/openstack/neutron/blob/7403c6db89bcd38c74fa39fa614bff5c871d9d8f/neutron/extensions/securitygroup.py#L146)
    
    Change-Id: I1faf318f09dc5e0191b9ca3e36df58145e38c950
    Closes-Bug: #1703169

--- dragonflow/db/field_types.py
+++ dragonflow/db/field_types.py
@@ -304,3 +304,15 @@ class PortRangeField(fields.BaseField):
                 # Raise an error if list in not of 2 values
                 port_min, port_max = value
                 return PortRange(port_min, port_max)
+
+
+class IpProto(fields.IntField):
+
+    def validate(self, value):
+        super(IpProto, self).validate(value)
+        if value is None:
+            return
+        if value < 0 or value > 255:
+            raise errors.ValidationError(
+                _('Ip protocol value -{val} is not between [0,255]').format(
+                    val=value))
