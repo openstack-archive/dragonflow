4033a34d365037260b1503365f4e819580ada735
Miguel Lavalle <miguel.lavalle@huawei.com>
2017-08-27 19:39:10 -0500

    [WIP] Fixing vagrant configuration
    Fixing Vagrant configuration
    
    Change-Id: I3dae2469695b1b20a05320ee6f06fdff032c3baa

--- vagrant/provisioning/setup-controller.sh
+++ vagrant/provisioning/setup-controller.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-cp /dragonflow/doc/source/multi-node-conf/controller_node_local_controller.conf ~/devstack/local.conf
+cp /dragonflow/doc/source/multi-node-conf/etcd_controller_node.conf ~/devstack/local.conf
 
 # Get the IP address
-ipaddress=$(/sbin/ifconfig eth1 | grep 'inet addr' | awk -F' ' '{print $2}' | awk -F':' '{print $2}')
+ipaddress=$(/sbin/ifconfig enp0s8 | grep 'inet addr' | awk -F' ' '{print $2}' | awk -F':' '{print $2}')
 
 # Adjust some things in local.conf
+sed -i -e 's/<controller_management_IP_Address>/'$ipaddress'/g' ~/devstack/local.conf
+sed -i -e 's/<controller_data_plane_IP_Address>/'$ipaddress'/g' ~/devstack/local.conf
 cat << DEVSTACKEOF >> devstack/local.conf
 
 # Adjust this in case we're running on a cloud which may use 10.0.0.x
@@ -13,7 +15,6 @@ NETWORK_GATEWAY=10.100.100.100
 FIXED_RANGE=10.100.100.0/24
 
 # Good to set these
-HOST_IP=$ipaddress
 HOSTNAME=$(hostname)
 SERVICE_HOST_NAME=${HOSTNAME}
 SERVICE_HOST=$ipaddress
