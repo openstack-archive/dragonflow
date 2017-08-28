8b373ff040e431197e195e68e0eb333f25dad72f
Miguel Lavalle <miguel.lavalle@huawei.com>
2017-08-27 19:39:10 -0500

    [WIP] Fixing vagrant configuration
    Fixing Vagrant configuration
    
    Change-Id: I3dae2469695b1b20a05320ee6f06fdff032c3baa

--- vagrant/provisioning/setup-compute.sh
+++ vagrant/provisioning/setup-compute.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
-cp /dragonflow/doc/source/multi-node-conf/compute_node_local_controller.conf ~/devstack/local.conf
+cp /dragonflow/doc/source/multi-node-conf/etcd_compute_node.conf ~/devstack/local.conf
 if [ "$1" != "" ]; then
     sed -i -e 's/<IP address of host running everything else>/'$1'/g' ~/devstack/local.conf
 fi
 
 # Get the IP address
-ipaddress=$(/sbin/ifconfig eth1 | grep 'inet addr' | awk -F' ' '{print $2}' | awk -F':' '{print $2}')
+ipaddress=$(/sbin/ifconfig enp0s8 | grep 'inet addr' | awk -F' ' '{print $2}' | awk -F':' '{print $2}')
 
 # Adjust some things in local.conf
+sed -i -e 's/<compute_node_management_IP_Address>/'$ipaddress'/g' ~/devstack/local.conf
+sed -i -e 's/<compute_node_data_plane_IP_Address>/'$ipaddress'/g' ~/devstack/local.conf
 cat << DEVSTACKEOF >> devstack/local.conf
 
-# Set this to the address of the main DevStack host running the rest of the
-# OpenStack services.
-Q_HOST=$1
-HOST_IP=$ipaddress
 HOSTNAME=$(hostname)
 DEVSTACKEOF
 
