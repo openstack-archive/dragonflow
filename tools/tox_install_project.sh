e09e8e33bf24e86fdfee3f067f3630ee8b53bbd8
Eyal Leshem <eyal.leshem@toganetworks.com>
2017-10-01 18:37:57 +0300

    [DNM] another test for zuul3
    Change-Id: I3116864d109d98c401c870f97ddcac8e35474580
    Depends-On: I581b02cb34602cc9466e9a121544a4e3a36d8e3d

--- tools/tox_install_project.sh
+++ tools/tox_install_project.sh
@@ -23,6 +23,36 @@ ZUUL_CLONER=/usr/zuul-env/bin/zuul-cloner
 proj_installed=$(echo "import ${MOD}" | python 2>/dev/null ; echo $?)
 BRANCH_NAME=master
 
+function zuul_install_project {
+    PROJ=$1
+    # Clone the projects only if running under zuul v2
+    # Zuul v3 should get the projects from the required-projects list
+    if [ -n "$LOG_PATH" ]; then
+        # Clone the project
+        echo "ZUUL CLONER" > /tmp/tox_install-${PROJ}.txt
+        # Make this relative to current working directory so that
+        # git clean can remove it. We cannot remove the directory directly
+        # since it is referenced after $install_cmd -e.
+        mkdir -p .tmp
+        PROJECT_DIR=$(/bin/mktemp -d -p $(pwd)/.tmp)
+        pushd $PROJECT_DIR
+        $ZUUL_CLONER --cache-dir \
+            /opt/git \
+            --branch ${BRANCH_NAME} \
+            git://git.openstack.org \
+            openstack/${PROJ}
+        cd openstack/${PROJ}
+    else
+        # We assume the project exists. This is done by zuul when the project
+        # exists in the required-projects list
+        pushd src/git.openstack.org/openstack/${PROJ}
+    fi
+
+    # Install the project
+    $install_cmd -e .
+    popd
+}
+
 set -e
 set -x
 
@@ -41,21 +71,7 @@ if [ $proj_installed -eq 0 ]; then
 
     echo "${PROJ} already installed; using existing package"
 elif [ -x "$ZUUL_CLONER" ]; then
-    echo "ZUUL CLONER" > /tmp/tox_install-${PROJ}.txt
-    # Make this relative to current working directory so that
-    # git clean can remove it. We cannot remove the directory directly
-    # since it is referenced after $install_cmd -e.
-    mkdir -p .tmp
-    PROJECT_DIR=$(/bin/mktemp -d -p $(pwd)/.tmp)
-    pushd $PROJECT_DIR
-    $ZUUL_CLONER --cache-dir \
-        /opt/git \
-        --branch ${BRANCH_NAME} \
-        git://git.openstack.org \
-        openstack/${PROJ}
-    cd openstack/${PROJ}
-    $install_cmd -e .
-    popd
+    zuul_install_project $PROJ
 else
     echo "PIP HARDCODE" > /tmp/tox_install-${PROJ}.txt
     $install_cmd -U -egit+https://git.openstack.org/openstack/${PROJ}@${BRANCH_NAME}#egg=${PROJ}
