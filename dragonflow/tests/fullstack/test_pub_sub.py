04e43ab74ffad492f58779b2a09887caf742bc2d
Shachar Snapiri <shachar.snapiri@huawei.com>
2017-10-16 14:53:09 +0300

    ZMQ pubsub tests errors are slipped and not caught
    As parts of the test are running in a different thread, the exceptions
    that are thrown by the assertions are not caught, thus missed.
    
    Change-Id: Icc41cb5f2b24489f6eeb0864bf3aeeb3e9e87898

--- dragonflow/tests/fullstack/test_pub_sub.py
+++ dragonflow/tests/fullstack/test_pub_sub.py
@@ -318,12 +318,12 @@ class TestMultiprocPubSub(PubSubTestBase):
         super(TestMultiprocPubSub, self).tearDown()
 
     def _verify_event(self, table, key, action, value, topic):
-        self.assertEqual(self.event.table, table)
-        self.assertEqual(self.event.key, key)
-        self.assertEqual(self.event.action, action)
-        # Value is not tested, since it's currently set to None
-        # self.assertEqual(self.event.value, value)
-        self.assertEqual(self.event.topic, topic)
+        self.event_received_info = db_common.DbUpdate(
+            table,
+            key,
+            action,
+            value,
+            topic=topic)
         self.event_received = True
 
     def test_multiproc_pub_sub(self):
@@ -344,6 +344,14 @@ class TestMultiprocPubSub(PubSubTestBase):
         test_utils.wait_until_true(lambda: self.event_received)
         self.subscriber.close()
         self.subscriber = None
+
+        # Check that we received the same event
+        self.assertEqual(self.event.table, self.event_received_info.table)
+        self.assertEqual(self.event.key, self.event_received_info.key)
+        self.assertEqual(self.event.action, self.event_received_info.action)
+        # Value is not tested, since it's currently set to None
+        # self.assertEqual(self.event.value, self.event_received_info.value)
+        self.assertEqual(self.event.topic, self.event_received_info.topic)
 
 
 class TestDbTableMonitors(PubSubTestBase):
