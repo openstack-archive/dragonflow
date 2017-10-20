54e8ea81bb1dcded4348ba5e1da952853655413b
XieYingYun <smokony@sina.com>
2017-10-19 19:09:23 -0700

    Fix post gate hook to accommodate for new os-testr
    New os-testr uses stestr under the hood, which creates .stestr but not
    .testrepository directory in the current dir. Other than that, it
    doesn't seem like there is any difference in the format or names of
    files generated in the directory.
    
    Closes-bug: #1716746
    Change-Id: Id8e7b7c678990d0b8d3f24f349800a8765206fa0

--- dragonflow/tests/contrib/post_test_hook.sh
+++ dragonflow/tests/contrib/post_test_hook.sh
@@ -25,9 +25,9 @@ function generate_testr_results {
     sudo -H -u "$owner" chmod o+rw .
     sudo -H -u "$owner" chmod o+rw -R .stestr
     if [[ -f ".stestr/0" ]] ; then
-        ".tox/$venv/bin/subunit-1to2" < .stestr/0 > ./testrepository.subunit
-        $SCRIPTS_DIR/subunit2html ./testrepository.subunit testr_results.html
-        gzip -9 ./testrepository.subunit
+        ".tox/$venv/bin/subunit-1to2" < .stestr/0 > ./stestr.subunit
+        $SCRIPTS_DIR/subunit2html ./stestr.subunit testr_results.html
+        gzip -9 ./stestr.subunit
         gzip -9 ./testr_results.html
         sudo mv ./*.gz /opt/stack/logs/
     fi
