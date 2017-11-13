7f9afc7d1460768ecfc44ed7de173f6fc7848508
Shachar Snapiri <shachar.snapiri@huawei.com>
2017-11-13 11:11:31 +0200

    Add support for multiple/no L3 addresses on port
    As a port may have multiple IP addresses (IPv4/IPv6), we should check
    all of them and not just look at the first one as the current code does.
    Also the new code handles the valid scenario where the port has no L3
    address at all.
    
    Change-Id: I616731d31e1ec12d50c919d393d0d3990e8d2547
    Partial-Bug: #1728518

--- dragonflow/tests/unit/test_l3_proactive_app.py
+++ dragonflow/tests/unit/test_l3_proactive_app.py
@@ -47,25 +47,34 @@ class TestL3ProactiveApp(test_app_base.DFAppTestBase,
         self.assertFalse(self.app.mod_flow.called)
 
         self.controller.update(test_app_base.fake_local_port1)
-        # 2 routes, 2 mod_flow and 1 mod_flow for add lport proactive route
-        self.assertEqual(3, self.app.mod_flow.call_count)
+        # 2 routes, 2 mod_flow and 2 mod_flow for add lport proactive route
+        self.assertEqual(4, self.app.mod_flow.call_count)
 
         self.app.mod_flow.reset_mock()
         self.controller.delete(test_app_base.fake_local_port1)
-        # 2 routes, 2 mod_flow and 1 mod_flow for del lport proactive route
-        self.assertEqual(3, self.app.mod_flow.call_count)
+        # 2 routes, 2 mod_flow and 2 mod_flow for del lport proactive route
+        self.assertEqual(4, self.app.mod_flow.call_count)
+
+    def _add_test_call(self, mockpatch, *args, **kwargs):
+        mockpatch.assert_any_call(*args, **kwargs)
+        return mock.call(*args, **kwargs)
 
     def _test_add_port(self, lport):
         with mock.patch('dragonflow.controller.apps.l3_proactive.'
                         'L3ProactiveApp._add_forward_to_port_flow'
                         ) as fake_add_forward_to_port_flow:
             self.controller.update(lport)
-            fake_add_forward_to_port_flow.assert_called_once_with(
-                lport.ip,
-                lport.mac,
-                lport.lswitch.unique_key,
-                lport.unique_key
-            )
+            calls = []
+            for ip in lport.ips:
+                call = self._add_test_call(
+                    fake_add_forward_to_port_flow,
+                    ip,
+                    lport.mac,
+                    lport.lswitch.unique_key,
+                    lport.unique_key,
+                )
+                calls.append(call)
+            fake_add_forward_to_port_flow.assert_has_calls(calls)
 
     def _test_remove_port(self, lport):
         self.controller.update(lport)
@@ -73,10 +82,15 @@ class TestL3ProactiveApp(test_app_base.DFAppTestBase,
                         'L3ProactiveApp._remove_forward_to_port_flow'
                         ) as fake_remove_forward_to_port_flow:
             self.controller.delete(lport)
-            fake_remove_forward_to_port_flow.assert_called_once_with(
-                lport.ip,
-                lport.lswitch.unique_key,
-            )
+            calls = []
+            for ip in lport.ips:
+                call = self._add_test_call(
+                    fake_remove_forward_to_port_flow,
+                    ip,
+                    lport.lswitch.unique_key,
+                )
+                calls.append(call)
+            fake_remove_forward_to_port_flow.assert_has_calls(calls)
 
     def test_add_local_port(self):
         # add local port
