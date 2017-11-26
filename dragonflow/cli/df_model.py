ca995a6f10b4bd50effb3ae5d0c9e9b2da0eafa2
Shachar Snapiri <shachar.snapiri@huawei.com>
2017-11-26 12:59:45 +0200

    Added 'required' field notation
    Added the information about reuquired fields.
    In the UML, the fields names are bold.
    
    Change-Id: Id595d946f578a3b37f28f3d66462d3c61fb70b3a
    Partial-Bug: #1734146

--- dragonflow/cli/df_model.py
+++ dragonflow/cli/df_model.py
@@ -70,8 +70,8 @@ class ModelsPrinter(object):
         pass
 
     @abc.abstractmethod
-    def handle_field(self, field_name, field_type, is_single=True,
-                     restrictions=None):
+    def handle_field(self, field_name, field_type, is_required,
+                     is_single=True, restrictions=None):
         """
         Called once for every field in a model.
         """
@@ -136,13 +136,14 @@ class PlaintextPrinter(ModelsPrinter):
         print('Fields', file=self._output)
         print('------', file=self._output)
 
-    def handle_field(self, field_name, field_type,
+    def handle_field(self, field_name, field_type, is_required,
                      is_single=True, restrictions=None):
         restriction_str = \
             ' {}'.format(restrictions) if restrictions else ''
-        print('{name} : {type}{restriction}, {to_many}'.format(
+        print('{name} : {type}{restriction}{required}, {to_many}'.format(
             name=field_name, type=field_type,
             restriction=restriction_str,
+            required=', Required' if is_required else '',
             to_many='One' if is_single else 'Many'),
             file=self._output)
 
@@ -195,12 +196,13 @@ class UMLPrinter(ModelsPrinter):
         self._processed.add(model_name)
         self._model = ''
 
-    def handle_field(self, field_name, field_type, is_single=True,
-                     restrictions=None):
+    def handle_field(self, field_name, field_type, is_required,
+                     is_single=True, restrictions=None):
         restriction_str = \
             ' {}'.format(restrictions) if restrictions else ''
+        name = '<b>{}</b>'.format(field_name) if is_required else field_name
         print('  +{name} : {type} {restriction}'.format(
-              name=field_name, type=field_type, restriction=restriction_str),
+              name=name, type=field_type, restriction=restriction_str),
               file=self._output)
         self._dependencies.add((self._model, field_type,
                                 field_name, is_single))
@@ -261,7 +263,8 @@ class DfModelParser(object):
             field_type, restrictions = self._stringify_field_type(field)
 
         field_type = re.sub('Field$', '', field_type)
-        self._printer.handle_field(key, field_type, is_single, restrictions)
+        self._printer.handle_field(key, field_type, field.required,
+                                   is_single, restrictions)
 
     def _process_fields(self, df_model):
         self._printer.fields_start()
@@ -335,5 +338,5 @@ def main():
         parser.parse_models()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
