ef99929e79e671a8922bd7d22a8eedd9103f48fc
Shachar Snapiri <shachar.snapiri@huawei.com>
2017-11-26 12:59:45 +0200

    Added 'required' field notation
    Added the information about reuquired fields.
    In the UML, the fields names are bold.
    
    Change-Id: Id595d946f578a3b37f28f3d66462d3c61fb70b3a
    Partial-Bug: #1734146

--- dragonflow/cli/df_model.py
+++ dragonflow/cli/df_model.py
@@ -70,8 +70,8 @@ class ModelsPrinter(object):
         pass
 
     @abc.abstractmethod
-    def handle_field(self, field_name, field_type, is_single=True,
-                     restrictions=None):
+    def handle_field(self, field_name, field_type, is_required,
+                     is_single=True, restrictions=None):
         """
         Called once for every field in a model.
         """
@@ -136,13 +136,14 @@ class PlaintextPrinter(ModelsPrinter):
         print('Fields', file=self._output)
         print('------', file=self._output)
 
-    def handle_field(self, field_name, field_type,
+    def handle_field(self, field_name, field_type, is_required,
                      is_single=True, restrictions=None):
         restriction_str = \
             ' {}'.format(restrictions) if restrictions else ''
-        print('{} : {}{}, {}'.format(field_name, field_type,
+        print('{} : {}{}{}{}'.format(field_name, field_type,
                                      restriction_str,
-                                     "One" if is_single else "Many"),
+                                     ', Required' if is_required else '',
+                                     ', Multi' if not is_single else ''),
               file=self._output)
 
     def indexes_start(self):
@@ -174,8 +175,8 @@ class UMLPrinter(ModelsPrinter):
     def output_end(self):
         for (dst, src, name, is_single) in self._dependencies:
             if src in self._processed:
-                many_str = '* ' if is_single else '+ '
-                print('{} --{} {} : < {}'.format(dst, many_str, src, name),
+                multi_str = '* ' if is_single else '+ '
+                print('{} --{} {} : < {}'.format(dst, multi_str, src, name),
                       file=self._output)
         print('@enduml', file=self._output)
 
@@ -189,11 +190,12 @@ class UMLPrinter(ModelsPrinter):
         self._processed.add(model_name)
         self._model = ''
 
-    def handle_field(self, field_name, field_type, is_single=True,
-                     restrictions=None):
+    def handle_field(self, field_name, field_type, is_required,
+                     is_single=True, restrictions=None):
         restriction_str = \
             ' {}'.format(restrictions) if restrictions else ''
-        print('  +{} {} {}'.format(field_name, field_type, restriction_str),
+        name = '<b>{}</b>'.format(field_name) if is_required else field_name
+        print('  +{} {} {}'.format(name, field_type, restriction_str),
               file=self._output)
         self._dependencies.add((self._model, field_type,
                                 field_name, is_single))
@@ -260,7 +262,8 @@ class DfModelParser(object):
             field_type, restrictions = self._stringify_field_type(field)
 
         field_type = re.sub('Field$', '', field_type)
-        self._printer.handle_field(key, field_type, is_single, restrictions)
+        self._printer.handle_field(key, field_type, field.required,
+                                   is_single, restrictions)
 
     def _process_fields(self, df_model):
         self._printer.fields_start()
@@ -316,7 +319,7 @@ def smart_open(filename=None):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Print Dragonflow schema")
+    parser = argparse.ArgumentParser(description='Print Dragonflow schema')
     group = parser.add_mutually_exclusive_group()
     group.add_argument('--plaintext', help='Plaintext output (default)',
                        action='store_true')
@@ -334,5 +337,5 @@ def main():
         parser.parse_models()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
