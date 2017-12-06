d7b492f882e2273ce7f6e4571b66ebb3502a9119
Omer Anson <omer.anson@toganetworks.com>
2017-12-06 14:54:17 +0200

    Add support for MACVLAN/IPVLAN
    Adds support for MACVLAN and IPVLAN in Dragonflow db model and
    Dragonflow controller trunk application.
    
    This adds support for e.g., MACVLAN containers, and amphora VMs.
    
    Change-Id: I3e7a557a642d8150f48f971e33e6bcc966e76030
    Partial-Bug: #1720734

--- dragonflow/db/models/trunk.py
+++ dragonflow/db/models/trunk.py
@@ -14,6 +14,7 @@
 
 import uuid
 
+from jsonmodels import errors
 from jsonmodels import fields
 from neutron_lib import constants as n_const
 
@@ -23,7 +24,9 @@ from dragonflow.db.models import l2
 from dragonflow.db.models import mixins
 
 
-SUPPORTED_SEGMENTATION_TYPES = (n_const.TYPE_VLAN, )
+TYPE_MACVLAN = 'macvlan'
+TYPE_IPVLAN = 'ipvlan'
+SUPPORTED_SEGMENTATION_TYPES = (n_const.TYPE_VLAN, TYPE_MACVLAN, TYPE_IPVLAN)
 UUID_NAMESPACE = uuid.UUID('a11fee2a-d833-4e22-be31-f915b55f1f77')
 
 
@@ -50,4 +53,16 @@ class ChildPortSegmentation(mf.ModelBase, mixins.Topic, mixins.BasicEvents):
     port = df_fields.ReferenceField(l2.LogicalPort, required=True)
     segmentation_type = df_fields.EnumField(SUPPORTED_SEGMENTATION_TYPES,
                                             required=True)
-    segmentation_id = fields.IntField(required=True)
+    segmentation_id = fields.IntField()
+
+    def validate(self):
+        """
+        Verify that the correct fields are filled for the correct type.
+        e.g. for VLAN, segmentation_id is not None.
+        """
+        super(ChildPortSegmentation, self).validate()
+        if self.segmentation_type == n_const.TYPE_VLAN:
+            if self.segmentation_id is None:
+                raise errors.ValidationError("segmentation_id required if "
+                                             "segmentation_type is " +
+                                             n_const.TYPE_VLAN)
