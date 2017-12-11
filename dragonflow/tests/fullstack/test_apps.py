3475fb7d32a48bc04cbd5cba92435f71fbf00005
Omer Anson <omer.anson@toganetworks.com>
2017-12-11 09:38:25 +0200

    Add MACVLAN fullstack tests
    Change-Id: I7ca3a39bc5bb8d3e3c8d82fb98e814e950e34d7c
    Closes-Bug: #1720734

--- dragonflow/tests/fullstack/test_apps.py
+++ dragonflow/tests/fullstack/test_apps.py
@@ -19,11 +19,13 @@ import time
 from neutron.agent.common import ip_lib
 from neutron.agent.common import utils
 from neutron.conf.agent import common as n_common
+from neutron_lib.api.definitions import allowedaddresspairs as aap
 from neutron_lib import constants as n_const
 from oslo_log import log
 import ryu.lib.packet
 from ryu.lib.packet import dhcp
 from ryu.ofproto import inet
+import testscenarios
 import testtools
 
 from dragonflow import conf as cfg
@@ -2694,6 +2696,209 @@ class TestTrunkApp(test_base.DFTestBase):
         result = ryu.lib.packet.packet.Packet()
         result.add_protocol(ether)
         result.add_protocol(vlan)
+        result.add_protocol(ip)
+        result.add_protocol(icmp)
+        result.serialize()
+        return result.data
+
+
+class TestMACVLANApp(test_base.DFTestBase):
+
+    scenarios = testscenarios.scenarios.multiply_scenarios(
+        [
+            ('ipv4', {'cidr': '192.168.12.0/24', 'ip_ver': 4}),
+            ('ipv6', {'cidr': '1111:1111:1111::/64', 'ip_ver': 6}),
+        ],
+    )
+
+    def test_icmp_ping_pong(self):
+        # Setup base components - two ports on 1 network
+        self.topology = app_testing_objects.Topology(self.neutron, self.nb_api)
+        self.addCleanup(self.topology.close)
+        self.subnet1 = self.topology.create_subnet(cidr=self.cidr)
+        self.port1 = self.subnet1.create_port()
+        self.port2 = self.subnet1.create_port()
+        time.sleep(const.DEFAULT_RESOURCE_READY_TIMEOUT)
+
+        # Setup MACVLAN ports
+        self.network = objects.NetworkTestObj(self.neutron, self.nb_api)
+        self.addCleanup(self.network.close)
+        self.network.create()
+        self.subnet = objects.SubnetTestObj(
+                self.neutron, self.nb_api, network_id=self.network.network_id)
+        self.addCleanup(self.subnet.close)
+        self.subnet.create(subnet={
+            'use_default_subnetpool': True,
+            'ip_version': self.ip_ver,
+            'network_id': self.network.network_id
+        })
+        self.vlan_port1 = objects.PortTestObj(
+                self.neutron, self.nb_api, network_id=self.network.network_id)
+        self.addCleanup(self.vlan_port1.close)
+        self.vlan_port1.create()
+        self.vlan_port2 = objects.PortTestObj(
+                self.neutron, self.nb_api, network_id=self.network.network_id)
+        self.addCleanup(self.vlan_port2.close)
+        self.vlan_port2.create()
+
+        vlan_port1_lport = self.vlan_port1.get_logical_port()
+        self.port1.port.update({
+            aap.ADDRESS_PAIRS: [{'ip-address': vlan_port1_lport.ip}]})
+
+        vlan_port2_lport = self.vlan_port1.get_logical_port()
+        self.port2.port.update({
+            aap.ADDRESS_PAIRS: [{'ip-address': vlan_port2_lport.ip}]})
+
+        # Setup policy
+        ignore_action = app_testing_objects.IgnoreAction()
+        key1 = (self.subnet1.subnet_id, self.port1.port_id)
+        rules1 = [
+            app_testing_objects.PortPolicyRule(
+                # Detect pong, end simulation
+                app_testing_objects.RyuICMPPongFilter(self._get_ping),
+                actions=[app_testing_objects.DisableRuleAction(),
+                         app_testing_objects.StopSimulationAction()]
+            ),
+            app_testing_objects.PortPolicyRule(
+                # Ignore gratuitous ARP packets
+                app_testing_objects.RyuARPGratuitousFilter(),
+                actions=[
+                    ignore_action
+                ]
+            ),
+            app_testing_objects.PortPolicyRule(
+                # Ignore IPv6 packets
+                app_testing_objects.RyuIPv6Filter(),
+                actions=[
+                    ignore_action
+                ]
+            ),
+        ]
+        key2 = (self.subnet1.subnet_id, self.port2.port_id)
+        rules2 = [
+            app_testing_objects.PortPolicyRule(
+                # Detect ping, reply with pong
+                app_testing_objects.RyuICMPPingFilter(),
+                actions=[app_testing_objects.SendAction(
+                                self.subnet1.subnet_id,
+                                self.port2.port_id,
+                                self._create_pong_packet),
+                         app_testing_objects.DisableRuleAction()]
+            ),
+            app_testing_objects.PortPolicyRule(
+                # Ignore gratuitous ARP packets
+                app_testing_objects.RyuARPGratuitousFilter(),
+                actions=[
+                    ignore_action
+                ]
+            ),
+            app_testing_objects.PortPolicyRule(
+                # Ignore IPv6 packets
+                app_testing_objects.RyuIPv6Filter(),
+                actions=[
+                    ignore_action
+                ]
+            ),
+        ]
+        raise_action = app_testing_objects.RaiseAction("Unexpected packet")
+        policy1 = app_testing_objects.PortPolicy(
+            rules=rules1,
+            default_action=raise_action
+        )
+        policy2 = app_testing_objects.PortPolicy(
+            rules=rules2,
+            default_action=raise_action
+        )
+        port_policies = {key1: policy1, key2: policy2}
+        initial_packet = self._create_ping_packet()
+        policy = app_testing_objects.Policy(
+                initial_actions=[
+                    app_testing_objects.SendAction(
+                        self.subnet1.subnet_id,
+                        self.port1.port_id,
+                        initial_packet,
+                    ),
+                ],
+                port_policies=port_policies,
+                unknown_port_action=app_testing_objects.IgnoreAction())
+        self.addCleanup(policy.close)
+
+        time.sleep(const.DEFAULT_RESOURCE_READY_TIMEOUT)
+
+        # Verify port is up
+        neutron_vlan_port1 = objects.get_port_by_id(self.neutron,
+                                                    self.vlan_port1.port_id)
+        self.assertEqual(n_const.PORT_STATUS_ACTIVE,
+                         neutron_vlan_port1['status'])
+        neutron_vlan_port2 = objects.get_port_by_id(self.neutron,
+                                                    self.vlan_port2.port_id)
+        self.assertEqual(n_const.PORT_STATUS_ACTIVE,
+                         neutron_vlan_port2['status'])
+
+        # Verify connectivity
+        policy.start(self.topology)
+        policy.wait(const.DEFAULT_RESOURCE_READY_TIMEOUT)
+        if len(policy.exceptions) > 0:
+            raise policy.exceptions[0]
+
+    def _get_ping(self):
+        return self._ping
+
+    def _create_ping_packet(self, ttl=255):
+        ethernet = ryu.lib.packet.ethernet.ethernet(
+            src=self.vlan_port1.get_logical_port().mac,
+            dst=self.vlan_port2.get_logical_port().mac,
+            ethertype=ryu.lib.packet.ethernet.ether.ETH_TYPE_IP,
+        )
+        ip = ryu.lib.packet.ipv4.ipv4(
+            src=str(self.vlan_port1.get_logical_port().ip),
+            dst=str(self.vlan_port2.get_logical_port().ip),
+            ttl=ttl,
+            proto=ryu.lib.packet.ipv4.inet.IPPROTO_ICMP,
+        )
+        ip_data = ryu.lib.packet.icmp.icmp(
+            type_=ryu.lib.packet.icmp.ICMP_ECHO_REQUEST,
+            data=ryu.lib.packet.icmp.echo(
+                data=self._create_random_string())
+        )
+        self._ping = ip_data
+        result = ryu.lib.packet.packet.Packet()
+        result.add_protocol(ethernet)
+        result.add_protocol(ip)
+        result.add_protocol(ip_data)
+        result.serialize()
+        return result.data
+
+    def _create_pong_packet(self, buf):
+        pkt = ryu.lib.packet.packet.Packet(buf)
+        ether = pkt.get_protocol(ryu.lib.packet.ethernet.ethernet)
+        ip = pkt.get_protocol(ryu.lib.packet.ipv4.ipv4)
+        icmp = pkt.get_protocol(ryu.lib.packet.icmp.icmp)
+
+        ether.src, ether.dst = ether.dst, ether.src
+        self.assertEqual(
+            ether.src,
+            str(self.vlan_port2.get_logical_port().mac)
+        )
+        self.assertEqual(
+            ether.dst,
+            str(self.vlan_port1.get_logical_port().mac)
+        )
+
+        ip.src, ip.dst = ip.dst, ip.src
+        self.assertEqual(
+            netaddr.IPAddress(ip.src),
+            self.vlan_port2.get_logical_port().ip
+        )
+        self.assertEqual(
+            netaddr.IPAddress(ip.dst),
+            self.vlan_port1.get_logical_port().ip
+        )
+
+        icmp.type = ryu.lib.packet.icmp.ICMP_ECHO_REPLY
+        icmp.csum = 0
+        result = ryu.lib.packet.packet.Packet()
+        result.add_protocol(ether)
         result.add_protocol(ip)
         result.add_protocol(icmp)
         result.serialize()
