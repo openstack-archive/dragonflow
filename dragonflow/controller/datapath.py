b4daff7989ece87b28efbe385c2ab38052bbe7cb
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2017-12-20 17:32:21 +0200

    [WIP] New application framework
    Change-Id: I81ee47b0c6e741888b3446602cca5e1835c9ae2f
    Related-Bug: #1738986
    Co-Authored-By: Lihi Wishnitzer <lihiwish@gmail.com>

---
+++ dragonflow/controller/datapath.py
@@ -0,0 +1,216 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import stevedore
+
+from dragonflow._i18n import _
+from dragonflow import conf as cfg
+from dragonflow.controller import app_base
+
+REGS = (
+    'reg0',
+    'reg1',
+    'reg2',
+    'reg3',
+    'reg4',
+    'reg5',
+    'reg6',
+    'reg7',
+    'metadata',
+)
+
+
+def _sequence_generator(offset):
+    while True:
+        yield offset
+        offset += 1
+
+
+class Datapath(object):
+    def __init__(self, layout):
+        self._layout = layout
+        self._dp_allocs = {}
+        self._public_variables = set()
+
+    def set_up(self, ryu_base, vswitch_api, nb_api, notifier):
+        self._dp = ryu_base.datapath
+        self._table_generator = _sequence_generator(
+            cfg.CONF.df.datapath_autoalloc_table_offset)
+        self._public_variables.clear()
+
+        app_classes = {}
+        apps = set()
+
+        for vertex in self._layout.vertices:
+            if vertex.type in app_classes:
+                continue
+
+            app_class = self._get_app_class(vertex.type)
+            app_classes[vertex.type] = app_class
+            self._public_variables.update(
+                set(app_class._specification.public_mapping.keys()),
+            )
+
+        for vertex in self._layout.vertices:
+            app_class = app_classes[vertex.type]
+            dp_alloc = self._create_dp_alloc(app_class._specification)
+            self._dp_allocs[vertex.name] = dp_alloc
+            apps.add(
+                app_class(
+                    api=ryu_base,
+                    vswitch_api=vswitch_api,
+                    nb_api=nb_api,
+                    neutron_server_notifier=notifier,
+                    dp_alloc=dp_alloc,
+                    **(vertex.params or {})
+                ),
+            )
+
+        for app in apps:
+            app.initialize()
+
+        for edge in self._layout.edges:
+            self._install_edge(edge)
+
+    def _get_app_class(self, app_type):
+        mgr = stevedore.NamedExtensionManager(
+            'dragonflow.controller.apps',
+            [app_type],
+            invoke_on_load=False,
+        )
+        for ext in mgr:
+            return ext.obj
+        else:
+            raise RuntimeError(_('Failed to load app {0}').format(app_type))
+
+    def _create_dp_alloc(self, specification):
+        public_mapping = specification.public_mapping.copy()
+        unmapped_vars = self._public_variables.difference(public_mapping)
+        unmapped_regs = set(REGS).difference(
+            public_mapping.values(),
+        ).difference(
+            specification.private_mapping.values(),
+        )
+
+        while unmapped_vars and unmapped_regs:
+            public_mapping[unmapped_vars.pop()] = unmapped_regs.pop()
+
+        if unmapped_vars:
+            raise RuntimeError(
+                _("Can't allocate enough registers for variables"),
+            )
+
+        states_dict = {
+            state: next(self._table_generator)
+            for state in specification.states
+        }
+        states = app_base.AttributeDict(**states_dict)
+
+        exitpoints_dict = {
+            exit.name: next(self._table_generator)
+            for exit in specification.exitpoints
+        }
+        exitpoints = app_base.AttributeDict(**exitpoints_dict)
+
+        entrypoints_dict = {
+            entry.name: states[entry.target]
+            for entry in specification.entrypoints
+        }
+        entrypoints = app_base.AttributeDict(**entrypoints_dict)
+
+        return app_base.DpAlloc(
+            states=states,
+            exitpoints=exitpoints,
+            entrypoints=entrypoints,
+            full_mapping=public_mapping,
+        )
+
+    def _get_connector_config(self, connector):
+        return self._dp_allocs[connector.vertex]
+
+    def _install_edge(self, edge):
+        exitpoint = edge.exitpoint
+        exit_config = self._get_connector_config(exitpoint)
+        entrypoint = edge.entrypoint
+        entry_config = self._get_connector_config(entrypoint)
+        translations = []
+        regs_map = {}
+
+        for var in self._public_variables:
+            exit_reg = exit_config.full_mapping[var]
+            entry_reg = entry_config.full_mapping[var]
+            if exit_reg == entry_reg:
+                continue
+
+            # Find current location of exit_reg, may have been moved by a prev
+            # translation
+            while exit_reg in regs_map:
+                exit_reg = regs_map[exit_reg]
+
+            # TODO(dimak) current _install_goto swaps regs with stack, we can
+            #             do better by finding by using graphs and finding
+            #             cycles.
+            if exit_reg != entry_reg:
+                translations.append(
+                    (exit_reg, entry_reg),
+                )
+                regs_map[entry_reg] = exit_reg
+
+        self._install_goto(
+            # Source
+            exit_config.exitpoints[exitpoint.name],
+            # Destination
+            entry_config.entrypoints[entrypoint.name],
+            translations,
+        )
+
+    def _install_goto(self, source, dest, translations):
+        ofproto = self._dp.ofproto
+        parser = self._dp.ofproto_parser
+
+        actions = []
+        for src_reg, dst_reg in translations:
+            actions.extend(
+                (
+                    parser.NXActionStackPush(field=dst_reg, start=0, end=32),
+                    parser.NXActionRegMove(src_field=src_reg,
+                                           dst_field=dst_reg,
+                                           n_bits=32),
+                    parser.NXActionStackPop(field=src_reg, start=0, end=32),
+                ),
+            )
+
+        if source < dest:
+            instructions = [
+                parser.OFPInstructionActions(
+                    ofproto.OFPIT_APPLY_ACTIONS,
+                    actions,
+                ),
+                parser.OFPInstructionGotoTable(dest),
+            ]
+        else:
+            actions.append(parser.NXActionResubmitTable(table_id=dest))
+
+            instructions = [
+                parser.OFPInstructionActions(
+                    ofproto.OFPIT_APPLY_ACTIONS,
+                    actions,
+                ),
+            ]
+
+        message = parser.OFPFlowMod(
+            self._dp,
+            table_id=source,
+            command=ofproto.OFPFC_ADD,
+            match=parser.OFPMatch(),
+            instructions=instructions,
+        )
+        self._dp.send_msg(message)
