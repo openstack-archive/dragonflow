460ff0c638a3c4ab2ee5388cc5b67100b2b54143
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2017-12-24 15:11:57 +0200

    [WIP] Connect new Datapath object to ryu base
    Change-Id: I12d716d334afdec889ddfbb825ce0b34215e6b23
    Co-Authored-By: Lihi Wishnitzer <lihiwish@gmail.com>

--- dragonflow/controller/ryu_base_app.py
+++ dragonflow/controller/ryu_base_app.py
@@ -27,6 +27,8 @@ from ryu import utils
 
 from dragonflow.common import profiler as df_profiler
 from dragonflow.controller.common import constants
+from dragonflow.controller import datapath as new_dp
+from dragonflow.controller import datapath_layout as new_dp_layout
 from dragonflow.controller import dispatcher
 
 
@@ -47,6 +49,7 @@ class RyuDFAdapter(ofp_handler.OFPHandler):
         self._datapath = None
         self.table_handlers = {}
         self.first_connect = True
+        self._new_dp = new_dp.Datapath(new_dp_layout.get_datapath_layout())
 
     @property
     def datapath(self):
@@ -106,6 +109,9 @@ class RyuDFAdapter(ofp_handler.OFPHandler):
 
         self.get_sw_async_msg_config()
 
+        self._new_dp.set_up(
+            self, self.vswitch_api, self.nb_api, self.neutron_server_notifier)
+
         self.dispatcher.dispatch('switch_features_handler', ev)
 
         if not self.first_connect:
@@ -114,6 +120,7 @@ class RyuDFAdapter(ofp_handler.OFPHandler):
             self.nb_api.db_change_callback(None, None,
                                            constants.CONTROLLER_REINITIALIZE,
                                            None)
+
         self.first_connect = False
         self.vswitch_api.initialize(self.nb_api)
 
