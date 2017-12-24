350dd567a44a99fc2a804bab8a9426893884c2ad
Eyal Leshem <eyal.leshem@toganetworks.com>
2017-12-24 11:53:08 +0200

    [WIP] POC -  drivers for kuryr to access df-api directly
    This is POC for running df as k8s networking infratrcute by writing
    drivers for kuryr. (this code isn't tested yet so probebly it's
    not working ..)
    
    Change-Id: Ib172acbcbf7bb68daf73f963d74b0406e2312e64

---
+++ dragonflow/cni/kuryr_kubernetes/drivers/project_driver.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2017 OpenStack Foundation.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from dragonflow import conf as cfg
+from dragonflow.common import utils as df_utils
+from dragonflow.db import api_nb
+from dragonflow.db.models import l2
+
+from kuryr_kubernetes.controller.drivers import base
+
+
+class DfProjectDriver(base.PodProjectDriver):
+
+    def get_project(self, pod):
+        return "POC_TOPIC"
