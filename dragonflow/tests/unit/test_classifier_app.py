99da312a7323f59936e0687a4094b9ecc539b9bb
Shachar Snapiri <shachar.snapiri@huawei.com>
2018-07-19 13:24:17 +0300

    Rename OVS INTERFACE constants to PORT INTERFACE
    These constants are generic and are relevant to any switch backend we
    will use. The current name is probably because of historical reasons.
    
    Change-Id: I15ce23580daba0c3b2682d9bf0233b9298b1e642
    Partial-Bug: #1781376

--- dragonflow/tests/unit/test_classifier_app.py
+++ dragonflow/tests/unit/test_classifier_app.py
@@ -61,7 +61,7 @@ class TestClassifierAppForVlan(testscenarios.WithScenarios,
         ovs_port = ovs.OvsPort(id='fake_ovs_port',
                                lport=fake_local_vlan_port.id,
                                ofport=1, admin_state='up',
-                               type=constants.OVS_COMPUTE_INTERFACE)
+                               type=constants.SWITCH_COMPUTE_INTERFACE)
         self.controller.update(ovs_port)
         port_key = fake_local_vlan_port.unique_key
         match = self.app.parser.OFPMatch(reg7=port_key)
